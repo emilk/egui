7a9fb94029e96ec3ea57f3af32d0df49bacc131e
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2020-05-20 21:20:39 +0200

    Add a benchmark

---
+++ emigui/benches/benchmark.rs
@@ -0,0 +1,23 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut example_app = emigui::examples::ExampleApp::default();
+    let mut ctx = emigui::Context::new(1.0);
+
+    let raw_input = emigui::RawInput {
+        screen_size: emigui::vec2(1280.0, 1024.0),
+        ..Default::default()
+    };
+
+    c.bench_function("example_app", |b| {
+        b.iter(|| {
+            ctx.begin_frame(raw_input.clone());
+            let mut ui = ctx.fullscreen_ui();
+            example_app.ui(&mut ui);
+            ctx.end_frame()
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
