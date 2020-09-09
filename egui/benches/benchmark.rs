d959f71d12650bf18402fb3865a8a6ced545ded7
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2020-09-09 12:41:59 +0200

    [text] add benchmark for text layout

--- egui/benches/benchmark.rs
+++ egui/benches/benchmark.rs
@@ -1,22 +1,39 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut demo_app = egui::demos::DemoApp::default();
-    let mut ctx = egui::Context::new();
-
     let raw_input = egui::RawInput {
         screen_size: egui::vec2(1280.0, 1024.0),
         ..Default::default()
     };
 
-    c.bench_function("demo_app", |b| {
-        b.iter(|| {
-            let mut ui = ctx.begin_frame(raw_input.clone());
-            demo_app.ui(&mut ui, "");
-            ctx.end_frame()
-        })
-    });
+    {
+        let mut ctx = egui::Context::new();
+        let mut demo_app = egui::demos::DemoApp::default();
+
+        c.bench_function("demo_app", |b| {
+            b.iter(|| {
+                let mut ui = ctx.begin_frame(raw_input.clone());
+                demo_app.ui(&mut ui, "");
+                ctx.end_frame()
+            })
+        });
+    }
+
+    {
+        let mut ctx = egui::Context::new();
+        let mut ui = ctx.begin_frame(raw_input.clone());
+        c.bench_function("label", |b| {
+            b.iter(|| {
+                ui.label(LOREM_IPSUM_LONG);
+            })
+        });
+        // let _ = ctx.end_frame(); // skip, because tessellating all that text is slow
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
+
+const LOREM_IPSUM_LONG: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.";
