14d8e26fec18c8396754252c491f61a6548cf0a1
t18b219k <t18b219k@mail.cc.niigata-u.ac.jp>
2021-03-01 11:49:54 +0900

    add new function that support offscreen rendering for glium backend

--- egui_glium/src/painter.rs
+++ egui_glium/src/painter.rs
@@ -250,7 +250,13 @@ impl Painter {
         self.user_textures.push(Some(Default::default()));
         id
     }
-
+    pub fn assume_glium_texture_as_egui_texture(&mut self,texture:glium::texture::srgb_texture2d::SrgbTexture2d)->egui::TextureId{
+        let id=self.alloc_user_texture();
+        if let egui::TextureId::User(id) =id {
+            self.user_textures.insert(id as usize, Some(UserTexture{ pixels: vec![], gl_texture: Some(texture) }));
+        }
+        id
+    }
     pub fn set_user_texture(
         &mut self,
         id: egui::TextureId,
