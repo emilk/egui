a8743e14b1f1fbf17bccd6a21d7430d0cf605689
Cristian Dinu <cristian.virgil.dinu@gmail.com>
2021-04-08 08:16:25 +0300

    Apply review suggestion
    Shorter and in line with the rest of the code base
    
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- egui/src/widgets/drag_value.rs
+++ egui/src/widgets/drag_value.rs
@@ -294,7 +294,7 @@ impl<'a> Widget for DragValue<'a> {
                 ui.memory().request_focus(kb_edit_id);
                 ui.memory().drag_value.edit_string = None; // Filled in next frame
             } else if response.dragged() {
-                response.ctx.output().cursor_icon = CursorIcon::ResizeHorizontal;
+                ui.output().cursor_icon = CursorIcon::ResizeHorizontal;
 
                 let mdelta = response.drag_delta();
                 let delta_points = mdelta.x - mdelta.y; // Increase to the right and up
