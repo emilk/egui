50222798bce50db2f1063628fce4ed25e9d9cdd1
Sven Niederberger <s-niederberger@outlook.com>
2021-04-18 19:00:42 +0200

    use impl ToString

--- egui/src/widgets/plot.rs
+++ egui/src/widgets/plot.rs
@@ -222,8 +222,8 @@ impl Curve {
     }
 
     /// Name of this curve.
-    pub fn name(mut self, name: impl Into<String>) -> Self {
-        self.name = name.into();
+    pub fn name(mut self, name: impl ToString) -> Self {
+        self.name = name.to_string();
         self
     }
 }
@@ -287,9 +287,9 @@ pub struct Plot {
 }
 
 impl Plot {
-    pub fn new(name: impl Into<String>) -> Self {
+    pub fn new(name: impl ToString) -> Self {
         Self {
-            name: name.into(),
+            name: name.to_string(),
             next_auto_color_idx: 0,
 
             curves: Default::default(),
