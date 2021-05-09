b1559963bf1c28877c6f04b7966d0614fced5e9d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-05-09 14:13:09 +0200

    Enable a bunch more clippy lints

--- sh/check.sh
+++ sh/check.sh
@@ -11,7 +11,7 @@ cargo test --workspace --doc
 cargo check --workspace --all-targets --all-features
 cargo check -p egui_demo_app --lib --target wasm32-unknown-unknown
 cargo check -p egui_demo_app --lib --target wasm32-unknown-unknown --all-features
-CARGO_INCREMENTAL=0 cargo clippy --workspace --all-targets --all-features --  -D warnings -W clippy::all
+cargo clippy --workspace --all-targets --all-features --  -D warnings -W clippy::all
 cargo test --workspace --all-targets --all-features
 cargo fmt --all -- --check
 
