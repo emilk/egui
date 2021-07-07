8e2b9438ac1a737d7e4637add9cbb7c630cf4eed
Linus Behrbohm <linusbehrbohm@web.de>
2021-07-07 20:40:44 +0200

    Fix warnings

--- egui/src/response.rs
+++ egui/src/response.rs
@@ -453,7 +453,7 @@ impl Response {
     }
 
     pub fn context_menu(&self, add_contents: impl FnOnce(&mut Ui, &mut super::context_menu::MenuState)) -> &Self {
-        self.ctx.show_context_menu(&self, add_contents);
+        self.ctx.show_context_menu(self, add_contents);
         self
     }
 }
