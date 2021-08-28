d1103ba8a8366287f9a770143d7c040cf38ad3dd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-08-28 12:26:12 +0200

    Revert "feat: Set whether to show decorations (#660)"
    This reverts commit 0db74f30006d02fac0feaea601be401269524e9a.

--- egui_web/src/backend.rs
+++ egui_web/src/backend.rs
@@ -179,7 +179,6 @@ impl AppRunner {
                 http: runner.http.clone(),
                 output: &mut app_output,
                 repaint_signal: runner.needs_repaint.clone(),
-                decorated: false,
             }
             .build();
             runner.app.setup(
@@ -252,7 +251,6 @@ impl AppRunner {
             http: self.http.clone(),
             output: &mut app_output,
             repaint_signal: self.needs_repaint.clone(),
-            decorated: false,
         }
         .build();
 
@@ -268,7 +266,6 @@ impl AppRunner {
             let epi::backend::AppOutput {
                 quit: _,        // Can't quit a web page
                 window_size: _, // Can't resize a web page
-                decorated: _,   // Can't show decorations
             } = app_output;
         }
 
