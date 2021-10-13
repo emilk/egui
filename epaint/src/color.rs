1ab61ce9bbe17dd1a42ac53bcb0186bf974654b8
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-10-13 08:56:34 +0200

    Implement bytemuck for epaint::Rgba
    Closes https://github.com/emilk/egui/pull/784

--- epaint/src/color.rs
+++ epaint/src/color.rs
@@ -185,8 +185,10 @@ impl Color32 {
 // ----------------------------------------------------------------------------
 
 /// 0-1 linear space `RGBA` color with premultiplied alpha.
+#[repr(C)]
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "bytemuck", derive(bytemuck::Pod, bytemuck::Zeroable))]
 pub struct Rgba(pub(crate) [f32; 4]);
 
 impl std::ops::Index<usize> for Rgba {
