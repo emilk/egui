105cb570502fcc7b917b59457a3b1ad8a001a4e7
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-11-13 13:09:08 +0100

    Clean up glsl code

--- egui_glium/src/shader/fragment_140.glsl
+++ egui_glium/src/shader/fragment_140.glsl
@@ -6,7 +6,7 @@ in vec2 v_tc;
 out vec4 f_color;
 
 void main() {
-  // The texture sampler is sRGB aware, and glium already expects linear rgba output
-  // so no need for any sRGB conversions here:
-  f_color = v_rgba * texture(u_sampler, v_tc);
+    // The texture sampler is sRGB aware, and glium already expects linear rgba output
+    // so no need for any sRGB conversions here:
+    f_color = v_rgba * texture(u_sampler, v_tc);
 }
