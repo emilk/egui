fbbd9eab2bbf59edf0372ca69d601282f5104f88
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2022-01-04 20:52:11 +0900

    fix pure glow

--- egui_glow/examples/pure_glow.rs
+++ egui_glow/examples/pure_glow.rs
@@ -44,7 +44,7 @@ fn main() {
     let event_loop = glutin::event_loop::EventLoop::with_user_event();
     let (gl_window, gl) = create_display(&event_loop);
 
-    let mut egui_glow = egui_glow::EguiGlow::new(&gl_window, &gl);
+    let mut egui_glow = egui_glow::EguiGlow::new(&gl_window.window(), &gl);
 
     event_loop.run(move |event, _, control_flow| {
         let mut redraw = || {
@@ -78,7 +78,7 @@ fn main() {
 
                 // draw things behind egui here
 
-                egui_glow.paint(&gl_window, &gl, shapes);
+                egui_glow.paint(&gl_window.window(), &gl, shapes);
 
                 // draw things on top of egui here
 
