59ec7bd7963f5bb5ca30d0f34eedebb5e80f1c4c
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-01-15 13:43:42 +0100

    Add example of loading an image from file

--- epaint/src/image.rs
+++ epaint/src/image.rs
@@ -69,7 +69,19 @@ impl ColorImage {
     ///
     /// ## Example using the [`image`](crates.io/crates/image) crate:
     /// ``` ignore
-    /// fn load_image(image_data: &[u8]) -> Result<ColorImage, image::ImageError> {
+    /// fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
+    ///     use image::GenericImageView as _;
+    ///     let image = image::io::Reader::open(path)?.decode()?;
+    ///     let size = [image.width() as _, image.height() as _];
+    ///     let image_buffer = image.to_rgba8();
+    ///     let pixels = image_buffer.as_flat_samples();
+    ///     Ok(egui::ColorImage::from_rgba_unmultiplied(
+    ///         size,
+    ///         pixels.as_slice(),
+    ///     ))
+    /// }
+    ///
+    /// fn load_image_from_memory(image_data: &[u8]) -> Result<ColorImage, image::ImageError> {
     ///     use image::GenericImageView as _;
     ///     let image = image::load_from_memory(image_data)?;
     ///     let size = [image.width() as _, image.height() as _];
