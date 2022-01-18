f0b1103b86b19fdb9374c1abd439dae3af812a50
Dusty DeWeese <dustin.deweese@gmail.com>
2022-01-18 10:17:09 -0800

    Update egui/src/widgets/color_picker.rs
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- egui/src/widgets/color_picker.rs
+++ egui/src/widgets/color_picker.rs
@@ -308,7 +308,7 @@ fn color_picker_hsvag_2d(ui: &mut Ui, hsva: &mut HsvaGamma, alpha: Alpha) {
     color_slider_2d(ui, v, s, |v, s| HsvaGamma { s, v, ..opaque }.into());
 }
 
-//// Shows a color picker where the user can change the given Hsva color.
+//// Shows a color picker where the user can change the given [`Hsva`] color.
 ///
 /// Returns `true` on change.
 pub fn color_picker_hsva_2d(ui: &mut Ui, hsva: &mut Hsva, alpha: Alpha) -> bool {
