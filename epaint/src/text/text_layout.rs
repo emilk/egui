4e429234643fd81d2984c98b1a8144170da09b72
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-01-22 14:03:29 +0100

    Replace indexing in Fonts with member function

--- epaint/src/text/text_layout.rs
+++ epaint/src/text/text_layout.rs
@@ -86,7 +86,7 @@ fn layout_section(
         byte_range,
         format,
     } = section;
-    let font = &fonts[format.style];
+    let font = fonts.font(format.style);
     let font_height = font.row_height();
 
     let mut paragraph = out_paragraphs.last_mut().unwrap();
