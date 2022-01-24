e2c4e804d7f064d7b1f484f866a78f669d3e7a15
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2022-01-24 23:27:19 +0900

    fmt

--- egui_demo_lib/src/apps/demo/code_editor.rs
+++ egui_demo_lib/src/apps/demo/code_editor.rs
@@ -1,6 +1,5 @@
 // ----------------------------------------------------------------------------
 
-
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "serde", serde(default))]
 pub struct CodeEditor {
