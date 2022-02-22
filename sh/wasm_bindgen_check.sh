4e0654e0fd9c231a0e69cd22d645fcec88ac0ffc
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-02-22 16:07:49 +0100

    Fastet check

---
+++ sh/wasm_bindgen_check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eu
+script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$script_path/.."
+
+CRATE_NAME="egui_demo_app"
+FEATURES="http,persistence,screen_reader"
+
+# This is required to enable the web_sys clipboard API which egui_web uses
+# https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.Clipboard.html
+# https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html
+export RUSTFLAGS=--cfg=web_sys_unstable_apis
+
+echo "Building rust…"
+BUILD=debug # debug builds are faster
+
+(cd $CRATE_NAME &&
+  cargo build \
+    --lib \
+    --target wasm32-unknown-unknown \
+    --no-default-features \
+    --features ${FEATURES}
+)
+
+TARGET="target"
+
+echo "Generating JS bindings for wasm…"
+
+TARGET_PATH="${CRATE_NAME}_bg.wasm"
+rm -f ${TARGET_PATH} # Remove old output (if any)
+
+TARGET_NAME="${CRATE_NAME}.wasm"
+wasm-bindgen "${TARGET}/wasm32-unknown-unknown/$BUILD/$TARGET_NAME" \
+  --out-dir . --no-modules --no-typescript
+
+rm -f ${TARGET_PATH} # Remove output
