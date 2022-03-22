9455851c328f386c996a0ec66abc5a749c9ee690
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-03-22 10:26:22 +0100

    Add example using three-d
    https://github.com/asny/three-d recently merged a PR adding
    `glow` support: https://github.com/asny/three-d/pull/210
    This means it is a prime candidate for embedding 3D painting inside
    an eframe app.
    
    There are currently a few kinks that need to be figured out:
    
    When reusing the same three_d context over time (as one should),
    we only get one frame of egui together with three_d, and then after that
    a black screen with just the three_d painting on top.
    
    I need to fix that before merging this PR.
    
    `Shape` is `Send + Sync` and `three_d::Context` is not. This means
    we cannot store a three_d context and send it to the `Shape::Callback`.
    
    So we either need to recreate the three_d context each frame (obviously
    a bad idea), or access it through a `thread_local` hack.
    This PR adds both as examples, with a checkbox to switch.
    
    We could consider making `Shape: !Send + !Sync`, but that would mean
    `egui::Context` could not be `Send+Sync` either (because the egui
    context stores shapes).
    
    This is discussed in https://github.com/emilk/egui/issues/1399

---
+++ eframe/examples/custom_3d_three-d.rs
@@ -0,0 +1,160 @@
+//! This demo shows how to embed 3D rendering using [`three-d`](https://github.com/asny/three-d) in `eframe`.
+
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(unsafe_code)]
+
+use eframe::egui;
+
+fn main() {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Custom 3D painting in eframe",
+        options,
+        Box::new(|cc| Box::new(MyApp::new(cc))),
+    );
+}
+
+struct MyApp {
+    // three_d: three_d::Context,
+    angle: f32,
+    reuse_three_d: bool,
+}
+
+impl MyApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self {
+            // three_d: three_d::Context::from_gl_context(cc.gl.clone()).unwrap(),
+            angle: 0.0,
+            reuse_three_d: false,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.spacing_mut().item_spacing.x = 0.0;
+                ui.label("The triangle is being painted using ");
+                ui.hyperlink_to("three-d", "https://github.com/asny/three-d");
+                ui.label(".");
+            });
+
+            ui.checkbox(&mut self.reuse_three_d, "Reuse three-d context");
+
+            egui::ScrollArea::both().show(ui, |ui| {
+                egui::Frame::dark_canvas(ui.style()).show(ui, |ui| {
+                    self.custom_painting(ui);
+                });
+                ui.label("Drag to rotate!");
+            });
+        });
+    }
+}
+
+impl MyApp {
+    fn custom_painting(&mut self, ui: &mut egui::Ui) {
+        let (rect, response) =
+            ui.allocate_exact_size(egui::Vec2::splat(256.0), egui::Sense::drag());
+
+        self.angle += response.drag_delta().x * 0.01;
+
+        // Clone locals so we can move them into the paint callback:
+        let angle = self.angle;
+        let reuse_three_d = self.reuse_three_d;
+        // let three_d = self.three_d.clone();
+
+        let callback = egui::PaintCallback {
+            rect,
+            callback: std::sync::Arc::new(move |render_ctx| {
+                if let Some(painter) = render_ctx.downcast_ref::<egui_glow::Painter>() {
+                    if reuse_three_d {
+                        with_three_d_context(painter.gl(), |three_d| {
+                            paint_with_three_d(three_d, rect, angle);
+                        });
+                    } else {
+                        let three_d =
+                            three_d::Context::from_gl_context(painter.gl().clone()).unwrap();
+                        paint_with_three_d(&three_d, rect, angle);
+                    }
+                } else {
+                    eprintln!("Can't do custom painting because we are not using a glow context");
+                }
+            }),
+        };
+        ui.painter().add(callback);
+    }
+}
+
+// TODO: make `three_d::Context` a member of `MyApp` instead.
+// Requires making `Shape`, and thus `Context`, `!Send + !Sync`.
+fn with_three_d_context<R>(
+    gl: &std::rc::Rc<glow::Context>,
+    f: impl FnOnce(&three_d::Context) -> R,
+) -> R {
+    use std::cell::RefCell;
+    thread_local! {
+        pub static THREE_D: RefCell<Option<three_d::Context>> = RefCell::new(None);
+    }
+
+    THREE_D.with(|three_d| {
+        let mut three_d = three_d.borrow_mut();
+        let three_d =
+            three_d.get_or_insert_with(|| three_d::Context::from_gl_context(gl.clone()).unwrap());
+        f(three_d)
+    })
+}
+
+fn paint_with_three_d(three_d: &three_d::Context, rect: egui::Rect, angle: f32) {
+    // Based on https://github.com/asny/three-d/blob/master/examples/triangle/src/main.rs
+    use three_d::*;
+
+    let viewport = Viewport {
+        x: rect.left() as _,
+        y: rect.top() as _,
+        width: rect.width() as _,
+        height: rect.height() as _,
+    };
+
+    // Create a camera
+    let mut camera = Camera::new_perspective(
+        &three_d,
+        viewport,
+        vec3(0.0, 0.0, 2.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        10.0,
+    )
+    .unwrap();
+
+    // Create a CPU-side mesh consisting of a single colored triangle
+    let positions = vec![
+        vec3(0.5, -0.5, 0.0),  // bottom right
+        vec3(-0.5, -0.5, 0.0), // bottom left
+        vec3(0.0, 0.5, 0.0),   // top
+    ];
+    let colors = vec![
+        Color::new(255, 0, 0, 255), // bottom right
+        Color::new(0, 255, 0, 255), // bottom left
+        Color::new(0, 0, 255, 255), // top
+    ];
+    let cpu_mesh = CpuMesh {
+        positions: Positions::F32(positions),
+        colors: Some(colors),
+        ..Default::default()
+    };
+
+    // Construct a model, with a default color material, thereby transferring the mesh data to the GPU
+    let mut model = Model::new(&three_d, &cpu_mesh).unwrap();
+
+    // Ensure the viewport matches the current window viewport which changes if the window is resized
+    camera.set_viewport(viewport).unwrap();
+
+    // Set the current transformation of the triangle
+    model.set_transformation(Mat4::from_angle_y(radians(angle)));
+
+    // Render the triangle with the color material which uses the per vertex colors defined at construction
+    model.render(&camera, &[]).unwrap();
+}
