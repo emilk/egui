64eaf74c0a6611f9c8b71c978c27ae53dc5ee8bb
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-04-28 21:02:31 +0200

    Remove mention of epi in egui

--- egui/src/data/input.rs
+++ egui/src/data/input.rs
@@ -60,7 +60,7 @@ pub struct RawInput {
     /// Dragged files dropped into egui.
     ///
     /// Note: when using `eframe` on Windows you need to enable
-    /// drag-and-drop support using `epi::NativeOptions`.
+    /// drag-and-drop support using `eframe::NativeOptions`.
     pub dropped_files: Vec<DroppedFile>,
 }
 
