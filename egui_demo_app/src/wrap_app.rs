462ad5a7f67ebd7edaae2068f99cd29bd021fbbb
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-04-30 17:17:36 +0200

    egui_demo_app: fall back to dark mode if there is no system preference

--- egui_demo_app/src/wrap_app.rs
+++ egui_demo_app/src/wrap_app.rs
@@ -109,6 +109,12 @@ impl WrapApp {
             }
         }
 
+        if cc.integration_info.prefer_dark_mode == Some(false) {
+            cc.egui_ctx.set_visuals(egui::Visuals::light()); // use light mode if explicitly asked for
+        } else {
+            cc.egui_ctx.set_visuals(egui::Visuals::dark()); // use dark mode if there is no preference, or the preference is dark mode
+        }
+
         slf
     }
 
