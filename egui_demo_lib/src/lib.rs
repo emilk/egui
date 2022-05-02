3a093e4ee55570023970bf4aa7b5a0bf4848be8e
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-05-02 11:27:22 +0200

    Fix docstring

--- egui_demo_lib/src/lib.rs
+++ egui_demo_lib/src/lib.rs
@@ -95,8 +95,8 @@ fn test_egui_zero_window_size() {
 
 // ----------------------------------------------------------------------------
 
-/// Detect narrow screens. This is used by [`egui_demo_lib`] to show a simpler UI
-/// on mobile devices.
+/// Detect narrow screens. This is used to show a simpler UI on mobile devices,
+/// especially for the web demo at <https://egui.rs>.
 pub fn is_mobile(ctx: &egui::Context) -> bool {
     let screen_size = ctx.input().screen_rect().size();
     screen_size.x < 550.0
