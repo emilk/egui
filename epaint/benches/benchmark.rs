7e093f123609a7ef45b8f2a563b263422f224210
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-05-10 18:52:27 +0200

    Add benchmark for tessellation of circles of various radii

--- epaint/benches/benchmark.rs
+++ epaint/benches/benchmark.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use epaint::{pos2, Color32, Shape, Stroke};
+use epaint::*;
 
 fn single_dashed_lines(c: &mut Criterion) {
     c.bench_function("single_dashed_lines", move |b| {
@@ -39,5 +39,39 @@ fn many_dashed_lines(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, single_dashed_lines, many_dashed_lines);
+fn tessellate_circles(c: &mut Criterion) {
+    c.bench_function("tessellate_circles_100k", move |b| {
+        let radii: [f32; 10] = [1.0, 2.0, 3.6, 4.0, 5.7, 8.0, 10.0, 13.0, 15.0, 17.0];
+        let mut clipped_shapes = vec![];
+        for r in radii {
+            for _ in 0..10_000 {
+                let clip_rect = Rect::from_min_size(Pos2::ZERO, Vec2::splat(1024.0));
+                let shape = Shape::circle_filled(Pos2::new(10.0, 10.0), r, Color32::WHITE);
+                clipped_shapes.push(ClippedShape(clip_rect, shape));
+            }
+        }
+        assert_eq!(clipped_shapes.len(), 100_000);
+
+        let pixels_per_point = 2.0;
+        let options = TessellationOptions::default();
+        let font_tex_size = [1024, 1024]; // unused
+
+        b.iter(|| {
+            let clipped_primitive = tessellate_shapes(
+                pixels_per_point,
+                options,
+                clipped_shapes.clone(),
+                font_tex_size,
+            );
+            black_box(clipped_primitive);
+        });
+    });
+}
+
+criterion_group!(
+    benches,
+    single_dashed_lines,
+    many_dashed_lines,
+    tessellate_circles
+);
 criterion_main!(benches);
