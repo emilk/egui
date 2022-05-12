14b7dacb75a9cd4e9f952b151c15c066560674bd
René Rössler <rene@freshx.de>
2022-05-12 22:57:27 +0200

    add screenshot example

---
+++ examples/screenshot/src/main.rs
@@ -0,0 +1,72 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::{
+    egui::{self, ColorImage},
+    glow::{self, HasContext},
+};
+use itertools::Itertools;
+
+fn main() {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Take screenshots and display with eframe/egui",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
+
+#[derive(Default)]
+struct MyApp {
+    texture: Option<egui::TextureHandle>,
+    screenshot: Option<ColorImage>,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if let Some(screenshot) = self.screenshot.take() {
+                self.texture = Some(ui.ctx().load_texture("screenshot", screenshot));
+            }
+
+            ui.label("Screenshot:");
+            if let Some(texture) = self.texture.as_ref() {
+                ui.image(texture, texture.size_vec2());
+            } else {
+                ui.spinner();
+            }
+
+            ctx.request_repaint();
+        });
+    }
+
+    #[allow(unsafe_code)]
+    fn post_rendering(&mut self, screen_size_px: [u32; 2], frame: &eframe::Frame) {
+        let mut buf = vec![0u8; screen_size_px[0] as usize * screen_size_px[1] as usize * 4];
+        let pixels = glow::PixelPackData::Slice(&mut buf[..]);
+        unsafe {
+            frame.gl().read_pixels(
+                0,
+                0,
+                screen_size_px[0] as i32,
+                screen_size_px[1] as i32,
+                glow::RGBA,
+                glow::UNSIGNED_BYTE,
+                pixels,
+            );
+        }
+
+        let mut rows: Vec<Vec<u8>> = buf
+            .into_iter()
+            .chunks(screen_size_px[0] as usize * 4)
+            .into_iter()
+            .map(|chunk| chunk.collect())
+            .collect();
+        rows.reverse();
+        let buf: Vec<u8> = rows.into_iter().flatten().collect();
+
+        self.screenshot = Some(ColorImage::from_rgba_unmultiplied(
+            [screen_size_px[0] as usize, screen_size_px[1] as usize],
+            &buf[..],
+        ));
+    }
+}
