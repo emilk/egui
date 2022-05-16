3ec9881a0ebb15e7173264f0827a22105aa6dc41
Erlend Walstad <96946613+lampsitter@users.noreply.github.com>
2022-05-16 12:08:26 +0200

    Make color-hex optional

--- egui/src/lib.rs
+++ egui/src/lib.rs
@@ -322,8 +322,10 @@ pub use epaint;
 pub use epaint::emath;
 
 pub use emath::{lerp, pos2, remap, remap_clamp, vec2, Align, Align2, NumExt, Pos2, Rect, Vec2};
+#[cfg(feature = "color-hex")]
+pub use epaint::hex_color;
 pub use epaint::{
-    color, hex_color, mutex,
+    color, mutex,
     text::{FontData, FontDefinitions, FontFamily, FontId, FontTweak},
     textures::TexturesDelta,
     ClippedPrimitive, Color32, ColorImage, FontImage, ImageData, Mesh, PaintCallback,
