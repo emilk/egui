f2a98b3a449812e7e59d4c7930ab3b6ddc728fa5
charles <c.ray5252@gmail.com>
2022-05-30 09:40:45 -0400

    issue #1685

--- egui_extras/src/datepicker/button.rs
+++ egui_extras/src/datepicker/button.rs
@@ -95,6 +95,12 @@ impl<'a> Widget for DatePickerButton<'a> {
             if pos.x + width_with_padding > ui.clip_rect().right() {
                 pos.x = button_response.rect.right() - width_with_padding;
             }
+
+            // Check to make sure the calendar never is displayed out of window
+            if pos.x < ui.style().spacing.window_margin.left {
+                pos.x = ui.style().spacing.window_margin.left;
+            }
+
             //TODO(elwerene): Better positioning
 
             let area_response = Area::new(ui.make_persistent_id(&self.id_source))
