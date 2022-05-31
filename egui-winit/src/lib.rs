0af77fbac4303d1dc6e4001fd89ce942c1b0617c
Thomas Hansen <31554741+thomas992@users.noreply.github.com>
2022-05-31 11:23:38 -0400

    ALTSHIFT is treated as a command

--- egui-winit/src/lib.rs
+++ egui-winit/src/lib.rs
@@ -195,8 +195,10 @@ impl State {
                 // We need to ignore these characters that are side-effects of commands.
                 let is_mac_cmd = cfg!(target_os = "macos")
                     && (self.egui_input.modifiers.ctrl || self.egui_input.modifiers.mac_cmd);
-
-                if is_printable_char(*ch) && !is_mac_cmd {
+                let is_altshift: bool =
+                    self.egui_input.modifiers.alt && self.egui_input.modifiers.shift;
+                
+                if is_printable_char(*ch) && !is_mac_cmd && !is_altshift {
                     self.egui_input
                         .events
                         .push(egui::Event::Text(ch.to_string()));
