39148b2aa7faf70cc96186dd11e7140ca6c06c2a
Thomas Hansen <31554741+thomas992@users.noreply.github.com>
2022-05-31 23:18:53 -0400

    big blue

--- egui/src/lib.rs
+++ egui/src/lib.rs
@@ -371,7 +371,7 @@ pub fn warn_if_debug_build(ui: &mut crate::Ui) {
         ui.label(
             RichText::new("‼ Debug build ‼")
                 .small()
-                .color(crate::Color32::from_rgb(122, 126, 253)),
+                .color(crate::Color32::from_rgb(106, 112, 255)),
         )
         .on_hover_text("egui was compiled with debug assertions enabled.");
     }
