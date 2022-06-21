b31e11b20b594cb16a314b4d87e0d4d401baf7a9
Red Artist <redartofcode@gmail.com>
2022-06-21 11:01:44 +0530

    fix more clippy glow

--- egui_glow/examples/pure_glow.rs
+++ egui_glow/examples/pure_glow.rs
@@ -3,8 +3,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(unsafe_code)]
 
-use web_sys::window;
-
 fn main() {
     let mut clear_color = [0.1, 0.1, 0.1];
 
