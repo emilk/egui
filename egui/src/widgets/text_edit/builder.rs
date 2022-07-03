d5ac563fc1fc127b5998a2d1bf0388aab4d59a61
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-07-03 15:15:44 +0200

    cargo fmt

--- egui/src/widgets/text_edit/builder.rs
+++ egui/src/widgets/text_edit/builder.rs
@@ -591,8 +591,7 @@ impl<'t> TextEdit<'t> {
                         );
 
                         let is_fully_visible = ui.clip_rect().contains_rect(rect); // TODO: remove this HACK workaround for https://github.com/emilk/egui/issues/1531
-                        if (response.changed || selection_changed) && !is_fully_visible
-                        {
+                        if (response.changed || selection_changed) && !is_fully_visible {
                             ui.scroll_to_rect(cursor_pos, None); // keep cursor in view
                         }
 
