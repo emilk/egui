97880e18d77cddbe82030fbddb0c75b64ed11a7d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-07-29 11:14:37 +0200

    Make egui_demo_app native window size larger by default

--- egui_demo_app/src/main.rs
+++ egui_demo_app/src/main.rs
@@ -10,6 +10,8 @@ fn main() {
     let options = eframe::NativeOptions {
         drag_and_drop_support: true,
 
+        initial_window_size: Some([1280.0, 1024.0].into()),
+
         #[cfg(feature = "wgpu")]
         renderer: eframe::Renderer::Wgpu,
 
