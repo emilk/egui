c50218192c272172298031c62b61ff519716ad73
Ar37-rs <adyaro37@gmail.com>
2022-09-01 18:41:21 +0700

    Support add optional custom shader version.

--- examples/retained_image/src/main.rs
+++ examples/retained_image/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
         initial_window_size: Some(egui::vec2(500.0, 900.0)),
         ..Default::default()
     };
+
     eframe::run_native(
         "Show an image with eframe/egui",
         options,
