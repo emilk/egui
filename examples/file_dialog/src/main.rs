7949043d8d43fe93cc9568551f350c4d4501e6a1
notalpha <yan.gyunashyan@gmail.com>
2022-11-13 16:53:00 +0100

    fix: Missapplied ide fix

--- examples/file_dialog/src/main.rs
+++ examples/file_dialog/src/main.rs
@@ -91,7 +91,7 @@ fn preview_files_being_dropped(ctx: &egui::Context) {
             ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
 
         let screen_rect = ctx.input().screen_rect();
-        painter.rect_filled(screen_rect);
+        painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
         painter.text(
             screen_rect.center(),
             Align2::CENTER_CENTER,
