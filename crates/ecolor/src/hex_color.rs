c55321573330593547a1c219feae74b3df547d7d
Andreas Reich <andreas@rerun.io>
2022-12-06 12:51:09 +0100

    fix hex color issues

--- crates/ecolor/src/hex_color.rs
+++ crates/ecolor/src/hex_color.rs
@@ -1,14 +1,14 @@
 /// Construct a [`Color32`] from a hex RGB or RGBA string.
 ///
 /// ```
-/// # use epaint::{hex_color, Color32};
+/// # use ecolor::{hex_color, Color32};
 /// assert_eq!(hex_color!("#202122"), Color32::from_rgb(0x20, 0x21, 0x22));
 /// assert_eq!(hex_color!("#abcdef12"), Color32::from_rgba_unmultiplied(0xab, 0xcd, 0xef, 0x12));
 /// ```
 #[macro_export]
 macro_rules! hex_color {
     ($s:literal) => {{
-        let array = $crate::color_hex::color_from_hex!($s);
+        let array = color_hex::color_from_hex!($s);
         if array.len() == 3 {
             $crate::Color32::from_rgb(array[0], array[1], array[2])
         } else {
