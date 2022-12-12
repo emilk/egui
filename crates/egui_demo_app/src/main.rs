bd5b54394eee92f401506c746a76e3af39d9f9ef
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-12-12 12:11:47 +0100

    egui_demo_app: silence wgpu log spam

--- crates/egui_demo_app/src/main.rs
+++ crates/egui_demo_app/src/main.rs
@@ -4,6 +4,17 @@
 
 // When compiling natively:
 fn main() -> Result<(), eframe::EframeError> {
+    {
+        // Silence wgpu log spam (https://github.com/gfx-rs/wgpu/issues/3206)
+        let mut rust_log = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".to_owned());
+        for loud_crate in ["naga", "wgpu_core", "wgpu_hal"] {
+            if !rust_log.contains(&format!("{loud_crate}=")) {
+                rust_log += &format!(",{loud_crate}=warn");
+            }
+        }
+        std::env::set_var("RUST_LOG", rust_log);
+    }
+
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
