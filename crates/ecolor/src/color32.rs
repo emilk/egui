ec31720a9b333e135c9944ab034273d1e054183d
Andreas Reich <andreas@rerun.io>
2023-02-04 10:29:30 +0100

    Color32 comment fix
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- crates/ecolor/src/color32.rs
+++ crates/ecolor/src/color32.rs
@@ -199,12 +199,12 @@ impl Color32 {
         Rgba::from(self).multiply(factor).into()
     }
 
-    /// Converts to floating point values in the range 0-1 without any color space conversion.
+    /// Converts to floating point values in the range 0-1 without any gamma space conversion.
     ///
     /// Use this with great care! In almost all cases, you want to convert to [`crate::Rgba`] instead
     /// in order to obtain linear space color values.
     #[inline]
-    pub fn to_srgba_f32(self) -> [f32; 4] {
+    pub fn to_normalized_gamma_f32(self) -> [f32; 4] {
         let Self([r, g, b, a]) = self;
         [
             r as f32 / 255.0,
