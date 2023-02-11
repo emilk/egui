734efa01470d0e7d2e0549cc3d70b42387869413
Ygor Souza <ygor.souza@protonmail.com>
2023-02-11 11:53:01 +0100

    Add methods to zoom the plot manually
    So you can bind different keyboard/mouse gestures to it, lock the X or Y
    axis, customize the zoom speed etc.

--- crates/egui_plot/src/lib.rs
+++ crates/egui_plot/src/lib.rs
@@ -991,6 +991,10 @@ impl Plot {
                     auto_bounds = false.into();
                 }
                 BoundsModification::AutoBounds(new_auto_bounds) => auto_bounds = new_auto_bounds,
+                BoundsModification::Zoom(zoom_factor, center) => {
+                    bounds.zoom(zoom_factor, center);
+                    auto_bounds = false.into();
+                }
             }
         }
 
@@ -1330,6 +1334,7 @@ enum BoundsModification {
     Set(PlotBounds),
     Translate(Vec2),
     AutoBounds(Vec2b),
+    Zoom(Vec2, PlotPoint),
 }
 
 /// Provides methods to interact with a plot while building it. It is the single argument of the closure
@@ -1391,6 +1396,27 @@ impl PlotUi {
     /// Can be used to check if the plot was hovered or clicked.
     pub fn response(&self) -> &Response {
         &self.response
+    }
+
+    /// Scale the plot bounds around a position in screen coordinates. Can be useful for
+    /// implementing alternative plot navigation methods.
+    /// The plot bounds are divided by `zoom_factor`, therefore:
+    /// `zoom_factor < 1.0` zooms out, i.e., increases the visible range to show more data.
+    /// `zoom_factor > 1.0` zooms in, i.e., reduces the visible range to show more detail.
+    pub fn zoom_bounds(&mut self, zoom_factor: Vec2, center: PlotPoint) {
+        self.bounds_modifications
+            .push(BoundsModification::Zoom(zoom_factor, center));
+    }
+
+    /// Scale the plot bounds around the hovered position, if any. Can be useful for implementing
+    /// alternative plot navigation methods.
+    /// The plot bounds are divided by `zoom_factor`, therefore:
+    /// `zoom_factor < 1.0` zooms out, i.e., increases the visible range to show more data.
+    /// `zoom_factor > 1.0` zooms in, i.e., reduces the visible range to show more detail.
+    pub fn zoom_bounds_around_hovered(&mut self, zoom_factor: Vec2) {
+        if let Some(hover_pos) = self.pointer_coordinate() {
+            self.zoom_bounds(zoom_factor, hover_pos);
+        }
     }
 
     /// The pointer position in plot coordinates. Independent of whether the pointer is in the plot area.
