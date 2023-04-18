00c9e0f5e4a66c54128d2902cc9f5cda157dcab1
Barugon <barugon@dungeonbox.net>
2023-04-18 09:53:38 -0700

    Make update while editing an option

--- crates/egui/src/widgets/drag_value.rs
+++ crates/egui/src/widgets/drag_value.rs
@@ -63,6 +63,7 @@ pub struct DragValue<'a> {
     max_decimals: Option<usize>,
     custom_formatter: Option<NumFormatter<'a>>,
     custom_parser: Option<NumParser<'a>>,
+    update_while_editing: bool,
 }
 
 impl<'a> DragValue<'a> {
@@ -94,6 +95,7 @@ impl<'a> DragValue<'a> {
             max_decimals: None,
             custom_formatter: None,
             custom_parser: None,
+            update_while_editing: true,
         }
     }
 
@@ -352,6 +354,12 @@ impl<'a> DragValue<'a> {
         }
         .custom_parser(|s| i64::from_str_radix(s, 16).map(|n| n as f64).ok())
     }
+
+    /// Update the value when there are changes while editing. (Default: true)
+    pub fn update_while_editing(mut self, update: bool) -> Self {
+        self.update_while_editing = update;
+        self
+    }
 }
 
 impl<'a> Widget for DragValue<'a> {
@@ -366,6 +374,7 @@ impl<'a> Widget for DragValue<'a> {
             max_decimals,
             custom_formatter,
             custom_parser,
+            update_while_editing,
         } = self;
 
         let shift = ui.input(|i| i.modifiers.shift_only());
@@ -475,9 +484,15 @@ impl<'a> Widget for DragValue<'a> {
                     .desired_width(ui.spacing().interact_size.x)
                     .font(text_style),
             );
-            // Only update when the edit content has changed. NOT every frame.
-            // See https://github.com/emilk/egui/issues/2687
-            if response.changed() {
+
+            let update = if update_while_editing {
+                // Update when the edit content has changed.
+                response.changed()
+            } else {
+                // Update only when the edit has lost focus.
+                response.lost_focus()
+            };
+            if update {
                 let parsed_value = match custom_parser {
                     Some(parser) => parser(&value_text),
                     None => value_text.parse().ok(),
