3d6a15f442929ae14b942e2d5248f236e7ef9b2f
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-04-27 09:45:44 +0200

    Log warning instead of error when failing to decode RON in storage (#2961)
    * Log warning instead of error when failing to decode RON in storage
    
    * New web demo
    
    * Clean up some warn/error logging
    
    * Avoid deadlock that could happen on crash
    
    * Log errors using console.warn, because console.error can cause crashes
    
    * Use patched version of wasm-bindgen-cli, allowing >2GB memory
    
    * New web demo

--- scripts/setup_web.sh
+++ scripts/setup_web.sh
@@ -5,4 +5,9 @@ cd "$script_path/.."
 
 # Pre-requisites:
 rustup target add wasm32-unknown-unknown
-cargo install wasm-bindgen-cli --version 0.2.84
+
+# For generating JS bindings:
+# cargo install wasm-bindgen-cli --version 0.2.84
+# We use a patched version containing this critical fix: https://github.com/rustwasm/wasm-bindgen/pull/3310
+# See https://github.com/rerun-io/wasm-bindgen/commits/0.2.84-patch
+cargo install wasm-bindgen-cli --git https://github.com/rerun-io/wasm-bindgen.git --rev 13283975ddf48c2d90758095e235b28d381c5762
