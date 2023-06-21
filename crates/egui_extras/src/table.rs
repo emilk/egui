64a0646c591ad1c1e28a3c589efe5cd0e1581319
KYovchevski <kyovchevski@gmail.com>
2023-06-21 10:12:44 +0200

    Add reference to ScrollArea::drag_to_scroll

--- crates/egui_extras/src/table.rs
+++ crates/egui_extras/src/table.rs
@@ -273,7 +273,9 @@ impl<'a> TableBuilder<'a> {
         self.vscroll(vscroll)
     }
 
-    /// Enables scrolling the table's contents using mouse drag (default: `true`)
+    /// Enables scrolling the table's contents using mouse drag (default: `true`).
+    ///
+    /// See [`ScrollArea::drag_to_scroll`] for more.
     pub fn drag_to_scroll(mut self, drag_to_scroll: bool) -> Self {
         self.scroll_options.drag_to_scroll = drag_to_scroll;
         self
