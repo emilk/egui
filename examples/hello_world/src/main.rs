35e537478ba1e34392ecddc474bcd945df4f66ff
Timon <timonpost@hotmail.nl>
2023-08-28 16:52:36 +0200

    undo example

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -17,44 +17,32 @@ fn main() -> Result<(), eframe::Error> {
 
 struct MyApp {
     name: String,
-    name2: String,
     age: u32,
-    age2: u32,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
             name: "Arthur".to_owned(),
-            name2: "Afadsf".to_owned(),
             age: 42,
-            age2: 42,
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        ctx.set_debug_on_hover(true);
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("My egui Application");
-
-            ui.vertical(|ui| {
-                ui.horizontal(|ui| {
-                    ui.text_edit_singleline(&mut self.name);
-                    ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age 1"));
-                    if ui.button("First button").clicked() {
-                        self.age += 1;
-                    }
-                });
-                ui.horizontal(|ui| {
-                    ui.text_edit_singleline(&mut self.name2);
-                    ui.add(egui::Slider::new(&mut self.age2, 0..=120).text("age 2"));
-                    if ui.button("Second button").clicked() {
-                        self.age += 1;
-                    }
-                })
-            })
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
         });
     }
 }
