015dc842123546448bdd0afd90d6c32b1477e3db
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-09-04 14:47:41 +0200

    refactor ehttp loader response to bytes mapping

--- crates/egui_extras/src/loaders/ehttp_loader.rs
+++ crates/egui_extras/src/loaders/ehttp_loader.rs
@@ -11,10 +11,41 @@ fn starts_with_one_of(s: &str, prefixes: &[&str]) -> bool {
     prefixes.iter().any(|prefix| s.starts_with(prefix))
 }
 
+fn get_image_bytes(
+    uri: &str,
+    response: Result<ehttp::Response, String>,
+) -> Result<Arc<[u8]>, String> {
+    let response = response?;
+    if !response.ok {
+        match response.text() {
+            Some(response_text) => {
+                return Err(format!(
+                    "failed to load `{uri}`: {} {} {response_text}",
+                    response.status, response.status_text
+                ))
+            }
+            None => {
+                return Err(format!(
+                    "failed to load `{uri}`: {} {}",
+                    response.status, response.status_text
+                ))
+            }
+        }
+    }
+
+    let Some(content_type) = response.content_type() else {
+    return Err(format!("failed to load `{uri}`: no content-type header found"));
+  };
+    if !content_type.starts_with("image/") {
+        return Err(format!("failed to load `{uri}`: expected content-type starting with \"image/\", found {content_type:?}"));
+    }
+
+    Ok(response.bytes.into())
+}
+
 impl BytesLoader for EhttpLoader {
     fn load(&self, ctx: &egui::Context, uri: &str) -> BytesLoadResult {
         if !starts_with_one_of(uri, PROTOCOLS) {
-            crate::log_trace!("cannot load `{uri}`, not supported");
             return Err(LoadError::NotSupported);
         }
 
@@ -35,21 +66,8 @@ impl BytesLoader for EhttpLoader {
             ehttp::fetch(ehttp::Request::get(uri.clone()), {
                 let ctx = ctx.clone();
                 let cache = self.cache.clone();
-                move |result| {
-                    let result = match result {
-                        Ok(response) if response.ok => Ok(response.bytes.into()),
-                        Ok(response) => match response.text() {
-                            Some(response_text) => Err(format!(
-                                "failed to get `{uri}`: {} {} {response_text}",
-                                response.status, response.status_text
-                            )),
-                            None => Err(format!(
-                                "failed to get `{uri}`: {} {}",
-                                response.status, response.status_text
-                            )),
-                        },
-                        Err(err) => Err(err),
-                    };
+                move |response| {
+                    let result = get_image_bytes(&uri, response);
                     crate::log_trace!("finished loading `{uri}`");
                     let prev = cache.lock().insert(uri, Poll::Ready(result));
                     assert!(matches!(prev, Some(Poll::Pending)));
