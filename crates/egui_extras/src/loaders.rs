08cb30b78671cdca5e13b599988c4b9a21140b90
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-09-04 23:37:18 +0200

    add some docs to `egui_extras::loaders::install`

--- crates/egui_extras/src/loaders.rs
+++ crates/egui_extras/src/loaders.rs
@@ -1,5 +1,15 @@
 // TODO: automatic cache eviction
 
+/// Install the default set of loaders:
+/// - `file` loader on non-Wasm targets
+/// - `http` loader with the `ehttp` feature
+/// - `image` loader with the `image` feature
+///   - the supported set of image formats is configured by enabling `image` crate features.
+/// - `svg` loader with the `svg` feature
+///
+/// The `file` and `http` loaders are bytes loaders, they do not know how to turn those
+/// bytes into an image. `svg` is for loading `.svg` files, and `image` can load any
+/// other image format enabled on the `image` crate.
 pub fn install(ctx: &egui::Context) {
     #[cfg(not(target_arch = "wasm32"))]
     ctx.add_bytes_loader(std::sync::Arc::new(self::file_loader::FileLoader::default()));
