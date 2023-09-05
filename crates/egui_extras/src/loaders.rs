76b5e745e93aeffd17928adc3f995f93070839d1
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-09-05 23:05:13 +0200

    explain how to enable image formats

--- crates/egui_extras/src/loaders.rs
+++ crates/egui_extras/src/loaders.rs
@@ -4,13 +4,17 @@
 /// - `file` loader on non-Wasm targets
 /// - `http` loader (with the `ehttp` feature)
 /// - `image` loader (with the `image` feature)
-///   - ⚠ the supported set of image formats is configured by adding the
-///     [`image`](https://crates.io/crates/image) crate as your direct dependency,
-///     and enabling features on it.
 /// - `svg` loader with the `svg` feature
 ///
 /// ⚠ This will do nothing and you won't see any images unless you enable some features!
 /// If you just want to be able to load `file://` and `http://` images, enable the `all-loaders` feature.
+///
+/// ⚠ The supported set of image formats is configured by adding the [`image`](https://crates.io/crates/image)
+/// crate as your direct dependency, and enabling features on it:
+///
+/// ```toml,ignore
+/// image = { version = "0.24", features = ["jpeg", "png"] }
+/// ```
 ///
 /// See [`egui::load`] for more information about how loaders work.
 pub fn install(ctx: &egui::Context) {
