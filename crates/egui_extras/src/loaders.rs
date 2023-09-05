ef71ea28985577562e72e6e597ae2a50bb757f7c
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-09-05 23:02:20 +0200

    clarify `egui_extras::loaders::install` behavior

--- crates/egui_extras/src/loaders.rs
+++ crates/egui_extras/src/loaders.rs
@@ -1,15 +1,18 @@
 // TODO: automatic cache eviction
 
-/// Install the default set of loaders:
+/// Installs the default set of loaders:
 /// - `file` loader on non-Wasm targets
-/// - `http` loader with the `ehttp` feature
-/// - `image` loader with the `image` feature
-///   - the supported set of image formats is configured by enabling `image` crate features.
+/// - `http` loader (with the `ehttp` feature)
+/// - `image` loader (with the `image` feature)
+///   - ⚠ the supported set of image formats is configured by adding the
+///     [`image`](https://crates.io/crates/image) crate as your direct dependency,
+///     and enabling features on it.
 /// - `svg` loader with the `svg` feature
 ///
-/// The `file` and `http` loaders are bytes loaders, they do not know how to turn those
-/// bytes into an image. `svg` is for loading `.svg` files, and `image` can load any
-/// other image format enabled on the `image` crate.
+/// ⚠ This will do nothing and you won't see any images unless you enable some features!
+/// If you just want to be able to load `file://` and `http://` images, enable the `all-loaders` feature.
+///
+/// See [`egui::load`] for more information about how loaders work.
 pub fn install(ctx: &egui::Context) {
     #[cfg(not(target_arch = "wasm32"))]
     ctx.add_bytes_loader(std::sync::Arc::new(self::file_loader::FileLoader::default()));
