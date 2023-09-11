269621a6a6b7ec5f13eaada7f9193e527f6b5588
Jan Procházka <1665677+jprochazk@users.noreply.github.com>
2023-09-11 10:22:10 +0200

    Update crates/egui/src/load.rs
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- crates/egui/src/load.rs
+++ crates/egui/src/load.rs
@@ -293,7 +293,7 @@ pub trait ImageLoader {
 }
 
 /// A texture with a known size.
-#[derive(Debug, Clone)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct SizedTexture {
     pub id: TextureId,
     pub size: Vec2,
