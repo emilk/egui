a966717735830dc5836e259d29c6b302d15e1306
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-09-28 11:38:25 +0200

    Explain why image_viewer is not part of the official web demo app

--- scripts/build_demo_web.sh
+++ scripts/build_demo_web.sh
@@ -12,8 +12,7 @@ export RUSTFLAGS=--cfg=web_sys_unstable_apis
 
 CRATE_NAME="egui_demo_app"
 
- # NOTE: persistence use up about 400kB (10%) of the WASM!
-FEATURES="http,persistence,web_screen_reader"
+FEATURES="web_app"
 
 OPEN=false
 OPTIMIZE=false
