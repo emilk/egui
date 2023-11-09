8de37bafb9d1fb96659518942991c4bddab4737d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-11-09 11:37:38 +0100

    Fix doclink

--- crates/egui/src/containers/scroll_area.rs
+++ crates/egui/src/containers/scroll_area.rs
@@ -411,7 +411,7 @@ struct Prepared {
     /// Does this `ScrollArea` have horizontal/vertical scrolling enabled?
     scroll_enabled: [bool; 2],
 
-    /// Like [`Self::show_bars`], but smoothly interpolated
+    /// Smoothly interpolated boolean of wether or not to show the scroll bars.
     show_bars_factor: Vec2,
 
     /// How much horizontal and vertical space are used up by the
