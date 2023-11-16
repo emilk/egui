94edb9d5e0616fa7148270e2bb09ff6b9b44829e
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-11-16 16:44:30 +0100

    Rename `docs/` to `web_demo/`

--- scripts/wasm_size.sh
+++ scripts/wasm_size.sh
@@ -3,4 +3,4 @@ set -eu
 script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$script_path"
 
-./build_demo_web.sh && ls -lh ../docs/*.wasm
+./build_demo_web.sh && ls -lh ../web_demo/*.wasm
