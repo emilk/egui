ba37733d44e22f2c0da19feb30afb8bf2afd172c
coryrobertson <cory.robertson17@gmail.com>
2023-11-21 17:21:59 -0800

    Updated clear_color(&Visuals) to use the given visuals window_fill as thought of in comment

--- crates/eframe/src/epi.rs
+++ crates/eframe/src/epi.rs
@@ -198,13 +198,13 @@ pub trait App {
     /// egui recommends that rendering backends use a normal "gamma-space" (non-sRGB-aware) blending,
     ///  which means the values you return here should also be in `sRGB` gamma-space in the 0-1 range.
     /// You can use [`egui::Color32::to_normalized_gamma_f32`] for this.
-    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+    fn clear_color(&self, visuals: &egui::Visuals) -> [f32; 4] {
         // NOTE: a bright gray makes the shadows of the windows look weird.
         // We use a bit of transparency so that if the user switches on the
         // `transparent()` option they get immediate results.
-        egui::Color32::from_rgba_unmultiplied(12, 12, 12, 180).to_normalized_gamma_f32()
+        // egui::Color32::from_rgba_unmultiplied(12, 12, 12, 180).to_normalized_gamma_f32()
 
-        // _visuals.window_fill() would also be a natural choice
+        visuals.window_fill.to_normalized_gamma_f32()
     }
 
     /// Controls whether or not the egui memory (window positions etc) will be
