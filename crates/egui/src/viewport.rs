9a60802610d00dcb5b04ece276860b62549c8a17
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-11-23 10:21:25 +0100

    Silence warning

--- crates/egui/src/viewport.rs
+++ crates/egui/src/viewport.rs
@@ -1006,7 +1006,7 @@ impl ViewportOutput {
 
         self.parent = parent;
         self.class = class;
-        self.builder.patch(builder);
+        let _ = self.builder.patch(builder); // we ignore the returned command, because `self.builder` will be the basis of a new patch
         self.viewport_ui_cb = viewport_ui_cb;
         self.commands.append(&mut commands);
         self.repaint_delay = self.repaint_delay.min(repaint_delay);
