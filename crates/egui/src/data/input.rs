3361d399fb2adf9c486a9a1c0a1ff73114c9b23d
zeozeozeo <108888572+zeozeozeo@users.noreply.github.com>
2023-12-18 13:48:16 +0500

    derive serde Serialize and Deserialize on `KeyboardShortcut` (#3694)
    <!--
    Please read the "Making a PR" section of
    [`CONTRIBUTING.md`](https://github.com/emilk/egui/blob/master/CONTRIBUTING.md)
    before opening a Pull Request!
    
    * Keep your PR:s small and focused.
    * If applicable, add a screenshot or gif.
    * If it is a non-trivial addition, consider adding a demo for it to
    `egui_demo_lib`, or a new example.
    * Do NOT open PR:s from your `master` branch, as that makes it hard for
    maintainers to add commits to your PR.
    * Remember to run `cargo fmt` and `cargo cranky`.
    * Open the PR as a draft until you have self-reviewed it and run
    `./scripts/check.sh`.
    * When you have addressed a PR comment, mark it as resolved.
    
    Please be patient! I will review your PR, but my time is limited!
    -->
    
    This is a very simple change so you don't have to do this:
    https://github.com/zeozeozeo/egui-keybind/blob/master/src/bind.rs#L149-L172

--- crates/egui/src/data/input.rs
+++ crates/egui/src/data/input.rs
@@ -1060,6 +1060,7 @@ impl Key {
 /// Can be used with [`crate::InputState::consume_shortcut`]
 /// and [`crate::Context::format_shortcut`].
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct KeyboardShortcut {
     pub modifiers: Modifiers,
     pub key: Key,
