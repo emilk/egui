980a54a46a7e6f2513f215dc8e1279b62fe9f61e
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-01-22 14:19:40 +0900

    Update context.rs

--- crates/egui/src/context.rs
+++ crates/egui/src/context.rs
@@ -1240,9 +1240,6 @@ impl Context {
     ///
     /// This will repaint the current viewport.
     pub fn request_repaint(&self) {
-        if self.input(|i| i.viewport().minimized.unwrap_or(false)) {
-            return;
-        }
         self.request_repaint_of(self.viewport_id());
     }
 
