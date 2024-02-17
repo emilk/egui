b850cf86a0435e553d7f9d283160f71f0478bef9
vincent-sparks <155431337+vincent-sparks@users.noreply.github.com>
2024-02-17 14:53:25 -0800

    Switch to using RectShape unconditionally for the backdrop
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- crates/egui/src/widgets/image.rs
+++ crates/egui/src/widgets/image.rs
@@ -739,17 +739,7 @@ pub fn paint_texture_at(
     texture: &SizedTexture,
 ) {
     if options.bg_fill != Default::default() {
-        // NOTE(vincent-sparks) originally this was only done with a mesh;
-        // however this created a bug if the image had rounding
-        // I assume the mesh was used in place of the rect for performance reasons
-        // so I have left it in
-        if options.rounding == Rounding::ZERO {
-            let mut mesh = Mesh::default();
-            mesh.add_colored_rect(rect, options.bg_fill);
-            painter.add(Shape::mesh(mesh));
-        } else {
-            painter.add(RectShape::new(rect, options.rounding, options.bg_fill, Stroke::NONE));
-        }
+        painter.add(RectShape::filled(rect, options.rounding, options.bg_fill));
     }
 
     match options.rotation {
