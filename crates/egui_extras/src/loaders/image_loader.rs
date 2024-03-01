dfe49fc1c6929759faede80f869ace24c0418cdb
Varphone Wong <varphone@qq.com>
2024-03-01 14:35:14 +0800

    Apply size hint to `ImageCrateLoader::load`

--- crates/egui_extras/src/loaders/image_loader.rs
+++ crates/egui_extras/src/loaders/image_loader.rs
@@ -53,7 +53,7 @@ impl ImageLoader for ImageCrateLoader {
         Self::ID
     }
 
-    fn load(&self, ctx: &egui::Context, uri: &str, _: SizeHint) -> ImageLoadResult {
+    fn load(&self, ctx: &egui::Context, uri: &str, hint: SizeHint) -> ImageLoadResult {
         // three stages of guessing if we support loading the image:
         // 1. URI extension (only done for files)
         // 2. Mime from `BytesPoll::Ready`
@@ -95,7 +95,8 @@ impl ImageLoader for ImageCrateLoader {
 
                     // (3)
                     log::trace!("started loading {uri:?}");
-                    let result = crate::image::load_image_bytes(&bytes).map(Arc::new);
+                    let result =
+                        crate::image::load_image_bytes_for_size(&bytes, hint).map(Arc::new);
                     log::trace!("finished loading {uri:?}");
                     cache.insert(uri.into(), result.clone());
                     result.map(|image| ImagePoll::Ready { image })
