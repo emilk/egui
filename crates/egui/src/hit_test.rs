emilk/egui

commit ca8c8792c2a6a65656105b971998f937edcb836d

New widget interaction logic (#4026)
* Closes https://github.com/emilk/egui/issues/3936
* Closes https://github.com/emilk/egui/issues/3923
* Closes https://github.com/emilk/egui/pull/4058

The interaction code is now done at the start of the frame, using stored
`WidgetRect`s from the previous frame.

The intention is that the new interaction code should be more accurate,
making it easier to hit widgets, and better respecting the rules of
overlapping widgets.

There is a new `style::Interaction::interact_radius` controlling how far
away from a widget the cursor can be and still hit it. This helps big
fat fingers hit small widgets on touch screens.

This PR adds a new `Context::read_response` which lets you read the
`Response` of a `Widget` _before_ you create the widget. This can be
used for styling, or for reading the result of an interaction early (to
prevent frame-delay) for a widget you add late (so it is on top of other
widgets).

# ⚠️ BREAKING CHANGES
`Memory::dragged_id`, `Memory::set_dragged_id` etc have been moved to
`Context`.
The semantics for `Context::dragged_id` is slightly different: a widget
is not considered dragged until egui it is sure this is not a
click-in-progress. For a widget that is only sensitive to drags, that is
right away, but for widgets sensitive to both clicks and drags it is not
until the mouse has moved a certain distance.

# TODO
* [x] Fix panel resizing
* [x] Fix scroll hover weirdness
* [x] Fix Resize widget
* [x] Fix drag-and-drop
* [x] Test all of egui_demo_app
* [x] Change `is_dragging` API
* [x] Consistent naming of start/stop or begin/end drag
* [x] Test `egui_tiles`
* [x] Test Rerun
* [x] Document
* [x] Document breaking changes in PR description
* [x] Test one final time

# Saving for a later PR
* [ ] Fix https://github.com/emilk/egui/issues/4047
* [ ] Specify what the response order for e.g. `ui.horizontal` is

crates/egui/src/interaction.rs
@@ -1,0 +1,230 @@
+//! How mouse and touch interzcts with widgets.
+
+use crate::*;
+
+use self::{hit_test::WidgetHits, id::IdSet, input_state::PointerEvent, memory::InteractionState};
+
+/// Calculated at the start of each frame
+/// based on:
+/// * Widget rects from precious frame
+/// * Mouse/touch input
+/// * Current [`InteractionState`].
+#[derive(Clone, Default)]
+pub struct InteractionSnapshot {
+    /// The widget that got clicked this frame.
+    pub clicked: Option<Id>,
+
+    /// Drag started on this widget this frame.
+    ///
+    /// This will also be found in `dragged` this frame.
+    pub drag_started: Option<Id>,
+
+    /// This widget is being dragged this frame.
+    ///
+    /// Set the same frame a drag starts,
+    /// but unset the frame a drag ends.
+    ///
+    /// NOTE: this may not have a corresponding [`WidgetRect`],
+    /// if this for instance is a drag-and-drop widget which
+    /// isn't painted whilst being dragged
+    pub dragged: Option<Id>,
+
+    /// This widget was let go this frame,
+    /// after having been dragged.
+    ///
+    /// The widget will not be found in [`Self::dragged`] this frame.
+    pub drag_stopped: Option<Id>,
+
+    /// A small set of widgets (usually 0-1) that the pointer is hovering over.
+    ///
+    /// Show these widgets as highlighted, if they are interactive.
+    ///
+    /// While dragging or clicking something, nothing else is hovered.
+    ///
+    /// Use [`Self::contains_pointer`] to find a drop-zone for drag-and-drop.
+    pub hovered: IdSet,
+
+    /// All widgets that contain the pointer this frame,
+    /// regardless if the user is currently clicking or dragging.
+    ///
+    /// This is usually a larger set than [`Self::hovered`],
+    /// and can be used for e.g. drag-and-drop zones.
+    pub contains_pointer: IdSet,
+}
+
+impl InteractionSnapshot {
+    pub fn ui(&self, ui: &mut crate::Ui) {
+        let Self {
+            clicked,
+            drag_started,
+            dragged,
+            drag_stopped,
+            hovered,
+            contains_pointer,
+        } = self;
+
+        fn id_ui<'a>(ui: &mut crate::Ui, widgets: impl IntoIterator<Item = &'a Id>) {
+            for id in widgets {
+                ui.label(id.short_debug_format());
+            }
+        }
+
+        crate::Grid::new("interaction").show(ui, |ui| {
+            ui.label("clicked");
+            id_ui(ui, clicked);
+            ui.end_row();
+
+            ui.label("drag_started");
+            id_ui(ui, drag_started);
+            ui.end_row();
+
+            ui.label("dragged");
+            id_ui(ui, dragged);
+            ui.end_row();
+
+            ui.label("drag_stopped");
+            id_ui(ui, drag_stopped);
+            ui.end_row();
+
+            ui.label("hovered");
+            id_ui(ui, hovered);
+            ui.end_row();
+
+            ui.label("contains_pointer");
+            id_ui(ui, contains_pointer);
+            ui.end_row();
+        });
+    }
+}
+
+pub(crate) fn interact(
+    prev_snapshot: &InteractionSnapshot,
+    widgets: &WidgetRects,
+    hits: &WidgetHits,
+    input: &InputState,
+    interaction: &mut InteractionState,
+) -> InteractionSnapshot {
+    crate::profile_function!();
+
+    if let Some(id) = interaction.potential_click_id {
+        if !widgets.by_id.contains_key(&id) {
+            // The widget we were interested in clicking is gone.
+            interaction.potential_click_id = None;
+        }
+    }
+    if let Some(id) = interaction.potential_drag_id {
+        if !widgets.by_id.contains_key(&id) {
+            // The widget we were interested in dragging is gone.
+            // This is fine! This could be drag-and-drop,
+            // and the widget being dragged is now "in the air" and thus
+            // not registered in the new frame.
+        }
+    }
+
+    let mut clicked = None;
+    let mut dragged = prev_snapshot.dragged;
+
+    // Note: in the current code a press-release in the same frame is NOT considered a drag.
+    for pointer_event in &input.pointer.pointer_events {
+        match pointer_event {
+            PointerEvent::Moved(_) => {}
+
+            PointerEvent::Pressed { .. } => {
+                // Maybe new click?
+                if interaction.potential_click_id.is_none() {
+                    interaction.potential_click_id = hits.click.map(|w| w.id);
+                }
+
+                // Maybe new drag?
+                if interaction.potential_drag_id.is_none() {
+                    interaction.potential_drag_id = hits.drag.map(|w| w.id);
+                }
+            }
+
+            PointerEvent::Released { click, button: _ } => {
+                if click.is_some() {
+                    if let Some(widget) = interaction
+                        .potential_click_id
+                        .and_then(|id| widgets.by_id.get(&id))
+                    {
+                        clicked = Some(widget.id);
+                    }
+                }
+
+                interaction.potential_drag_id = None;
+                interaction.potential_click_id = None;
+                dragged = None;
+            }
+        }
+    }
+
+    if dragged.is_none() {
+        // Check if we started dragging something new:
+        if let Some(widget) = interaction
+            .potential_drag_id
+            .and_then(|id| widgets.by_id.get(&id))
+        {
+            let is_dragged = if widget.sense.click && widget.sense.drag {
+                // This widget is sensitive to both clicks and drags.
+                // When the mouse first is pressed, it could be either,
+                // so we postpone the decision until we know.
+                input.pointer.is_decidedly_dragging()
+            } else {
+                // This widget is just sensitive to drags, so we can mark it as dragged right away:
+                widget.sense.drag
+            };
+
+            if is_dragged {
+                dragged = Some(widget.id);
+            }
+        }
+    }
+
+    // ------------------------------------------------------------------------
+
+    let drag_changed = dragged != prev_snapshot.dragged;
+    let drag_stopped = drag_changed.then_some(prev_snapshot.dragged).flatten();
+    let drag_started = drag_changed.then_some(dragged).flatten();
+
+    // if let Some(drag_started) = drag_started {
+    //     eprintln!(
+    //         "Started dragging {} {:?}",
+    //         drag_started.id.short_debug_format(),
+    //         drag_started.rect
+    //     );
+    // }
+
+    let contains_pointer: IdSet = hits
+        .contains_pointer
+        .iter()
+        .chain(&hits.click)
+        .chain(&hits.drag)
+        .map(|w| w.id)
+        .collect();
+
+    let hovered = if clicked.is_some() || dragged.is_some() {
+        // If currently clicking or dragging, nothing else is hovered.
+        clicked.iter().chain(&dragged).copied().collect()
+    } else if hits.click.is_some() || hits.drag.is_some() {
+        // We are hovering over an interactive widget or two.
+        hits.click.iter().chain(&hits.drag).map(|w| w.id).collect()
+    } else {
+        // Whatever is topmost is what we are hovering.
+        // TODO: consider handle hovering over multiple top-most widgets?
+        // TODO: allow hovering close widgets?
+        hits.contains_pointer
+            .last()
+            .map(|w| w.id)
+            .into_iter()
+            .collect()
+    };
+
+    InteractionSnapshot {
+        clicked,
+        drag_started,
+        dragged,
+        drag_stopped,
+        contains_pointer,
+        hovered,
+    }
+}

crates/egui/src/response.rs
@@ -2,7 +2,7 @@ use std::{any::Any, sync::Arc};
 
 use crate::{
     emath::{Align, Pos2, Rect, Vec2},
-    menu, Context, CursorIcon, Id, LayerId, PointerButton, Sense, Ui, WidgetText,
+    menu, Context, CursorIcon, Id, LayerId, PointerButton, Sense, Ui, WidgetRect, WidgetText,
     NUM_POINTER_BUTTONS,
 };
 
@@ -85,7 +85,7 @@ pub struct Response {
 
     /// The widget was being dragged, but now it has been released.
     #[doc(hidden)]
-    pub drag_released: bool,
+    pub drag_stopped: bool,
 
     /// Is the pointer button currently down on this widget?
     /// This is true if the pointer is pressing down or dragging a widget
@@ -317,13 +317,26 @@ impl Response {
 
     /// The widget was being dragged, but now it has been released.
     #[inline]
-    pub fn drag_released(&self) -> bool {
-        self.drag_released
+    pub fn drag_stopped(&self) -> bool {
+        self.drag_stopped
     }
 
     /// The widget was being dragged by the button, but now it has been released.
+    pub fn drag_stopped_by(&self, button: PointerButton) -> bool {
+        self.drag_stopped() && self.ctx.input(|i| i.pointer.button_released(button))
+    }
+
+    /// The widget was being dragged, but now it has been released.
+    #[inline]
+    #[deprecated = "Renamed 'dragged_stopped'"]
+    pub fn drag_released(&self) -> bool {
+        self.drag_stopped
+    }
+
+    /// The widget was being dragged by the button, but now it has been released.
+    #[deprecated = "Renamed 'dragged_stopped_by'"]
     pub fn drag_released_by(&self, button: PointerButton) -> bool {
-        self.drag_released() && self.ctx.input(|i| i.pointer.button_released(button))
+        self.drag_stopped_by(button)
     }
 
     /// If dragged, how many points were we dragged and in what direction?
@@ -609,37 +622,45 @@ impl Response {
         self
     }
 
-    /// Check for more interactions (e.g. sense clicks on a [`Response`] returned from a label).
+    /// Sense more interactions (e.g. sense clicks on a [`Response`] returned from a label).
+    ///
+    /// The interaction will occur on the same plane as the original widget,
+    /// i.e. if the response was from a widget behind button, the interaction will also be behind that button.
+    /// egui gives priority to the _last_ added widget (the one on top gets clicked first).
     ///
     /// Note that this call will not add any hover-effects to the widget, so when possible
     /// it is better to give the widget a [`Sense`] instead, e.g. using [`crate::Label::sense`].
     ///
+    /// Using this method on a `Response` that is the result of calling `union` on multiple `Response`s
+    /// is undefined behavior.
+    ///
     /// ```
     /// # egui::__run_test_ui(|ui| {
-    /// let response = ui.label("hello");
-    /// assert!(!response.clicked()); // labels don't sense clicks by default
-    /// let response = response.interact(egui::Sense::click());
-    /// if response.clicked() { /* … */ }
+    /// let horiz_response = ui.horizontal(|ui| {
+    ///     ui.label("hello");
+    /// }).response;
+    /// assert!(!horiz_response.clicked()); // ui's don't sense clicks by default
+    /// let horiz_response = horiz_response.interact(egui::Sense::click());
+    /// if horiz_response.clicked() {
+    ///     // The background behind the label was clicked
+    /// }
     /// # });
     /// ```
     #[must_use]
     pub fn interact(&self, sense: Sense) -> Self {
-        // Test if we must sense something new compared to what we have already sensed. If not, then
-        // we can return early. This may avoid unnecessarily "masking" some widgets with unneeded
-        // interactions.
         if (self.sense | sense) == self.sense {
+            // Early-out: we already sense everything we need to sense.
             return self.clone();
         }
 
-        self.ctx.interact_with_hovered(
-            self.layer_id,
-            self.id,
-            self.rect,
-            self.interact_rect,
+        self.ctx.create_widget(WidgetRect {
+            layer_id: self.layer_id,
+            id: self.id,
+            rect: self.rect,
+            interact_rect: self.interact_rect,
             sense,
-            self.enabled,
-            self.contains_pointer,
-        )
+            enabled: self.enabled,
+        })
     }
 
     /// Adjust the scroll position until this UI becomes visible.
@@ -843,6 +864,8 @@ impl Response {
     /// For instance `a.union(b).hovered` means "was either a or b hovered?".
     ///
     /// The resulting [`Self::id`] will come from the first (`self`) argument.
+    ///
+    /// You may not call [`Self::interact`] on the resulting `Response`.
     pub fn union(&self, other: Self) -> Self {
         assert!(self.ctx == other.ctx);
         crate::egui_assert!(
@@ -883,7 +906,7 @@ impl Response {
             ],
             drag_started: self.drag_started || other.drag_started,
             dragged: self.dragged || other.dragged,
-            drag_released: self.drag_released || other.drag_released,
+            drag_stopped: self.drag_stopped || other.drag_stopped,
             is_pointer_button_down_on: self.is_pointer_button_down_on
                 || other.is_pointer_button_down_on,
             interact_pointer_pos: self.interact_pointer_pos.or(other.interact_pointer_pos),
@@ -900,6 +923,8 @@ impl Response {
     }
 }
 
+/// See [`Response::union`].
+///
 /// To summarize the response from many widgets you can use this pattern:
 ///
 /// ```
@@ -918,6 +943,8 @@ impl std::ops::BitOr for Response {
     }
 }
 
+/// See [`Response::union`].
+///
 /// To summarize the response from many widgets you can use this pattern:
 ///
 /// ```

commit d0a6bbf2b880ae00dd3db1185591ba9c0ed51ed6

Fix style of disabled widgets (#4163)
* Broke in https://github.com/emilk/egui/pull/4026/files

The `Response::sense` for `enabled: false` widgets was wrong, leading to
the wrong widget style.

crates/egui/src/context.rs
@@ -1018,12 +1018,7 @@ impl Context {
     ///
     /// If the widget already exists, its state (sense, Rect, etc) will be updated.
     #[allow(clippy::too_many_arguments)]
-    pub(crate) fn create_widget(&self, mut w: WidgetRect) -> Response {
-        if !w.enabled {
-            w.sense.click = false;
-            w.sense.drag = false;
-        }
-
+    pub(crate) fn create_widget(&self, w: WidgetRect) -> Response {
         // Remember this widget
         self.write(|ctx| {
             let viewport = ctx.viewport();
@@ -1130,7 +1125,8 @@ impl Context {
             let input = &viewport.input;
             let memory = &mut ctx.memory;
 
-            if sense.click
+            if enabled
+                && sense.click
                 && memory.has_focus(id)
                 && (input.key_pressed(Key::Space) || input.key_pressed(Key::Enter))
             {
@@ -1139,7 +1135,10 @@ impl Context {
             }
 
             #[cfg(feature = "accesskit")]
-            if sense.click && input.has_accesskit_action_request(id, accesskit::Action::Default) {
+            if enabled
+                && sense.click
+                && input.has_accesskit_action_request(id, accesskit::Action::Default)
+            {
                 res.clicked[PointerButton::Primary as usize] = true;
             }
 
@@ -1159,7 +1158,7 @@ impl Context {
 
             for pointer_event in &input.pointer.pointer_events {
                 if let PointerEvent::Released { click, button } = pointer_event {
-                    if sense.click && clicked {
+                    if enabled && sense.click && clicked {
                         if let Some(click) = click {
                             res.clicked[*button as usize] = true;
                             res.double_clicked[*button as usize] = click.is_double();

crates/egui/src/interaction.rs
@@ -161,18 +161,20 @@ pub(crate) fn interact(
     if dragged.is_none() {
         // Check if we started dragging something new:
         if let Some(widget) = interaction.potential_drag_id.and_then(|id| widgets.get(id)) {
-            let is_dragged = if widget.sense.click && widget.sense.drag {
-                // This widget is sensitive to both clicks and drags.
-                // When the mouse first is pressed, it could be either,
-                // so we postpone the decision until we know.
-                input.pointer.is_decidedly_dragging()
-            } else {
-                // This widget is just sensitive to drags, so we can mark it as dragged right away:
-                widget.sense.drag
-            };
+            if widget.enabled {
+                let is_dragged = if widget.sense.click && widget.sense.drag {
+                    // This widget is sensitive to both clicks and drags.
+                    // When the mouse first is pressed, it could be either,
+                    // so we postpone the decision until we know.
+                    input.pointer.is_decidedly_dragging()
+                } else {
+                    // This widget is just sensitive to drags, so we can mark it as dragged right away:
+                    widget.sense.drag
+                };
 
-            if is_dragged {
-                dragged = Some(widget.id);
+                if is_dragged {
+                    dragged = Some(widget.id);
+                }
             }
         }
     }

crates/egui/src/response.rs
@@ -42,6 +42,13 @@ pub struct Response {
     pub interact_rect: Rect,
 
     /// The senses (click and/or drag) that the widget was interested in (if any).
+    ///
+    /// Note: if [`Self::enabled`] is `false`, then
+    /// the widget _effectively_ doesn't sense anything,
+    /// but can still have the same `Sense`.
+    /// This is because the sense informs the styling of the widget,
+    /// but we don't want to change the style when a widget is disabled
+    /// (that is handled by the `Painter` directly).
     pub sense: Sense,
 
     /// Was the widget enabled?

crates/egui/src/widget_rect.rs
@@ -29,6 +29,13 @@ pub struct WidgetRect {
     pub interact_rect: Rect,
 
     /// How the widget responds to interaction.
+    ///
+    /// Note: if [`Self::enabled`] is `false`, then
+    /// the widget _effectively_ doesn't sense anything,
+    /// but can still have the same `Sense`.
+    /// This is because the sense informs the styling of the widget,
+    /// but we don't want to change the style when a widget is disabled
+    /// (that is handled by the `Painter` directly).
     pub sense: Sense,
 
     /// Is the widget enabled?

crates/egui/src/hit_test.rs
@@ -78,6 +78,16 @@ pub fn hit_test(
         let top_layer = closest_hit.layer_id;
         close.retain(|w| w.layer_id == top_layer);
 
+        // If the widget is disabled, treat it as if it isn't sensing anything.
+        // This simplifies the code in `hit_test_on_close` so it doesn't have to check
+        // the `enabled` flag everywhere:
+        for w in &mut close {
+            if !w.enabled {
+                w.sense.click = false;
+                w.sense.drag = false;
+            }
+        }
+
         let pos_in_layer = pos_in_layers.get(&top_layer).copied().unwrap_or(pos);
         let hits = hit_test_on_close(&close, pos_in_layer);
 