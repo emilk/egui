7cc9f9bdb3ff53f2aa9f16824d0254249ef250de
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-03-21 00:29:26 +0900

    Update window.rs

--- crates/egui/src/containers/window.rs
+++ crates/egui/src/containers/window.rs
@@ -429,9 +429,9 @@ impl<'open> Window<'open> {
             (0.0, 0.0)
         };
 
-        let viewport_rect = ctx.input(|i| i.viewport().inner_rect.unwrap_or(Rect::EVERYTHING));
-        let max_width = viewport_rect.width();
-        let max_height = viewport_rect.height() - title_bar_height;
+        let max_rect = ctx.input(|i| i.viewport().inner_rect.unwrap_or(ctx.screen_rect()));
+        let max_width = max_rect.width();
+        let max_height = max_rect.height() - title_bar_height;
         resize.max_size.x = resize.max_size.x.min(max_width);
         resize.max_size.y = resize.max_size.y.min(max_height);
 
