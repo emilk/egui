d3c595fc75e89d3b7b5ba242197cbd06471c1c33
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-03-21 23:43:45 +0900

    Update visuals.rs

--- crates/egui/src/text_selection/visuals.rs
+++ crates/egui/src/text_selection/visuals.rs
@@ -51,21 +51,7 @@ pub fn paint_text_selection(
 }
 
 /// Paint one end of the selection, e.g. the primary cursor.
-pub fn paint_cursor(
-    painter: &Painter,
-    visuals: &Visuals,
-    cursor_rect: Rect,
-    i_time: f64,
-    blink: bool,
-) -> bool {
-    if blink {
-        // Make i_time_piece between 0 to 9.
-        let i_time_piece = ((i_time % 1.0) * 10.0).trunc() as i64;
-        if i_time_piece >= 5 && i_time_piece <= 8 {
-            return false;
-        }
-    }
-
+pub fn paint_cursor(painter: &Painter, visuals: &Visuals, cursor_rect: Rect) {
     let stroke = visuals.text_cursor;
 
     let top = cursor_rect.center_top();
@@ -86,6 +72,49 @@ pub fn paint_cursor(
             (width, stroke.color),
         );
     }
+}
 
-    true
+/// Paint text cursor.
+pub fn paint_text_cursor(
+    ui: &mut Ui,
+    painter: &Painter,
+    primary_cursor_rect: Rect,
+    is_stay_cursor: bool,
+) {
+    let i_time = ui.input(|i| i.time);
+    let blink_mode = ui.visuals().text_cursor_blink;
+    let is_blink_mode = blink_mode && is_stay_cursor;
+
+    let mut is_cursor_visible = true;
+
+    let on_duration = ui.visuals().text_cursor_on_duration;
+    let off_duration = ui.visuals().text_cursor_off_duratio;
+    let total_duration = on_duration + off_duration;
+
+    if is_blink_mode {
+        if (i_time % total_duration) < on_duration {
+            is_cursor_visible = true;
+        } else {
+            is_cursor_visible = false;
+        }
+    }
+
+    if is_cursor_visible {
+        paint_cursor(painter, ui.visuals(), primary_cursor_rect);
+    }
+
+    if is_blink_mode {
+        if is_cursor_visible {
+            ui.ctx()
+                .request_repaint_after(std::time::Duration::from_millis(
+                    (on_duration * 1000.0) as u64,
+                ));
+        }
+        if !is_cursor_visible {
+            ui.ctx()
+                .request_repaint_after(std::time::Duration::from_millis(
+                    (off_duration * 1000.0) as u64,
+                ));
+        }
+    }
 }
