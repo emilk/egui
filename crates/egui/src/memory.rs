1d4d54ba291a8e8c49bf2667f032543cae566e73
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-03-23 16:21:20 +0900

    Update memory.rs

--- crates/egui/src/memory.rs
+++ crates/egui/src/memory.rs
@@ -193,10 +193,6 @@ pub struct Options {
 
     /// If `true`, This will call `egui::Context::request_repaint()` at the end of each frame
     /// If `false` (default), egui is only updated if are input events (like mouse movements) or there are some animations in the GUI.
-    ///
-    /// ```no_run
-    ///     ui.ctx().options_mut(|options| options.continuous_mode = true);
-    /// ```
     pub continuous_mode: bool,
 
     /// If any widget moves or changes id, repaint everything.
