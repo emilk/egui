69b517c8223b6f2a3cec73e3e3340b70eb4f1b5e
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-03-29 21:36:34 +0900

    Update glow_integration.rs

--- crates/eframe/src/native/glow_integration.rs
+++ crates/eframe/src/native/glow_integration.rs
@@ -1052,7 +1052,7 @@ impl GlutinWindowContext {
             .get_mut(&viewport_id)
             .expect("viewport doesn't exist");
 
-        viewport.info.this = Some(viewport_id);
+        viewport.info.this = viewport_id;
         viewport.info.parent = Some(self.egui_ctx.parent_viewport_id_of(viewport_id));
 
         let window = if let Some(window) = &mut viewport.window {
