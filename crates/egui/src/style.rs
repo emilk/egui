55467b37e07dd2096a3f0d70e76ae9257b7e5df5
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-03-29 23:01:53 +0900

    Update style.rs

--- crates/egui/src/style.rs
+++ crates/egui/src/style.rs
@@ -1860,14 +1860,10 @@ impl Visuals {
         ui.collapsing("Misc", |ui| {
             ui.add(Slider::new(resize_corner_size, 0.0..=20.0).text("resize_corner_size"));
             ui.checkbox(text_cursor_preview, "Preview text cursor on hover");
-            ui.add(Slider::new(clip_rect_margin, 0.0..=20.0).text("clip_rect_margin"));
-
-            ui.add(Slider::new(resize_corner_size, 0.0..=20.0).text("resize_corner_size"));
-            ui.checkbox(text_cursor_preview, "Preview text cursor on hover");
             ui.checkbox(text_cursor_blink, "text cursor to blink");
             ui.add(Slider::new(text_cursor_on_duration, 0.0..=2.0).text("text cursor on duration"));
-            ui.add(
-                Slider::new(text_cursor_off_duration, 0.0..=2.0).text("text cursor off duration")
+            ui.add(Slider::new(
+                text_cursor_off_duration, 0.0..=2.0).text("text cursor off duration")
             );
             ui.add(Slider::new(clip_rect_margin, 0.0..=20.0).text("clip_rect_margin"));
 
