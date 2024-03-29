d098a8f0f06795df83d91d26b961f800fdab2b1e
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-03-30 02:03:50 +0900

    Update visuals.rs

--- crates/egui/src/text_selection/visuals.rs
+++ crates/egui/src/text_selection/visuals.rs
@@ -92,7 +92,15 @@ pub fn paint_text_cursor(
     let mut is_cursor_visible = true;
 
     if is_blink_mode {
+        // Check if the text cursor is on/off based on the time
         is_cursor_visible = (i_time % total_duration) < on_duration;
+    }
+
+    if blink_pause {
+        // Keep the text cursor visible for some time.
+        if i_time < ui.visuals().text_cursor_style.last_edit_time + 3.0 {
+            is_cursor_visible = true;
+        }
     }
 
     if is_cursor_visible {
