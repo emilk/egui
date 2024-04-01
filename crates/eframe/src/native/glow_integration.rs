4f5a9277b9d2b7a84611da6b82c7ff737c61898f
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-04-02 00:26:35 +0900

    Update glow_integration.rs

--- crates/eframe/src/native/glow_integration.rs
+++ crates/eframe/src/native/glow_integration.rs
@@ -1266,16 +1266,7 @@ impl GlutinWindowContext {
 
         if let Some(viewport) = self.viewports.get(&viewport_id) {
             if let Some(gl_surface) = &viewport.gl_surface {
-                // change_gl_context(&mut self.current_gl_context, gl_surface);
-                self.current_gl_context = Some(
-                    self.current_gl_context
-                        .take()
-                        .unwrap()
-                        .make_not_current()
-                        .unwrap()
-                        .make_current(gl_surface)
-                        .unwrap(),
-                );
+                change_gl_context(&mut self.current_gl_context, gl_surface);
 
                 gl_surface.resize(
                     self.current_gl_context
@@ -1530,19 +1521,7 @@ fn render_immediate_viewport(
 
     let screen_size_in_pixels: [u32; 2] = window.inner_size().into();
 
-    // change_gl_context(current_gl_context, gl_surface);
-    {
-        crate::profile_function!("context-switch");
-        *current_gl_context = Some(
-            current_gl_context
-                .take()
-                .unwrap()
-                .make_not_current()
-                .unwrap()
-                .make_current(gl_surface)
-                .unwrap(),
-        );
-    }
+    change_gl_context(current_gl_context, gl_surface);
 
     let current_gl_context = current_gl_context.as_ref().unwrap();
 
