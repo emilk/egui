b9f6cb863ebe1f06dfe9ea6e83104a701031259c
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-04-03 10:50:03 +0900

    Update slider.rs

--- crates/egui/src/widgets/slider.rs
+++ crates/egui/src/widgets/slider.rs
@@ -706,10 +706,10 @@ impl<'a> Slider<'a> {
                 // The trailing rect has to be drawn differently depending on the orientation.
                 match self.orientation {
                     SliderOrientation::Horizontal => {
-                        trailing_rail_rect.max.x = center.x + rounding.nw
+                        trailing_rail_rect.max.x = center.x + rounding.nw;
                     }
                     SliderOrientation::Vertical => {
-                        trailing_rail_rect.min.y = center.y - rounding.se
+                        trailing_rail_rect.min.y = center.y - rounding.se;
                     }
                 };
 
