c0f36d62f36f2a1a1af2ae3826039b70cec6403a
Seth Rider <seth.rider@gmail.com>
2024-04-05 10:35:40 +0100

    send meta on mac, too

--- crates/egui-winit/src/lib.rs
+++ crates/egui-winit/src/lib.rs
@@ -425,12 +425,8 @@ impl State {
                 self.egui_input.modifiers.alt = alt;
                 self.egui_input.modifiers.ctrl = ctrl;
                 self.egui_input.modifiers.shift = shift;
+                self.egui_input.modifiers.meta = super_;
                 self.egui_input.modifiers.mac_cmd = cfg!(target_os = "macos") && super_;
-                self.egui_input.modifiers.meta = if cfg!(target_os = "macos") {
-                    false
-                } else {
-                    super_
-                };
                 self.egui_input.modifiers.command = if cfg!(target_os = "macos") {
                     super_
                 } else {
