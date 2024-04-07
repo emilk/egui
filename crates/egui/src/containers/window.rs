8164d69096752f670aabb2e2527db304eb5cfd03
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-04-07 23:42:02 +0900

    Update window.rs

--- crates/egui/src/containers/window.rs
+++ crates/egui/src/containers/window.rs
@@ -567,14 +567,9 @@ impl<'open> Window<'open> {
                         round.sw = 0.0;
                     }
 
-                    let outer_stroke = ctx.style().visuals.widgets.noninteractive.fg_stroke;
                     area_content_ui.painter().set(
                         *where_to_put_header_background,
-                        RectShape::filled(
-                            title_rect.shrink(outer_stroke.width),
-                            round,
-                            header_color,
-                        ),
+                        RectShape::filled(title_rect, round, header_color),
                     );
                 };
 
