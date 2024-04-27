8a47702ccc6de0e8ce6ce80010a607ae11adfc50
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-04-28 02:05:49 +0900

    Update context.rs

--- crates/egui/src/context.rs
+++ crates/egui/src/context.rs
@@ -1086,11 +1086,14 @@ impl Context {
             .map_or(false, |response| response.contains_pointer)
     }
 
+    /// Create scroll delta
+    ///
+    /// See [`InputState`], [`ScrollArea`]
     pub(crate) fn create_scroll_delta(&self, inner_rect: Rect) {
         self.input_mut(|input| {
             let pointer_position = input.pointer.interact_pos().unwrap_or_default();
-            let is_contain = inner_rect.contains(pointer_position);
-            input.create_scroll_delta(false, is_contain);
+            let is_contains_pointer = inner_rect.contains(pointer_position);
+            input.create_scroll_delta(false, is_contains_pointer);
         });
     }
 
