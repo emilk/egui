6ecdf37fc1cb90a2e8f312b39c13dcde2a1f43b0
Andri <andri.baal@bluewin.ch>
2024-05-27 06:40:02 +0200

    [FIX] Migrated winit to 0.30 for X11 and Wayland

--- crates/egui-winit/src/lib.rs
+++ crates/egui-winit/src/lib.rs
@@ -1670,30 +1670,30 @@ pub fn create_winit_window_attributes(
 
     #[cfg(all(feature = "wayland", target_os = "linux"))]
     if let Some(app_id) = _app_id {
-        use winit::platform::wayland::WindowBuilderExtWayland as _;
+        use winit::platform::wayland::WindowAttributesExtWayland as _;
         window_attributes = window_attributes.with_name(app_id, "");
     }
 
     #[cfg(all(feature = "x11", target_os = "linux"))]
     {
         if let Some(window_type) = _window_type {
-            use winit::platform::x11::WindowBuilderExtX11 as _;
-            use winit::platform::x11::XWindowType;
+            use winit::platform::x11::WindowAttributesExtX11 as _;
+            use winit::platform::x11::WindowType;
             window_attributes = window_attributes.with_x11_window_type(vec![match window_type {
-                egui::X11WindowType::Normal => XWindowType::Normal,
-                egui::X11WindowType::Utility => XWindowType::Utility,
-                egui::X11WindowType::Dock => XWindowType::Dock,
-                egui::X11WindowType::Desktop => XWindowType::Desktop,
-                egui::X11WindowType::Toolbar => XWindowType::Toolbar,
-                egui::X11WindowType::Menu => XWindowType::Menu,
-                egui::X11WindowType::Splash => XWindowType::Splash,
-                egui::X11WindowType::Dialog => XWindowType::Dialog,
-                egui::X11WindowType::DropdownMenu => XWindowType::DropdownMenu,
-                egui::X11WindowType::PopupMenu => XWindowType::PopupMenu,
-                egui::X11WindowType::Tooltip => XWindowType::Tooltip,
-                egui::X11WindowType::Notification => XWindowType::Notification,
-                egui::X11WindowType::Combo => XWindowType::Combo,
-                egui::X11WindowType::Dnd => XWindowType::Dnd,
+                egui::X11WindowType::Normal => WindowType::Normal,
+                egui::X11WindowType::Utility => WindowType::Utility,
+                egui::X11WindowType::Dock => WindowType::Dock,
+                egui::X11WindowType::Desktop => WindowType::Desktop,
+                egui::X11WindowType::Toolbar => WindowType::Toolbar,
+                egui::X11WindowType::Menu => WindowType::Menu,
+                egui::X11WindowType::Splash => WindowType::Splash,
+                egui::X11WindowType::Dialog => WindowType::Dialog,
+                egui::X11WindowType::DropdownMenu => WindowType::DropdownMenu,
+                egui::X11WindowType::PopupMenu => WindowType::PopupMenu,
+                egui::X11WindowType::Tooltip => WindowType::Tooltip,
+                egui::X11WindowType::Notification => WindowType::Notification,
+                egui::X11WindowType::Combo => WindowType::Combo,
+                egui::X11WindowType::Dnd => WindowType::Dnd,
             }]);
         }
     }
