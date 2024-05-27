abb3cd6d4f2368626d6dac6f11622639f0e6992c
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-05-27 22:04:22 +0200

    Add test app `test_size_pass`

---
+++ tests/test_size_pass/src/main.rs
@@ -0,0 +1,19 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    env_logger::init(); // Use `RUST_LOG=debug` to see logs.
+
+    let options = eframe::NativeOptions::default();
+    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("The menu should be as wide as the widest button");
+            ui.menu_button("Click for menu", |ui| {
+                let _ = ui.button("Narrow").clicked();
+                let _ = ui.button("Very wide text").clicked();
+                let _ = ui.button("Narrow").clicked();
+            });
+        });
+    })
+}
