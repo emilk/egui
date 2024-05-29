emilk/egui

commit 2ce82cce2128a260daacf622e955a43a2ed67999

Added ability to define colors at UV coordinates along a path (#4353)
<!--
Please read the "Making a PR" section of
[`CONTRIBUTING.md`](https://github.com/emilk/egui/blob/master/CONTRIBUTING.md)
before opening a Pull Request!

* Keep your PR:s small and focused.
* The PR title is what ends up in the changelog, so make it descriptive!
* If applicable, add a screenshot or gif.
* If it is a non-trivial addition, consider adding a demo for it to
`egui_demo_lib`, or a new example.
* Do NOT open PR:s from your `master` branch, as that makes it hard for
maintainers to add commits to your PR.
* Remember to run `cargo fmt` and `cargo cranky`.
* Open the PR as a draft until you have self-reviewed it and run
`./scripts/check.sh`.
* When you have addressed a PR comment, mark it as resolved.

Please be patient! I will review your PR, but my time is limited!
-->

I had to make a couple types not Copy because closures, but it should'nt
be a massive deal.

I tried my best to make the API change as non breaking as possible.
Anywhere a PathStroke is used, you can just use a normal Stroke instead.
As mentioned above, the bezier paths couldn't be copy anymore, but IMO
that's a minor caveat.

---------

Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

crates/epaint/src/shape_transform.rs
@@ -1,7 +1,12 @@
+use std::sync::Arc;
+
 use crate::*;
 
 /// Remember to handle [`Color32::PLACEHOLDER`] specially!
-pub fn adjust_colors(shape: &mut Shape, adjust_color: &impl Fn(&mut Color32)) {
+pub fn adjust_colors(
+    shape: &mut Shape,
+    adjust_color: impl Fn(&mut Color32) + Send + Sync + Copy + 'static,
+) {
     #![allow(clippy::match_same_arms)]
     match shape {
         Shape::Noop => {}
@@ -10,8 +15,48 @@ pub fn adjust_colors(shape: &mut Shape, adjust_color: &impl Fn(&mut Color32)) {
                 adjust_colors(shape, adjust_color);
             }
         }
-        Shape::LineSegment { stroke, points: _ } => {
-            adjust_color(&mut stroke.color);
+        Shape::LineSegment { stroke, points: _ } => match &stroke.color {
+            color::ColorMode::Solid(mut col) => adjust_color(&mut col),
+            color::ColorMode::UV(callback) => {
+                let callback = callback.clone();
+                stroke.color = color::ColorMode::UV(Arc::new(Box::new(move |rect, pos| {
+                    let mut col = callback(rect, pos);
+                    adjust_color(&mut col);
+                    col
+                })));
+            }
+        },
+
+        Shape::Path(PathShape {
+            points: _,
+            closed: _,
+            fill,
+            stroke,
+        })
+        | Shape::QuadraticBezier(QuadraticBezierShape {
+            points: _,
+            closed: _,
+            fill,
+            stroke,
+        })
+        | Shape::CubicBezier(CubicBezierShape {
+            points: _,
+            closed: _,
+            fill,
+            stroke,
+        }) => {
+            adjust_color(fill);
+            match &stroke.color {
+                color::ColorMode::Solid(mut col) => adjust_color(&mut col),
+                color::ColorMode::UV(callback) => {
+                    let callback = callback.clone();
+                    stroke.color = color::ColorMode::UV(Arc::new(Box::new(move |rect, pos| {
+                        let mut col = callback(rect, pos);
+                        adjust_color(&mut col);
+                        col
+                    })));
+                }
+            }
         }
 
         Shape::Circle(CircleShape {
@@ -26,12 +71,6 @@ pub fn adjust_colors(shape: &mut Shape, adjust_color: &impl Fn(&mut Color32)) {
             fill,
             stroke,
         })
-        | Shape::Path(PathShape {
-            points: _,
-            closed: _,
-            fill,
-            stroke,
-        })
         | Shape::Rect(RectShape {
             rect: _,
             rounding: _,
@@ -40,18 +79,6 @@ pub fn adjust_colors(shape: &mut Shape, adjust_color: &impl Fn(&mut Color32)) {
             blur_width: _,
             fill_texture_id: _,
             uv: _,
-        })
-        | Shape::QuadraticBezier(QuadraticBezierShape {
-            points: _,
-            closed: _,
-            fill,
-            stroke,
-        })
-        | Shape::CubicBezier(CubicBezierShape {
-            points: _,
-            closed: _,
-            fill,
-            stroke,
         }) => {
             adjust_color(fill);
             adjust_color(&mut stroke.color);

crates/ecolor/Cargo.toml
@@ -30,7 +30,6 @@ extra_debug_asserts = []
 ## Always enable additional checks.
 extra_asserts = []
 
-
 [dependencies]
 #! ### Optional dependencies
 

crates/egui/src/painter.rs
@@ -7,7 +7,7 @@ use crate::{
 };
 use epaint::{
     text::{Fonts, Galley, LayoutJob},
-    CircleShape, ClippedShape, RectShape, Rounding, Shape, Stroke,
+    CircleShape, ClippedShape, PathStroke, RectShape, Rounding, Shape, Stroke,
 };
 
 /// Helper to paint shapes and text to a specific region on a specific layer.
@@ -280,7 +280,7 @@ impl Painter {
 /// # Paint different primitives
 impl Painter {
     /// Paints a line from the first point to the second.
-    pub fn line_segment(&self, points: [Pos2; 2], stroke: impl Into<Stroke>) -> ShapeIdx {
+    pub fn line_segment(&self, points: [Pos2; 2], stroke: impl Into<PathStroke>) -> ShapeIdx {
         self.add(Shape::LineSegment {
             points,
             stroke: stroke.into(),
@@ -288,13 +288,13 @@ impl Painter {
     }
 
     /// Paints a horizontal line.
-    pub fn hline(&self, x: impl Into<Rangef>, y: f32, stroke: impl Into<Stroke>) -> ShapeIdx {
-        self.add(Shape::hline(x, y, stroke))
+    pub fn hline(&self, x: impl Into<Rangef>, y: f32, stroke: impl Into<PathStroke>) -> ShapeIdx {
+        self.add(Shape::hline(x, y, stroke.into()))
     }
 
     /// Paints a vertical line.
-    pub fn vline(&self, x: f32, y: impl Into<Rangef>, stroke: impl Into<Stroke>) -> ShapeIdx {
-        self.add(Shape::vline(x, y, stroke))
+    pub fn vline(&self, x: f32, y: impl Into<Rangef>, stroke: impl Into<PathStroke>) -> ShapeIdx {
+        self.add(Shape::vline(x, y, stroke.into()))
     }
 
     pub fn circle(
@@ -513,7 +513,7 @@ impl Painter {
 }
 
 fn tint_shape_towards(shape: &mut Shape, target: Color32) {
-    epaint::shape_transform::adjust_colors(shape, &|color| {
+    epaint::shape_transform::adjust_colors(shape, move |color| {
         if *color != Color32::PLACEHOLDER {
             *color = crate::ecolor::tint_color_towards(*color, target);
         }
@@ -521,7 +521,7 @@ fn tint_shape_towards(shape: &mut Shape, target: Color32) {
 }
 
 fn multiply_opacity(shape: &mut Shape, opacity: f32) {
-    epaint::shape_transform::adjust_colors(shape, &|color| {
+    epaint::shape_transform::adjust_colors(shape, move |color| {
         if *color != Color32::PLACEHOLDER {
             *color = color.gamma_multiply(opacity);
         }

crates/egui_demo_lib/Cargo.toml
@@ -38,7 +38,7 @@ syntect = ["egui_extras/syntect"]
 
 
 [dependencies]
-egui = { workspace = true, default-features = false }
+egui = { workspace = true, default-features = false, features = ["color-hex"] }
 egui_extras = { workspace = true, features = ["default"] }
 egui_plot = { workspace = true, features = ["default"] }
 

crates/egui_demo_lib/src/demo/dancing_strings.rs
@@ -1,9 +1,11 @@
-use egui::{containers::*, *};
+use egui::{containers::*, epaint::PathStroke, *};
 
 #[derive(Default)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "serde", serde(default))]
-pub struct DancingStrings {}
+pub struct DancingStrings {
+    colors: bool,
+}
 
 impl super::Demo for DancingStrings {
     fn name(&self) -> &'static str {
@@ -27,6 +29,9 @@ impl super::View for DancingStrings {
         } else {
             Color32::from_black_alpha(240)
         };
+
+        ui.checkbox(&mut self.colors, "Colored")
+            .on_hover_text("Demonstrates how a path can have varying color across its length.");
 
         Frame::canvas(ui.style()).show(ui, |ui| {
             ui.ctx().request_repaint();
@@ -55,7 +60,24 @@ impl super::View for DancingStrings {
                     .collect();
 
                 let thickness = 10.0 / mode as f32;
-                shapes.push(epaint::Shape::line(points, Stroke::new(thickness, color)));
+                shapes.push(epaint::Shape::line(
+                    points,
+                    if self.colors {
+                        PathStroke::new_uv(thickness, move |rect, p| {
+                            let t = remap(p.x, rect.x_range(), -1.0..=1.0).abs();
+                            let center_color = hex_color!("#5BCEFA");
+                            let outer_color = hex_color!("#F5A9B8");
+
+                            Color32::from_rgb(
+                                lerp(center_color.r() as f32..=outer_color.r() as f32, t) as u8,
+                                lerp(center_color.g() as f32..=outer_color.g() as f32, t) as u8,
+                                lerp(center_color.b() as f32..=outer_color.b() as f32, t) as u8,
+                            )
+                        })
+                    } else {
+                        PathStroke::new(thickness, color)
+                    },
+                ));
             }
 
             ui.painter().extend(shapes);

crates/epaint/benches/benchmark.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use epaint::*;
+use epaint::{tessellator::Path, *};
 
 fn single_dashed_lines(c: &mut Criterion) {
     c.bench_function("single_dashed_lines", move |b| {
@@ -72,10 +72,166 @@ fn tessellate_circles(c: &mut Criterion) {
     });
 }
 
+fn thick_line_solid(c: &mut Criterion) {
+    c.bench_function("thick_solid_line", move |b| {
+        let line = [pos2(0.0, 0.0), pos2(50.0, 0.0), pos2(100.0, 1.0)];
+        let mut path = Path::default();
+        path.add_open_points(&line);
+
+        b.iter(|| {
+            let mut mesh = Mesh::default();
+            path.stroke_closed(1.5, &Stroke::new(2.0, Color32::RED).into(), &mut mesh);
+
+            black_box(mesh);
+        });
+    });
+}
+
+fn thick_large_line_solid(c: &mut Criterion) {
+    c.bench_function("thick_large_solid_line", move |b| {
+        let line = (0..1000).map(|i| pos2(i as f32, 10.0)).collect::<Vec<_>>();
+        let mut path = Path::default();
+        path.add_open_points(&line);
+
+        b.iter(|| {
+            let mut mesh = Mesh::default();
+            path.stroke_closed(1.5, &Stroke::new(2.0, Color32::RED).into(), &mut mesh);
+
+            black_box(mesh);
+        });
+    });
+}
+
+fn thin_line_solid(c: &mut Criterion) {
+    c.bench_function("thin_solid_line", move |b| {
+        let line = [pos2(0.0, 0.0), pos2(50.0, 0.0), pos2(100.0, 1.0)];
+        let mut path = Path::default();
+        path.add_open_points(&line);
+
+        b.iter(|| {
+            let mut mesh = Mesh::default();
+            path.stroke_closed(1.5, &Stroke::new(0.5, Color32::RED).into(), &mut mesh);
+
+            black_box(mesh);
+        });
+    });
+}
+
+fn thin_large_line_solid(c: &mut Criterion) {
+    c.bench_function("thin_large_solid_line", move |b| {
+        let line = (0..1000).map(|i| pos2(i as f32, 10.0)).collect::<Vec<_>>();
+        let mut path = Path::default();
+        path.add_open_points(&line);
+
+        b.iter(|| {
+            let mut mesh = Mesh::default();
+            path.stroke_closed(1.5, &Stroke::new(0.5, Color32::RED).into(), &mut mesh);
+
+            black_box(mesh);
+        });
+    });
+}
+
+fn thick_line_uv(c: &mut Criterion) {
+    c.bench_function("thick_uv_line", move |b| {
+        let line = [pos2(0.0, 0.0), pos2(50.0, 0.0), pos2(100.0, 1.0)];
+        let mut path = Path::default();
+        path.add_open_points(&line);
+
+        b.iter(|| {
+            let mut mesh = Mesh::default();
+            path.stroke_closed(
+                1.5,
+                &PathStroke::new_uv(2.0, |_, p| {
+                    black_box(p * 2.0);
+                    Color32::RED
+                }),
+                &mut mesh,
+            );
+
+            black_box(mesh);
+        });
+    });
+}
+
+fn thick_large_line_uv(c: &mut Criterion) {
+    c.bench_function("thick_large_uv_line", move |b| {
+        let line = (0..1000).map(|i| pos2(i as f32, 10.0)).collect::<Vec<_>>();
+        let mut path = Path::default();
+        path.add_open_points(&line);
+
+        b.iter(|| {
+            let mut mesh = Mesh::default();
+            path.stroke_closed(
+                1.5,
+                &PathStroke::new_uv(2.0, |_, p| {
+                    black_box(p * 2.0);
+                    Color32::RED
+                }),
+                &mut mesh,
+            );
+
+            black_box(mesh);
+        });
+    });
+}
+
+fn thin_line_uv(c: &mut Criterion) {
+    c.bench_function("thin_uv_line", move |b| {
+        let line = [pos2(0.0, 0.0), pos2(50.0, 0.0), pos2(100.0, 1.0)];
+        let mut path = Path::default();
+        path.add_open_points(&line);
+
+        b.iter(|| {
+            let mut mesh = Mesh::default();
+            path.stroke_closed(
+                1.5,
+                &PathStroke::new_uv(2.0, |_, p| {
+                    black_box(p * 2.0);
+                    Color32::RED
+                }),
+                &mut mesh,
+            );
+
+            black_box(mesh);
+        });
+    });
+}
+
+fn thin_large_line_uv(c: &mut Criterion) {
+    c.bench_function("thin_large_uv_line", move |b| {
+        let line = (0..1000).map(|i| pos2(i as f32, 10.0)).collect::<Vec<_>>();
+        let mut path = Path::default();
+        path.add_open_points(&line);
+
+        b.iter(|| {
+            let mut mesh = Mesh::default();
+            path.stroke_closed(
+                1.5,
+                &PathStroke::new_uv(2.0, |_, p| {
+                    black_box(p * 2.0);
+                    Color32::RED
+                }),
+                &mut mesh,
+            );
+
+            black_box(mesh);
+        });
+    });
+}
+
 criterion_group!(
     benches,
     single_dashed_lines,
     many_dashed_lines,
-    tessellate_circles
+    tessellate_circles,
+    thick_line_solid,
+    thick_large_line_solid,
+    thin_line_solid,
+    thin_large_line_solid,
+    thick_line_uv,
+    thick_large_line_uv,
+    thin_line_uv,
+    thin_large_line_uv
 );
 criterion_main!(benches);

crates/epaint/src/bezier.rs
@@ -3,7 +3,7 @@
 
 use std::ops::Range;
 
-use crate::{shape::Shape, Color32, PathShape, Stroke};
+use crate::{shape::Shape, Color32, PathShape, PathStroke};
 use emath::*;
 
 // ----------------------------------------------------------------------------
@@ -11,7 +11,7 @@ use emath::*;
 /// A cubic [Bézier Curve](https://en.wikipedia.org/wiki/B%C3%A9zier_curve).
 ///
 /// See also [`QuadraticBezierShape`].
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct CubicBezierShape {
     /// The first point is the starting point and the last one is the ending point of the curve.
@@ -20,7 +20,7 @@ pub struct CubicBezierShape {
     pub closed: bool,
 
     pub fill: Color32,
-    pub stroke: Stroke,
+    pub stroke: PathStroke,
 }
 
 impl CubicBezierShape {
@@ -32,7 +32,7 @@ impl CubicBezierShape {
         points: [Pos2; 4],
         closed: bool,
         fill: Color32,
-        stroke: impl Into<Stroke>,
+        stroke: impl Into<PathStroke>,
     ) -> Self {
         Self {
             points,
@@ -52,7 +52,7 @@ impl CubicBezierShape {
             points,
             closed: self.closed,
             fill: self.fill,
-            stroke: self.stroke,
+            stroke: self.stroke.clone(),
         }
     }
 
@@ -69,7 +69,7 @@ impl CubicBezierShape {
                 points,
                 closed: self.closed,
                 fill: self.fill,
-                stroke: self.stroke,
+                stroke: self.stroke.clone(),
             };
             pathshapes.push(pathshape);
         }
@@ -156,7 +156,7 @@ impl CubicBezierShape {
             points: [d_from, d_ctrl, d_to],
             closed: self.closed,
             fill: self.fill,
-            stroke: self.stroke,
+            stroke: self.stroke.clone(),
         };
         let delta_t = t_range.end - t_range.start;
         let q_start = q.sample(t_range.start);
@@ -168,7 +168,7 @@ impl CubicBezierShape {
             points: [from, ctrl1, ctrl2, to],
             closed: self.closed,
             fill: self.fill,
-            stroke: self.stroke,
+            stroke: self.stroke.clone(),
         }
     }
 
@@ -375,7 +375,7 @@ impl From<CubicBezierShape> for Shape {
 /// A quadratic [Bézier Curve](https://en.wikipedia.org/wiki/B%C3%A9zier_curve).
 ///
 /// See also [`CubicBezierShape`].
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct QuadraticBezierShape {
     /// The first point is the starting point and the last one is the ending point of the curve.
@@ -384,7 +384,7 @@ pub struct QuadraticBezierShape {
     pub closed: bool,
 
     pub fill: Color32,
-    pub stroke: Stroke,
+    pub stroke: PathStroke,
 }
 
 impl QuadraticBezierShape {
@@ -397,7 +397,7 @@ impl QuadraticBezierShape {
         points: [Pos2; 3],
         closed: bool,
         fill: Color32,
-        stroke: impl Into<Stroke>,
+        stroke: impl Into<PathStroke>,
     ) -> Self {
         Self {
             points,
@@ -417,7 +417,7 @@ impl QuadraticBezierShape {
             points,
             closed: self.closed,
             fill: self.fill,
-            stroke: self.stroke,
+            stroke: self.stroke.clone(),
         }
     }
 
@@ -429,7 +429,7 @@ impl QuadraticBezierShape {
             points,
             closed: self.closed,
             fill: self.fill,
-            stroke: self.stroke,
+            stroke: self.stroke.clone(),
         }
     }
 
@@ -688,7 +688,7 @@ fn single_curve_approximation(curve: &CubicBezierShape) -> QuadraticBezierShape 
         points: [curve.points[0], c, curve.points[3]],
         closed: curve.closed,
         fill: curve.fill,
-        stroke: curve.stroke,
+        stroke: curve.stroke.clone(),
     }
 }
 

crates/epaint/src/color.rs
@@ -1,0 +1,48 @@
+use std::{fmt::Debug, sync::Arc};
+
+use ecolor::Color32;
+use emath::{Pos2, Rect};
+
+/// How paths will be colored.
+#[derive(Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub enum ColorMode {
+    /// The entire path is one solid color, this is the default.
+    Solid(Color32),
+
+    /// Provide a callback which takes in the path's bounding box and a position and converts it to a color.
+    /// When used with a path, the bounding box will have a margin of [`TessellationOptions::feathering_size_in_pixels`](`crate::tessellator::TessellationOptions::feathering_size_in_pixels`)
+    ///
+    /// **This cannot be serialized**
+    #[cfg_attr(feature = "serde", serde(skip))]
+    UV(Arc<dyn Fn(Rect, Pos2) -> Color32 + Send + Sync>),
+}
+
+impl Default for ColorMode {
+    fn default() -> Self {
+        Self::Solid(Color32::TRANSPARENT)
+    }
+}
+
+impl Debug for ColorMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Solid(arg0) => f.debug_tuple("Solid").field(arg0).finish(),
+            Self::UV(_arg0) => f.debug_tuple("UV").field(&"<closure>").finish(),
+        }
+    }
+}
+
+impl PartialEq for ColorMode {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Solid(l0), Self::Solid(r0)) => l0 == r0,
+            (Self::UV(_l0), Self::UV(_r0)) => false,
+            _ => false,
+        }
+    }
+}
+
+impl ColorMode {
+    pub const TRANSPARENT: Self = Self::Solid(Color32::TRANSPARENT);
+}

commit e440067f9c7aabdda21dcf3bcacb15ab14955ad6

Bug fix: ui opacity and gray-out not affecting strokes
Bug introduced in https://github.com/emilk/egui/pull/4353

crates/epaint/src/shape_transform.rs
@@ -10,22 +10,16 @@ pub fn adjust_colors(
     #![allow(clippy::match_same_arms)]
     match shape {
         Shape::Noop => {}
+
         Shape::Vec(shapes) => {
             for shape in shapes {
                 adjust_colors(shape, adjust_color);
             }
         }
-        Shape::LineSegment { stroke, points: _ } => match &stroke.color {
-            color::ColorMode::Solid(mut col) => adjust_color(&mut col),
-            color::ColorMode::UV(callback) => {
-                let callback = callback.clone();
-                stroke.color = color::ColorMode::UV(Arc::new(Box::new(move |rect, pos| {
-                    let mut col = callback(rect, pos);
-                    adjust_color(&mut col);
-                    col
-                })));
-            }
-        },
+
+        Shape::LineSegment { stroke, points: _ } => {
+            adjust_color_mode(&mut stroke.color, adjust_color);
+        }
 
         Shape::Path(PathShape {
             points: _,
@@ -46,17 +40,7 @@ pub fn adjust_colors(
             stroke,
         }) => {
             adjust_color(fill);
-            match &stroke.color {
-                color::ColorMode::Solid(mut col) => adjust_color(&mut col),
-                color::ColorMode::UV(callback) => {
-                    let callback = callback.clone();
-                    stroke.color = color::ColorMode::UV(Arc::new(Box::new(move |rect, pos| {
-                        let mut col = callback(rect, pos);
-                        adjust_color(&mut col);
-                        col
-                    })));
-                }
-            }
+            adjust_color_mode(&mut stroke.color, adjust_color);
         }
 
         Shape::Circle(CircleShape {
@@ -121,6 +105,23 @@ pub fn adjust_colors(
 
         Shape::Callback(_) => {
             // Can't tint user callback code
+        }
+    }
+}
+
+fn adjust_color_mode(
+    color_mode: &mut ColorMode,
+    adjust_color: impl Fn(&mut Color32) + Send + Sync + Copy + 'static,
+) {
+    match color_mode {
+        color::ColorMode::Solid(color) => adjust_color(color),
+        color::ColorMode::UV(callback) => {
+            let callback = callback.clone();
+            *color_mode = color::ColorMode::UV(Arc::new(Box::new(move |rect, pos| {
+                let mut color = callback(rect, pos);
+                adjust_color(&mut color);
+                color
+            })));
         }
     }
 }