4a5239f965ba128a13fb24e07bcf04c7e103b808
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-06-05 17:29:04 +0900

    Update wgpu_integration.rs

--- crates/eframe/src/native/wgpu_integration.rs
+++ crates/eframe/src/native/wgpu_integration.rs
@@ -504,19 +504,6 @@ impl WinitApp for WgpuWinitApp {
                 }
             }
 
-            winit::event::Event::NewEvents(winit::event::StartCause::Poll) => {
-                if let Some(running) = &mut self.running {
-                    let viewport = &running.shared.borrow().viewports[&ViewportId::ROOT];
-                    if let Some(window) = viewport.window.as_ref() {
-                        EventResult::RepaintNow(window.id())
-                    } else {
-                        EventResult::Wait
-                    }
-                } else {
-                    EventResult::Wait
-                }
-            }
-
             _ => EventResult::Wait,
         })
     }
