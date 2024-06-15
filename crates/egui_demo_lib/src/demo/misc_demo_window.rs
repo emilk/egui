10d31277f200756dc19a2e9e6834532506896c0a
Ygor Souza <ygor.souza@protonmail.com>
2024-06-15 22:31:31 +0200

    Update Label::truncate example string in demo

--- crates/egui_demo_lib/src/demo/misc_demo_window.rs
+++ crates/egui_demo_lib/src/demo/misc_demo_window.rs
@@ -226,7 +226,7 @@ fn label_ui(ui: &mut egui::Ui) {
 
     ui.add(
         egui::Label::new(
-            "Labels containing long text can be set to elide the text that doesn't fit on a single line using `Label::elide`. When hovered, the label will show the full text.",
+            "Labels containing long text can be set to elide the text that doesn't fit on a single line using `Label::truncate`. When hovered, the label will show the full text.",
         )
         .truncate(),
     );
