df827c9559353c80c805c1dfa0f790f9ab247d64
zkldi <20380519+zkldi@users.noreply.github.com>
2024-06-18 23:50:51 +0100

    feat: Rectangle scale functions

--- crates/emath/src/rect.rs
+++ crates/emath/src/rect.rs
@@ -191,6 +191,18 @@ impl Rect {
         Self::from_min_max(self.min - amnt, self.max + amnt)
     }
 
+    /// Scale up by this factor in each direction, keeping the center
+    #[must_use]
+    pub fn scale(self, amnt: f32) -> Self {
+        self.scale2(Vec2::splat(amnt))
+    }
+
+    /// Scale up by this factor in each direction, keeping the center
+    #[must_use]
+    pub fn scale2(self, scale: Vec2) -> Self {
+        Self::from_center_size(self.center(), self.size() * scale)
+    }
+
     /// Shrink by this much in each direction, keeping the center
     #[must_use]
     pub fn shrink(self, amnt: f32) -> Self {
@@ -711,6 +723,19 @@ mod tests {
         assert_eq!(r.distance_sq_to_pos(pos2(15.0, 5.0)), 25.0); // above
         assert_eq!(r.distance_sq_to_pos(pos2(15.0, 25.0)), 25.0); // below
         assert_eq!(r.distance_sq_to_pos(pos2(25.0, 5.0)), 50.0); // right and above
+    }
+
+    #[test]
+    fn scale_rect() {
+        let c = pos2(100.0, 50.0);
+        let r = Rect::from_center_size(c, vec2(30.0, 60.0));
+
+        assert_eq!(r.scale(2.0), Rect::from_center_size(c, vec2(60.0, 120.0)));
+        assert_eq!(r.scale(0.5), Rect::from_center_size(c, vec2(15.0, 30.0)));
+        assert_eq!(
+            r.scale2(vec2(2.0, 3.0)),
+            Rect::from_center_size(c, vec2(60.0, 180.0))
+        );
     }
 
     #[test]
