dca552ea4896b18df038e65309fa2386482614c4
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-07-03 10:31:02 +0200

    Save `FrameState` of previous frame (#4761)
    By saving the `FrameState` of the previous frame, we make it much more
    versatile, and allows us to remove some other double-buffering.

--- crates/egui/src/frame_state.rs
+++ crates/egui/src/frame_state.rs
@@ -27,12 +27,17 @@ pub struct AccessKitFrameState {
     pub parent_stack: Vec<Id>,
 }
 
-/// State that is collected during a frame and then cleared.
-/// Short-term (single frame) memory.
+/// State that is collected during a frame, then saved for the next frame,
+/// and then cleared.
+///
+/// One per viewport.
 #[derive(Clone)]
 pub struct FrameState {
     /// All [`Id`]s that were used this frame.
     pub used_ids: IdMap<Rect>,
+
+    /// All widgets produced this frame.
+    pub widgets: WidgetRects,
 
     /// Starts off as the `screen_rect`, shrinks as panels are added.
     /// The [`CentralPanel`] does not change this.
@@ -68,10 +73,7 @@ pub struct FrameState {
     #[cfg(feature = "accesskit")]
     pub accesskit_state: Option<AccessKitFrameState>,
 
-    /// Highlight these widgets this next frame. Read from this.
-    pub highlight_this_frame: IdSet,
-
-    /// Highlight these widgets the next frame. Write to this.
+    /// Highlight these widgets the next frame.
     pub highlight_next_frame: IdSet,
 
     #[cfg(debug_assertions)]
@@ -82,6 +84,7 @@ impl Default for FrameState {
     fn default() -> Self {
         Self {
             used_ids: Default::default(),
+            widgets: Default::default(),
             available_rect: Rect::NAN,
             unused_rect: Rect::NAN,
             used_by_panels: Rect::NAN,
@@ -90,7 +93,6 @@ impl Default for FrameState {
             scroll_delta: Vec2::default(),
             #[cfg(feature = "accesskit")]
             accesskit_state: None,
-            highlight_this_frame: Default::default(),
             highlight_next_frame: Default::default(),
 
             #[cfg(debug_assertions)]
@@ -104,6 +106,7 @@ impl FrameState {
         crate::profile_function!();
         let Self {
             used_ids,
+            widgets,
             available_rect,
             unused_rect,
             used_by_panels,
@@ -112,7 +115,6 @@ impl FrameState {
             scroll_delta,
             #[cfg(feature = "accesskit")]
             accesskit_state,
-            highlight_this_frame,
             highlight_next_frame,
 
             #[cfg(debug_assertions)]
@@ -120,6 +122,7 @@ impl FrameState {
         } = self;
 
         used_ids.clear();
+        widgets.clear();
         *available_rect = screen_rect;
         *unused_rect = screen_rect;
         *used_by_panels = Rect::NOTHING;
@@ -137,7 +140,7 @@ impl FrameState {
             *accesskit_state = None;
         }
 
-        *highlight_this_frame = std::mem::take(highlight_next_frame);
+        highlight_next_frame.clear();
     }
 
     /// How much space is still available after panels has been added.
