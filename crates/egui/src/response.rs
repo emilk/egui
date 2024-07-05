76398013aed7a616dea0d52c5ba57c7ed80725c5
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-07-05 09:17:45 +0200

    Add `Context::request_repaint_after_secs`

--- crates/egui/src/response.rs
+++ crates/egui/src/response.rs
@@ -706,9 +706,7 @@ impl Response {
 
             if 0.0 < time_til_tooltip {
                 // Wait until the mouse has been still for a while
-                if let Ok(duration) = std::time::Duration::try_from_secs_f32(time_til_tooltip) {
-                    self.ctx.request_repaint_after(duration);
-                }
+                self.ctx.request_repaint_after_secs(time_til_tooltip);
                 return false;
             }
         }
