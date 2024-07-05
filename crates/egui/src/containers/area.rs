d7881e1dc7c73968b48f1210ebc848f6a838968a
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-07-05 11:00:35 +0200

    Fade in tooltips and windows slightly slower

--- crates/egui/src/containers/area.rs
+++ crates/egui/src/containers/area.rs
@@ -548,7 +548,7 @@ impl Prepared {
                 let age =
                     ctx.input(|i| (i.time - last_became_visible_at) as f32 + i.predicted_dt / 2.0);
                 let opacity = crate::remap_clamp(age, 0.0..=ctx.style().animation_time, 0.0..=1.0);
-                let opacity = emath::easing::cubic_out(opacity); // slow fade-out = quick fade-in
+                let opacity = emath::easing::quadratic_out(opacity); // slow fade-out = quick fade-in
                 ui.multiply_opacity(opacity);
                 if opacity < 1.0 {
                     ctx.request_repaint();
