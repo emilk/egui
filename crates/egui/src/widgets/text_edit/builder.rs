336196c2fa17795b19eb0245a3991b9900a50ac7
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-07-07 16:13:12 +0900

    Update builder.rs

--- crates/egui/src/widgets/text_edit/builder.rs
+++ crates/egui/src/widgets/text_edit/builder.rs
@@ -425,7 +425,7 @@ impl<'t> TextEdit<'t> {
                         frame_rect,
                         visuals.rounding,
                         ui.visuals().extreme_bg_color,
-                        ui.visuals().widgets.noninteractive.bg_stroke, // TODO(emilk): we want to show something here, or a text-edit field doesn't "pop".
+                        visuals.bg_stroke, // TODO(emilk): we want to show something here, or a text-edit field doesn't "pop".
                     )
                 }
             } else {
@@ -858,7 +858,7 @@ fn events(
     let mut events = ui.input(|i| i.filtered_events(&event_filter));
 
     if state.ime_enabled {
-        ime_enabled_filter_events(&mut events);
+        remove_ime_incompatible_events(&mut events);
         // Process IME events first:
         events.sort_by_key(|e| !matches!(e, Event::Ime(_)));
     }
@@ -1062,7 +1062,7 @@ fn events(
 
 // ----------------------------------------------------------------------------
 
-fn ime_enabled_filter_events(events: &mut Vec<Event>) {
+fn remove_ime_incompatible_events(events: &mut Vec<Event>) {
     // Remove key events which cause problems while 'IME' is being used.
     // See https://github.com/emilk/egui/pull/4509
     events.retain(|event| {
