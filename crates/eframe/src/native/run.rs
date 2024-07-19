491e2b382d267d39932319abe9611fc9911a2ea9
ardocrat <ardocrat@proton.me>
2024-07-19 10:01:55 +0300

    eframe: fix ios

--- crates/eframe/src/native/run.rs
+++ crates/eframe/src/native/run.rs
@@ -364,7 +364,7 @@ fn run_and_exit(
             // WaitUntil seems to not work on iOS
             #[cfg(target_os = "ios")]
             winit_app
-                .get_window_winit_id(ViewportId::ROOT)
+                .window_id_from_viewport_id(egui::ViewportId::ROOT)
                 .map(|window_id| {
                     winit_app
                         .window(window_id)
