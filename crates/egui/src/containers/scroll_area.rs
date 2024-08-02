517ada0f58f1820e6be42b6a6187792b4e5f0dc8
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-08-03 02:21:01 +0900

    Update scroll_area.rs

--- crates/egui/src/containers/scroll_area.rs
+++ crates/egui/src/containers/scroll_area.rs
@@ -966,6 +966,7 @@ impl Prepared {
             let main_range = Rangef::new(inner_rect.min[d], inner_rect.max[d]);
 
             // Margin on either side of the scroll bar:
+            let inner_margin = show_factor * scroll_style.bar_inner_margin;
             let outer_margin = show_factor * scroll_style.bar_outer_margin;
             let clip_max = ui.clip_rect().max[1 - d] - ui.spacing().item_spacing[1 - d];
 
@@ -1006,12 +1007,12 @@ impl Prepared {
 
             let outer_scroll_rect = if d == 0 {
                 Rect::from_min_max(
-                    pos2(inner_rect.left(), cross.min),
+                    pos2(inner_rect.left(), cross.min - inner_margin),
                     pos2(inner_rect.right(), cross.max + outer_margin),
                 )
             } else {
                 Rect::from_min_max(
-                    pos2(cross.min, inner_rect.top()),
+                    pos2(cross.min - inner_margin, inner_rect.top()),
                     pos2(cross.max + outer_margin, inner_rect.bottom()),
                 )
             };
