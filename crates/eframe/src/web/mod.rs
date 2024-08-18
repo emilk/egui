f63f525d5c0e0c5239fa64f753f0c5f1b91915c7
Nicolas <bircni@icloud.com>
2024-08-18 12:56:13 +0200

    update web-sys & wasm

--- crates/eframe/src/web/mod.rs
+++ crates/eframe/src/web/mod.rs
@@ -171,23 +171,14 @@ fn set_cursor_icon(cursor: egui::CursorIcon) -> Option<()> {
 #[cfg(web_sys_unstable_apis)]
 fn set_clipboard_text(s: &str) {
     if let Some(window) = web_sys::window() {
-        if let Some(clipboard) = window.navigator().clipboard() {
-            let promise = clipboard.write_text(s);
-            let future = wasm_bindgen_futures::JsFuture::from(promise);
-            let future = async move {
-                if let Err(err) = future.await {
-                    log::error!("Copy/cut action failed: {}", string_from_js_value(&err));
-                }
-            };
-            wasm_bindgen_futures::spawn_local(future);
-        } else {
-            let is_secure_context = window.is_secure_context();
-            if is_secure_context {
-                log::warn!("window.navigator.clipboard is null; can't copy text");
-            } else {
-                log::warn!("window.navigator.clipboard is null; can't copy text, probably because we're not in a secure context. See https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts");
+        let promise = window.navigator().clipboard().write_text(s);
+        let future = wasm_bindgen_futures::JsFuture::from(promise);
+        let future = async move {
+            if let Err(err) = future.await {
+                log::error!("Copy/cut action failed: {}", string_from_js_value(&err));
             }
-        }
+        };
+        wasm_bindgen_futures::spawn_local(future);
     }
 }
 
