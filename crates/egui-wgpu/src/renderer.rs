08f5eb30a429b7c7e34a7d40a7a1a26dfb254e4a
9SMTM6 <44668330+9SMTM6@users.noreply.github.com>
2024-09-13 13:00:18 +0200

    Add opt-out `fragile-send-sync-non-atomic-wasm` feature for wgpu (#5098)
    Note this will break people depending on eframe or egui-wgpu with
    --no-default-features.
    I don't know what to do about that to be honest.
    
    * Closes #4914 
    * [x] I have followed the instructions in the PR template
    
    ---------
    
    Co-authored-by: Andreas Reich <r_andreas2@web.de>

--- crates/egui-wgpu/src/renderer.rs
+++ crates/egui-wgpu/src/renderer.rs
@@ -7,8 +7,19 @@ use epaint::{emath::NumExt, PaintCallbackInfo, Primitive, Vertex};
 
 use wgpu::util::DeviceExt as _;
 
+// Only implements Send + Sync on wasm32 in order to allow storing wgpu resources on the type map.
+#[cfg(not(all(
+    target_arch = "wasm32",
+    not(feature = "fragile-send-sync-non-atomic-wasm"),
+)))]
 /// You can use this for storage when implementing [`CallbackTrait`].
 pub type CallbackResources = type_map::concurrent::TypeMap;
+#[cfg(all(
+    target_arch = "wasm32",
+    not(feature = "fragile-send-sync-non-atomic-wasm"),
+))]
+/// You can use this for storage when implementing [`CallbackTrait`].
+pub type CallbackResources = type_map::TypeMap;
 
 /// You can use this to do custom [`wgpu`] rendering in an egui app.
 ///
@@ -1017,6 +1028,11 @@ impl ScissorRect {
     }
 }
 
+// Look at the feature flag for an explanation.
+#[cfg(not(all(
+    target_arch = "wasm32",
+    not(feature = "fragile-send-sync-non-atomic-wasm"),
+)))]
 #[test]
 fn renderer_impl_send_sync() {
     fn assert_send_sync<T: Send + Sync>() {}
