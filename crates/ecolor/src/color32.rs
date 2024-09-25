5d46f67f7911c91be556e2a3d4583479c309a766
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-09-25 16:05:30 +0200

    Add `DebugOptions::show_unaligned` (#5165)
    This tool highlights coordinates that are non-integer.
    
    * Closes https://github.com/emilk/egui/issues/4927
    * Will be used for https://github.com/emilk/egui/issues/5163
    
    This is disabled by default (even in debug builds), because so many
    widgets cause un-alignment currently.

--- crates/ecolor/src/color32.rs
+++ crates/ecolor/src/color32.rs
@@ -54,6 +54,7 @@ impl Color32 {
     pub const LIGHT_RED: Self = Self::from_rgb(255, 128, 128);
 
     pub const YELLOW: Self = Self::from_rgb(255, 255, 0);
+    pub const ORANGE: Self = Self::from_rgb(255, 165, 0);
     pub const LIGHT_YELLOW: Self = Self::from_rgb(255, 255, 0xE0);
     pub const KHAKI: Self = Self::from_rgb(240, 230, 140);
 
