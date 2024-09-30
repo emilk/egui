emilk/egui

commit 76fe6c855bdef3b33174987f692921a6c658abf1

Fix: Backspace not working after IME input (#4912)
Fix: Changed the handling method of `Ime::Preedit(_, None)`

Fix: backspace fail after ime input

* Related #4358
* Related #4430 
* Related #4436
* Related #4794 
* Related #4896
* Closes #4908 

Issues: backspace fail after ime input
* #4908 (Chinese)

Changed the handling method of `Ime::Preedit(_, None)`

crates/egui-winit/src/lib.rs
@@ -341,8 +341,7 @@ impl State {
                 // We use input_method_editor_started to manually insert CompositionStart
                 // between Commits.
                 match ime {
-                    winit::event::Ime::Enabled => {}
-                    winit::event::Ime::Preedit(_, None) => {
+                    winit::event::Ime::Enabled => {
                         self.ime_event_enable();
                     }
                     winit::event::Ime::Preedit(text, Some(_cursor)) => {
@@ -357,7 +356,7 @@ impl State {
                             .push(egui::Event::Ime(egui::ImeEvent::Commit(text.clone())));
                         self.ime_event_disable();
                     }
-                    winit::event::Ime::Disabled => {
+                    winit::event::Ime::Disabled | winit::event::Ime::Preedit(_, None) => {
                         self.ime_event_disable();
                     }
                 };

commit 15d3d43aa30317ec7c0611d4faae890166d7a22b

Fix backspace/arrow keys in TextEdit on Linux (#5188)
* Closes https://github.com/emilk/egui/issues/5008
* Closes https://github.com/emilk/egui/pull/5182
* Bug introduced in https://github.com/emilk/egui/pull/4912

I suspect this will make IME no longer work on Linux, though I don't
know if it ever worked.
I rather have backspace/arrows working though.

Please help test this (I don't have Linux!)

# Tested on
* [x] Mac
* [ ] Linux Wayland
* [x] Linux X11

crates/egui-winit/src/lib.rs
@@ -329,39 +329,43 @@ impl State {
             }
 
             WindowEvent::Ime(ime) => {
-                // on Mac even Cmd-C is pressed during ime, a `c` is pushed to Preedit.
-                // So no need to check is_mac_cmd.
-                //
-                // How winit produce `Ime::Enabled` and `Ime::Disabled` differs in MacOS
-                // and Windows.
-                //
-                // - On Windows, before and after each Commit will produce an Enable/Disabled
-                // event.
-                // - On MacOS, only when user explicit enable/disable ime. No Disabled
-                // after Commit.
-                //
-                // We use input_method_editor_started to manually insert CompositionStart
-                // between Commits.
-                match ime {
-                    winit::event::Ime::Enabled => {
-                        self.ime_event_enable();
-                    }
-                    winit::event::Ime::Preedit(text, Some(_cursor)) => {
-                        self.ime_event_enable();
-                        self.egui_input
-                            .events
-                            .push(egui::Event::Ime(egui::ImeEvent::Preedit(text.clone())));
-                    }
-                    winit::event::Ime::Commit(text) => {
-                        self.egui_input
-                            .events
-                            .push(egui::Event::Ime(egui::ImeEvent::Commit(text.clone())));
-                        self.ime_event_disable();
-                    }
-                    winit::event::Ime::Disabled | winit::event::Ime::Preedit(_, None) => {
-                        self.ime_event_disable();
-                    }
-                };
+                if cfg!(target_os = "linux") {
+                    // We ignore IME events on linux, because of https://github.com/emilk/egui/issues/5008
+                } else {
+                    // on Mac even Cmd-C is pressed during ime, a `c` is pushed to Preedit.
+                    // So no need to check is_mac_cmd.
+                    //
+                    // How winit produce `Ime::Enabled` and `Ime::Disabled` differs in MacOS
+                    // and Windows.
+                    //
+                    // - On Windows, before and after each Commit will produce an Enable/Disabled
+                    // event.
+                    // - On MacOS, only when user explicit enable/disable ime. No Disabled
+                    // after Commit.
+                    //
+                    // We use input_method_editor_started to manually insert CompositionStart
+                    // between Commits.
+                    match ime {
+                        winit::event::Ime::Enabled => {
+                            self.ime_event_enable();
+                        }
+                        winit::event::Ime::Preedit(text, Some(_cursor)) => {
+                            self.ime_event_enable();
+                            self.egui_input
+                                .events
+                                .push(egui::Event::Ime(egui::ImeEvent::Preedit(text.clone())));
+                        }
+                        winit::event::Ime::Commit(text) => {
+                            self.egui_input
+                                .events
+                                .push(egui::Event::Ime(egui::ImeEvent::Commit(text.clone())));
+                            self.ime_event_disable();
+                        }
+                        winit::event::Ime::Disabled | winit::event::Ime::Preedit(_, None) => {
+                            self.ime_event_disable();
+                        }
+                    };
+                }
 
                 EventResponse {
                     repaint: true,