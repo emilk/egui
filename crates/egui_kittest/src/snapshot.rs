66a6c00ff6ec470066974f9bc963862d9d1ea231
Lucas Meurer <lucasmeurer96@gmail.com>
2024-10-04 21:24:58 +0200

    Fix lint

--- crates/egui_kittest/src/snapshot.rs
+++ crates/egui_kittest/src/snapshot.rs
@@ -6,6 +6,7 @@ pub enum SnapshotError {
     Diff { diff: i32, diff_path: PathBuf },
     MissingSnapshot { path: PathBuf },
 }
+
 impl Display for SnapshotError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
