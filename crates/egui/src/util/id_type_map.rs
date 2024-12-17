ece7a6403eeba2b7777a643345da60c4c7aa5b7e
Onè <43485962+c-git@users.noreply.github.com>
2024-12-17 12:14:38 -0500

    docs: remove "a"
    Was a duplicate article in the sentence. Already has "the"

--- crates/egui/src/util/id_type_map.rs
+++ crates/egui/src/util/id_type_map.rs
@@ -308,7 +308,7 @@ fn from_ron_str<T: serde::de::DeserializeOwned>(ron: &str) -> Option<T> {
 use crate::Id;
 
 // TODO(emilk): make IdTypeMap generic over the key (`Id`), and make a library of IdTypeMap.
-/// Stores values identified by an [`Id`] AND a the [`std::any::TypeId`] of the value.
+/// Stores values identified by an [`Id`] AND the [`std::any::TypeId`] of the value.
 ///
 /// In other words, it maps `(Id, TypeId)` to any value you want.
 ///
