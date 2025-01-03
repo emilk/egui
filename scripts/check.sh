5cbf337f1823028ce9614cd1d09c73ffb29cce8b
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-01-03 14:55:51 +0100

    check.sh: enable all features when running `cargo test`

--- scripts/check.sh
+++ scripts/check.sh
@@ -30,7 +30,7 @@ cargo check --quiet  --all-targets --all-features
 cargo check --quiet  -p egui_demo_app --lib --target wasm32-unknown-unknown
 cargo check --quiet  -p egui_demo_app --lib --target wasm32-unknown-unknown --all-features
 # TODO(#5297) re-enable --all-features once the tests work with the unity feature
-cargo test  --quiet --all-targets
+cargo test  --quiet --all-targets --all-features
 cargo test  --quiet --doc # slow - checks all doc-tests
 
 cargo check --quiet -p eframe --no-default-features --features "glow"
