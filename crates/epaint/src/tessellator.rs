ae670d55f03908bd1b9b5bf2e57f4c50255a6dad
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-02-02 12:12:28 +0100

    Rename: call it `uv_bbox` for clarity

--- crates/epaint/src/tessellator.rs
+++ crates/epaint/src/tessellator.rs
@@ -769,16 +769,15 @@ fn fill_closed_path(
 
     // TODO(juancampa): This bounding box is computed twice per shape: once here and another when tessellating the
     // stroke, consider hoisting that logic to the tessellator/scratchpad.
-    let bbox = if matches!(stroke.color, ColorMode::UV(_)) {
+    let uv_bbox = if matches!(stroke.color, ColorMode::UV(_)) {
         Rect::from_points(&path.iter().map(|p| p.pos).collect::<Vec<Pos2>>()).expand(feathering)
     } else {
         Rect::NAN
     };
-
     let stroke_color = &stroke.color;
     let get_stroke_color: Box<dyn Fn(Pos2) -> Color32> = match stroke_color {
         ColorMode::Solid(col) => Box::new(|_pos: Pos2| *col),
-        ColorMode::UV(fun) => Box::new(|pos: Pos2| fun(bbox, pos)),
+        ColorMode::UV(fun) => Box::new(|pos: Pos2| fun(uv_bbox, pos)),
     };
 
     let n = path.len() as u32;
@@ -952,16 +951,15 @@ fn stroke_path(
     }
 
     // Expand the bounding box to include the thickness of the path
-    let bbox = if matches!(stroke.color, ColorMode::UV(_)) {
+    let uv_bbox = if matches!(stroke.color, ColorMode::UV(_)) {
         Rect::from_points(&path.iter().map(|p| p.pos).collect::<Vec<Pos2>>())
             .expand((stroke.width / 2.0) + feathering)
     } else {
         Rect::NAN
     };
-
     let get_color = |col: &ColorMode, pos: Pos2| match col {
         ColorMode::Solid(col) => *col,
-        ColorMode::UV(fun) => fun(bbox, pos),
+        ColorMode::UV(fun) => fun(uv_bbox, pos),
     };
 
     if feathering > 0.0 {
