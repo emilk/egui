27e7303ebe1ccca44873813a3f684cba12de973e
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2025-02-20 13:32:34 -0500

    Clarify platform-specific details for `Viewport` positioning (#5715)
    On Wayland (and android apparently) it is not possible for applications
    to position their windows or request the window position (for some
    reason). This is not specified in the documentation.
    
    proof: [`winit::set_outer_position`], [`winit::outer_position`],
    [`winit::inner_position`] all specify that Android / Wayland are not
    supported.
    
    [`winit::set_outer_position`]:
    https://docs.rs/winit/latest/winit/window/struct.Window.html#method.set_outer_position
    [`winit::outer_position`]:
    https://docs.rs/winit/latest/winit/window/struct.Window.html#method.outer_position
    [`winit::inner_position`]:
    https://docs.rs/winit/latest/winit/window/struct.Window.html#method.inner_position
    
    * Maybe closes #4469
    * the lack of support makes me sad. I believe this makes some items in
    #3556 impossible on Wayland. Oh well!
    
    ---------
    
    Co-authored-by: lucasmerlin <lucasmeurer96@gmail.com>

--- crates/egui/src/data/input.rs
+++ crates/egui/src/data/input.rs
@@ -214,11 +214,21 @@ pub struct ViewportInfo {
     /// The inner rectangle of the native window, in monitor space and ui points scale.
     ///
     /// This is the content rectangle of the viewport.
+    ///
+    /// **`eframe` notes**:
+    ///
+    /// On Android / Wayland, this will always be `None` since getting the
+    /// position of the window is not possible.
     pub inner_rect: Option<Rect>,
 
     /// The outer rectangle of the native window, in monitor space and ui points scale.
     ///
     /// This is the content rectangle plus decoration chrome.
+    ///
+    /// **`eframe` notes**:
+    ///
+    /// On Android / Wayland, this will always be `None` since getting the
+    /// position of the window is not possible.
     pub outer_rect: Option<Rect>,
 
     /// Are we minimized?
