emilk/egui

commit d3c1ac37989671131817dcc1a41d1731f0f7714d

Fix `context_menu` not closing when clicking widget (#5799)
The rerun timeline context menu wouldn't close when clicking outside,
this fixes it

crates/egui/src/containers/popup.rs
@@ -273,11 +273,15 @@ impl<'a> Popup<'a> {
     /// In contrast to [`Self::menu`], this will open at the pointer position.
     pub fn context_menu(response: &Response) -> Self {
         Self::menu(response)
-            .open_memory(
-                response
-                    .secondary_clicked()
-                    .then_some(SetOpenCommand::Bool(true)),
-            )
+            .open_memory(if response.secondary_clicked() {
+                Some(SetOpenCommand::Bool(true))
+            } else if response.clicked() {
+                // Explicitly close the menu if the widget was clicked
+                // Without this, the context menu would stay open if the user clicks the widget
+                Some(SetOpenCommand::Bool(false))
+            } else {
+                None
+            })
             .at_pointer_fixed()
     }
 

commit a2afc8d092a4e3f0b17675b114db8fad08810947

Make `close_popup` take an `Id` and add `close_all_popups`  (#5820)
Ooops, fixes a regression introduced in #5799

* [x] I have followed the instructions in the PR template

crates/egui/src/containers/popup.rs
@@ -85,7 +85,7 @@ pub enum PopupCloseBehavior {
     /// but in the popup's body
     CloseOnClickOutside,
 
-    /// Clicks will be ignored. Popup might be closed manually by calling [`crate::Memory::close_popup`]
+    /// Clicks will be ignored. Popup might be closed manually by calling [`crate::Memory::close_all_popups`]
     /// or by pressing the escape button
     IgnoreClicks,
 }
@@ -524,7 +524,7 @@ impl<'a> Popup<'a> {
                             _ => mem.open_popup(id),
                         }
                     } else {
-                        mem.close_popup();
+                        mem.close_popup(id);
                     }
                 }
                 Some(SetOpenCommand::Toggle) => {
@@ -606,7 +606,7 @@ impl<'a> Popup<'a> {
             }
             OpenKind::Memory { .. } => {
                 if should_close {
-                    ctx.memory_mut(|mem| mem.close_popup());
+                    ctx.memory_mut(|mem| mem.close_popup(id));
                 }
             }
         }

crates/egui/src/memory/mod.rs
@@ -1098,9 +1098,18 @@ impl Memory {
             .and_then(|(popup_id, pos)| if popup_id == id { pos } else { None })
     }
 
-    /// Close the open popup, if any.
-    pub fn close_popup(&mut self) {
+    /// Close any currently open popup.
+    pub fn close_all_popups(&mut self) {
         self.popup = None;
+    }
+
+    /// Close the given popup, if it is open.
+    ///
+    /// See also [`Self::close_all_popups`] if you want to close any / all currently open popups.
+    pub fn close_popup(&mut self, popup_id: Id) {
+        if self.is_popup_open(popup_id) {
+            self.popup = None;
+        }
     }
 
     /// Toggle the given popup between closed and open.
@@ -1108,7 +1117,7 @@ impl Memory {
     /// Note: At most, only one popup can be open at a time.
     pub fn toggle_popup(&mut self, popup_id: Id) {
         if self.is_popup_open(popup_id) {
-            self.close_popup();
+            self.close_popup(popup_id);
         } else {
             self.open_popup(popup_id);
         }

crates/egui/src/widgets/color_picker.rs
@@ -521,7 +521,7 @@ pub fn color_edit_button_hsva(ui: &mut Ui, hsva: &mut Hsva, alpha: Alpha) -> Res
         if !button_response.clicked()
             && (ui.input(|i| i.key_pressed(Key::Escape)) || area_response.clicked_elsewhere())
         {
-            ui.memory_mut(|mem| mem.close_popup());
+            ui.memory_mut(|mem| mem.close_popup(popup_id));
         }
     }
 
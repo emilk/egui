a2afc8d092a4e3f0b17675b114db8fad08810947
Lucas Meurer <lucasmeurer96@gmail.com>
2025-03-18 15:32:14 +0100

    Make `close_popup` take an `Id` and add `close_all_popups`  (#5820)
    Ooops, fixes a regression introduced in #5799
    
    * [x] I have followed the instructions in the PR template

--- crates/egui/src/memory/mod.rs
+++ crates/egui/src/memory/mod.rs
@@ -1098,9 +1098,18 @@ impl Memory {
             .and_then(|(popup_id, pos)| if popup_id == id { pos } else { None })
     }
 
-    /// Close the open popup, if any.
-    pub fn close_popup(&mut self) {
+    /// Close any currently open popup.
+    pub fn close_all_popups(&mut self) {
         self.popup = None;
+    }
+
+    /// Close the given popup, if it is open.
+    ///
+    /// See also [`Self::close_all_popups`] if you want to close any / all currently open popups.
+    pub fn close_popup(&mut self, popup_id: Id) {
+        if self.is_popup_open(popup_id) {
+            self.popup = None;
+        }
     }
 
     /// Toggle the given popup between closed and open.
@@ -1108,7 +1117,7 @@ impl Memory {
     /// Note: At most, only one popup can be open at a time.
     pub fn toggle_popup(&mut self, popup_id: Id) {
         if self.is_popup_open(popup_id) {
-            self.close_popup();
+            self.close_popup(popup_id);
         } else {
             self.open_popup(popup_id);
         }
