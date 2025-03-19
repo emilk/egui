61547a95235ed1ba4261cf686520faf012d1b4c4
valadaptive <valadaptive@protonmail.com>
2025-03-19 08:38:57 -0400

    Color emoji

--- crates/epaint/src/text/fonts.rs
+++ crates/epaint/src/text/fonts.rs
@@ -501,7 +501,7 @@ impl Fonts {
 
     /// The full font atlas image.
     #[inline]
-    pub fn image(&self) -> crate::FontImage {
+    pub fn image(&self) -> crate::ColorImage {
         self.lock().fonts.atlas.lock().image().clone()
     }
 
