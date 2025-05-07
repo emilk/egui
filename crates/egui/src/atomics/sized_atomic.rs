a74f3dda45ecbb158b63b4f939cd8ea565ca01f5
lucasmerlin <hi@lucasmerlin.me>
2025-05-07 16:37:15 +0200

    Make grow private in SizedAtomic

--- crates/egui/src/atomics/sized_atomic.rs
+++ crates/egui/src/atomics/sized_atomic.rs
@@ -4,8 +4,15 @@ use emath::Vec2;
 /// A [`Atomic`] which has been sized.
 #[derive(Clone, Debug)]
 pub struct SizedAtomic<'a> {
-    pub grow: bool,
+    grow: bool,
     pub size: Vec2,
     pub preferred_size: Vec2,
     pub kind: SizedAtomicKind<'a>,
+}
+
+impl SizedAtomic<'_> {
+    /// Was this [`Atomic`] marked as `grow`?
+    fn is_grow(&self) -> bool {
+        self.grow
+    }
 }
