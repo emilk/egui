emilk/egui

commit 557bd56e1962266e765cc7b7958c1fd3f14fa3e1

Optimize editing long text by caching each paragraph (#5411)
## What
(written by @emilk)
When editing long text (thousands of line), egui would previously
re-layout the entire text on each edit. This could be slow.

With this PR, we instead split the text into paragraphs (split on `\n`)
and then cache each such paragraph. When editing text then, only the
changed paragraph needs to be laid out again.

Still, there is overhead from splitting the text, hashing each
paragraph, and then joining the results, so the runtime complexity is
still O(N).

In our benchmark, editing a 2000 line string goes from ~8ms to ~300 ms,
a speedup of ~25x.

In the future, we could also consider laying out each paragraph in
parallel, to speed up the initial layout of the text.

## Details
This is an ~~almost complete~~ implementation of the approach described
by emilk [in this
comment](<https://github.com/emilk/egui/issues/3086#issuecomment-1724205777>),
excluding CoW semantics for `LayoutJob` (but including them for `Row`).
It supersedes the previous unsuccessful attempt here:
https://github.com/emilk/egui/pull/4000.

Draft because:
- [X] ~~Currently individual rows will have `ends_with_newline` always
set to false.
This breaks selection with Ctrl+A (and probably many other things)~~
- [X] ~~The whole block for doing the splitting and merging should
probably become a function (I'll do that later).~~
- [X] ~~I haven't run the check script, the tests, and haven't made sure
all of the examples build (although I assume they probably don't rely on
Galley internals).~~
- [x] ~~Layout is sometimes incorrect (missing empty lines, wrapping
sometimes makes text overlap).~~
- A lot of text-related code had to be changed so this needs to be
properly tested to ensure no layout issues were introduced, especially
relating to the now row-relative coordinate system of `Row`s. Also this
requires that we're fine making these very breaking changes.

crates/epaint/src/text/fonts.rs
@@ -4,7 +4,7 @@ use crate::{
     mutex::{Mutex, MutexGuard},
     text::{
         font::{Font, FontImpl},
-        Galley, LayoutJob,
+        Galley, LayoutJob, LayoutSection,
     },
     TextureAtlas,
 };
@@ -617,7 +617,9 @@ pub struct FontsAndCache {
 
 impl FontsAndCache {
     fn layout_job(&mut self, job: LayoutJob) -> Arc<Galley> {
-        self.galley_cache.layout(&mut self.fonts, job)
+        let allow_split_paragraphs = true; // Optimization for editing text with many paragraphs.
+        self.galley_cache
+            .layout(&mut self.fonts, job, allow_split_paragraphs)
     }
 }
 
@@ -726,6 +728,12 @@ impl FontsImpl {
 struct CachedGalley {
     /// When it was last used
     last_used: u32,
+
+    /// Hashes of all other entries this one depends on for quick re-layout.
+    /// Their `last_used`s should be updated alongside this one to make sure they're
+    /// not evicted.
+    children: Option<Arc<[u64]>>,
+
     galley: Arc<Galley>,
 }
 
@@ -737,13 +745,18 @@ struct GalleyCache {
 }
 
 impl GalleyCache {
-    fn layout(&mut self, fonts: &mut FontsImpl, mut job: LayoutJob) -> Arc<Galley> {
+    fn layout_internal(
+        &mut self,
+        fonts: &mut FontsImpl,
+        mut job: LayoutJob,
+        allow_split_paragraphs: bool,
+    ) -> (u64, Arc<Galley>) {
         if job.wrap.max_width.is_finite() {
             // Protect against rounding errors in egui layout code.
 
             // Say the user asks to wrap at width 200.0.
             // The text layout wraps, and reports that the final width was 196.0 points.
-            // This than trickles up the `Ui` chain and gets stored as the width for a tooltip (say).
+            // This then trickles up the `Ui` chain and gets stored as the width for a tooltip (say).
             // On the next frame, this is then set as the max width for the tooltip,
             // and we end up calling the text layout code again, this time with a wrap width of 196.0.
             // Except, somewhere in the `Ui` chain with added margins etc, a rounding error was introduced,
@@ -765,22 +778,176 @@ impl GalleyCache {
 
         let hash = crate::util::hash(&job); // TODO(emilk): even faster hasher?
 
-        match self.cache.entry(hash) {
+        let galley = match self.cache.entry(hash) {
             std::collections::hash_map::Entry::Occupied(entry) => {
+                // The job was found in cache - no need to re-layout.
                 let cached = entry.into_mut();
                 cached.last_used = self.generation;
-                cached.galley.clone()
-            }
-            std::collections::hash_map::Entry::Vacant(entry) => {
-                let galley = super::layout(fonts, job.into());
-                let galley = Arc::new(galley);
-                entry.insert(CachedGalley {
-                    last_used: self.generation,
-                    galley: galley.clone(),
-                });
+
+                let galley = cached.galley.clone();
+                if let Some(children) = &cached.children {
+                    // The point of `allow_split_paragraphs` is to split large jobs into paragraph,
+                    // and then cache each paragraph individually.
+                    // That way, if we edit a single paragraph, only that paragraph will be re-layouted.
+                    // For that to work we need to keep all the child/paragraph
+                    // galleys alive while the parent galley is alive:
+                    for child_hash in children.clone().iter() {
+                        if let Some(cached_child) = self.cache.get_mut(child_hash) {
+                            cached_child.last_used = self.generation;
+                        }
+                    }
+                }
+
                 galley
             }
+            std::collections::hash_map::Entry::Vacant(entry) => {
+                let job = Arc::new(job);
+                if allow_split_paragraphs && should_cache_each_paragraph_individually(&job) {
+                    let (child_galleys, child_hashes) =
+                        self.layout_each_paragraph_individuallly(fonts, &job);
+                    debug_assert_eq!(
+                        child_hashes.len(),
+                        child_galleys.len(),
+                        "Bug in `layout_each_paragraph_individuallly`"
+                    );
+                    let galley =
+                        Arc::new(Galley::concat(job, &child_galleys, fonts.pixels_per_point));
+
+                    self.cache.insert(
+                        hash,
+                        CachedGalley {
+                            last_used: self.generation,
+                            children: Some(child_hashes.into()),
+                            galley: galley.clone(),
+                        },
+                    );
+                    galley
+                } else {
+                    let galley = super::layout(fonts, job);
+                    let galley = Arc::new(galley);
+                    entry.insert(CachedGalley {
+                        last_used: self.generation,
+                        children: None,
+                        galley: galley.clone(),
+                    });
+                    galley
+                }
+            }
+        };
+
+        (hash, galley)
+    }
+
+    fn layout(
+        &mut self,
+        fonts: &mut FontsImpl,
+        job: LayoutJob,
+        allow_split_paragraphs: bool,
+    ) -> Arc<Galley> {
+        self.layout_internal(fonts, job, allow_split_paragraphs).1
+    }
+
+    /// Split on `\n` and lay out (and cache) each paragraph individually.
+    fn layout_each_paragraph_individuallly(
+        &mut self,
+        fonts: &mut FontsImpl,
+        job: &LayoutJob,
+    ) -> (Vec<Arc<Galley>>, Vec<u64>) {
+        profiling::function_scope!();
+
+        let mut current_section = 0;
+        let mut start = 0;
+        let mut max_rows_remaining = job.wrap.max_rows;
+        let mut child_galleys = Vec::new();
+        let mut child_hashes = Vec::new();
+
+        while start < job.text.len() {
+            let is_first_paragraph = start == 0;
+            let end = job.text[start..]
+                .find('\n')
+                .map_or(job.text.len(), |i| start + i + 1);
+
+            let mut paragraph_job = LayoutJob {
+                text: job.text[start..end].to_owned(),
+                wrap: crate::text::TextWrapping {
+                    max_rows: max_rows_remaining,
+                    ..job.wrap
+                },
+                sections: Vec::new(),
+                break_on_newline: job.break_on_newline,
+                halign: job.halign,
+                justify: job.justify,
+                first_row_min_height: if is_first_paragraph {
+                    job.first_row_min_height
+                } else {
+                    0.0
+                },
+                round_output_to_gui: job.round_output_to_gui,
+            };
+
+            // Add overlapping sections:
+            for section in &job.sections[current_section..job.sections.len()] {
+                let LayoutSection {
+                    leading_space,
+                    byte_range: section_range,
+                    format,
+                } = section;
+
+                // `start` and `end` are the byte range of the current paragraph.
+                // How does the current section overlap with the paragraph range?
+
+                if section_range.end <= start {
+                    // The section is behind us
+                    current_section += 1;
+                } else if end <= section_range.start {
+                    break; // Haven't reached this one yet.
+                } else {
+                    // Section range overlaps with paragraph range
+                    debug_assert!(
+                        section_range.start < section_range.end,
+                        "Bad byte_range: {section_range:?}"
+                    );
+                    let new_range = section_range.start.saturating_sub(start)
+                        ..(section_range.end.at_most(end)).saturating_sub(start);
+                    debug_assert!(
+                        new_range.start <= new_range.end,
+                        "Bad new section range: {new_range:?}"
+                    );
+                    paragraph_job.sections.push(LayoutSection {
+                        leading_space: if start <= new_range.start {
+                            *leading_space
+                        } else {
+                            0.0
+                        },
+                        byte_range: new_range,
+                        format: format.clone(),
+                    });
+                }
+            }
+
+            // TODO(emilk): we could lay out each paragraph in parallel to get a nice speedup on multicore machines.
+            let (hash, galley) = self.layout_internal(fonts, paragraph_job, false);
+            child_hashes.push(hash);
+
+            // This will prevent us from invalidating cache entries unnecessarily:
+            if max_rows_remaining != usize::MAX {
+                max_rows_remaining -= galley.rows.len();
+                // Ignore extra trailing row, see merging `Galley::concat` for more details.
+                if end < job.text.len() && !galley.elided {
+                    max_rows_remaining += 1;
+                }
+            }
+
+            let elided = galley.elided;
+            child_galleys.push(galley);
+            if elided {
+                break;
+            }
+
+            start = end;
         }
+
+        (child_galleys, child_hashes)
     }
 
     pub fn num_galleys_in_cache(&self) -> usize {
@@ -795,6 +962,16 @@ impl GalleyCache {
         });
         self.generation = self.generation.wrapping_add(1);
     }
+}
+
+/// If true, lay out and cache each paragraph (sections separated by newlines) individually.
+///
+/// This makes it much faster to re-layout the full text when only a portion of it has changed since last frame, i.e. when editing somewhere in a file with thousands of lines/paragraphs.
+fn should_cache_each_paragraph_individually(job: &LayoutJob) -> bool {
+    // We currently don't support this elided text, i.e. when `max_rows` is set.
+    // Most often, elided text is elided to one row,
+    // and so will always be fast to lay out.
+    job.break_on_newline && job.wrap.max_rows == usize::MAX && job.text.contains('\n')
 }
 
 // ----------------------------------------------------------------------------
@@ -865,5 +1042,123 @@ impl FontImplCache {
                 ))
             })
             .clone()
+    }
+}
+
+#[cfg(feature = "default_fonts")]
+#[cfg(test)]
+mod tests {
+    use core::f32;
+
+    use super::*;
+    use crate::{text::TextFormat, Stroke};
+    use ecolor::Color32;
+    use emath::Align;
+
+    fn jobs() -> Vec<LayoutJob> {
+        vec![
+            LayoutJob::simple(
+                String::default(),
+                FontId::new(14.0, FontFamily::Monospace),
+                Color32::WHITE,
+                f32::INFINITY,
+            ),
+            LayoutJob::simple(
+                "Simple test.".to_owned(),
+                FontId::new(14.0, FontFamily::Monospace),
+                Color32::WHITE,
+                f32::INFINITY,
+            ),
+            LayoutJob::simple(
+                "This some text that may be long.\nDet kanske också finns lite ÅÄÖ här.".to_owned(),
+                FontId::new(14.0, FontFamily::Proportional),
+                Color32::WHITE,
+                50.0,
+            ),
+            {
+                let mut job = LayoutJob {
+                    first_row_min_height: 20.0,
+                    ..Default::default()
+                };
+                job.append(
+                    "1st paragraph has some leading space.\n",
+                    16.0,
+                    TextFormat {
+                        font_id: FontId::new(14.0, FontFamily::Proportional),
+                        ..Default::default()
+                    },
+                );
+                job.append(
+                    "2nd paragraph has underline and strikthrough, and has some non-ASCII characters:\n ÅÄÖ.",
+                    0.0,
+                    TextFormat {
+                        font_id: FontId::new(15.0, FontFamily::Monospace),
+                        underline: Stroke::new(1.0, Color32::RED),
+                        strikethrough: Stroke::new(1.0, Color32::GREEN),
+                        ..Default::default()
+                    },
+                );
+                job.append(
+                    "3rd paragraph is kind of boring, but has italics.\nAnd a newline",
+                    0.0,
+                    TextFormat {
+                        font_id: FontId::new(10.0, FontFamily::Proportional),
+                        italics: true,
+                        ..Default::default()
+                    },
+                );
+
+                job
+            },
+        ]
+    }
+
+    #[test]
+    fn test_split_paragraphs() {
+        for pixels_per_point in [1.0, 2.0_f32.sqrt(), 2.0] {
+            let max_texture_side = 4096;
+            let mut fonts = FontsImpl::new(
+                pixels_per_point,
+                max_texture_side,
+                FontDefinitions::default(),
+            );
+
+            for halign in [Align::Min, Align::Center, Align::Max] {
+                for justify in [false, true] {
+                    for mut job in jobs() {
+                        job.halign = halign;
+                        job.justify = justify;
+
+                        let whole = GalleyCache::default().layout(&mut fonts, job.clone(), false);
+
+                        let split = GalleyCache::default().layout(&mut fonts, job.clone(), true);
+
+                        for (i, row) in whole.rows.iter().enumerate() {
+                            println!(
+                                "Whole row {i}: section_index_at_start={}, first glyph section_index: {:?}",
+                                row.row.section_index_at_start,
+                                row.row.glyphs.first().map(|g| g.section_index)
+                            );
+                        }
+                        for (i, row) in split.rows.iter().enumerate() {
+                            println!(
+                                "Split row {i}: section_index_at_start={}, first glyph section_index: {:?}",
+                                row.row.section_index_at_start,
+                                row.row.glyphs.first().map(|g| g.section_index)
+                            );
+                        }
+
+                        // Don't compare for equaliity; but format with a specific precision and make sure we hit that.
+                        // NOTE: we use a rather low precision, because as long as we're within a pixel I think it's good enough.
+                        similar_asserts::assert_eq!(
+                            format!("{:#.1?}", split),
+                            format!("{:#.1?}", whole),
+                            "pixels_per_point: {pixels_per_point:.2}, input text: '{}'",
+                            job.text
+                        );
+                    }
+                }
+            }
+        }
     }
 }

Cargo.toml
@@ -96,6 +96,7 @@ puffin_http = "0.16"
 raw-window-handle = "0.6.0"
 ron = "0.8"
 serde = { version = "1", features = ["derive"] }
+similar-asserts = "1.4.2"
 thiserror = "1.0.37"
 type-map = "0.5.0"
 wasm-bindgen = "0.2"

crates/egui/src/text_selection/accesskit_text.rs
@@ -45,7 +45,7 @@ pub fn update_accesskit_for_text_widget(
             let row_id = parent_id.with(row_index);
             ctx.accesskit_node_builder(row_id, |builder| {
                 builder.set_role(accesskit::Role::TextRun);
-                let rect = global_from_galley * row.rect;
+                let rect = global_from_galley * row.rect();
                 builder.set_bounds(accesskit::Rect {
                     x0: rect.min.x.into(),
                     y0: rect.min.y.into(),
@@ -76,14 +76,14 @@ pub fn update_accesskit_for_text_widget(
                     let old_len = value.len();
                     value.push(glyph.chr);
                     character_lengths.push((value.len() - old_len) as _);
-                    character_positions.push(glyph.pos.x - row.rect.min.x);
+                    character_positions.push(glyph.pos.x - row.pos.x);
                     character_widths.push(glyph.advance_width);
                 }
 
                 if row.ends_with_newline {
                     value.push('\n');
                     character_lengths.push(1);
-                    character_positions.push(row.rect.max.x - row.rect.min.x);
+                    character_positions.push(row.size.x);
                     character_widths.push(0.0);
                 }
                 word_lengths.push((character_lengths.len() - last_word_start) as _);

crates/egui/src/text_selection/label_text_selection.rs
@@ -186,7 +186,10 @@ impl LabelSelectionState {
                                 if let epaint::Shape::Text(text_shape) = &mut shape.shape {
                                     let galley = Arc::make_mut(&mut text_shape.galley);
                                     for row_selection in row_selections {
-                                        if let Some(row) = galley.rows.get_mut(row_selection.row) {
+                                        if let Some(placed_row) =
+                                            galley.rows.get_mut(row_selection.row)
+                                        {
+                                            let row = Arc::make_mut(&mut placed_row.row);
                                             for vertex_index in row_selection.vertex_indices {
                                                 if let Some(vertex) = row
                                                     .visuals
@@ -701,8 +704,8 @@ fn selected_text(galley: &Galley, cursor_range: &CCursorRange) -> String {
 }
 
 fn estimate_row_height(galley: &Galley) -> f32 {
-    if let Some(row) = galley.rows.first() {
-        row.rect.height()
+    if let Some(placed_row) = galley.rows.first() {
+        placed_row.height()
     } else {
         galley.size().y
     }

crates/egui/src/text_selection/visuals.rs
@@ -31,11 +31,12 @@ pub fn paint_text_selection(
     let max = galley.layout_from_cursor(max);
 
     for ri in min.row..=max.row {
-        let row = &mut galley.rows[ri];
+        let row = Arc::make_mut(&mut galley.rows[ri].row);
+
         let left = if ri == min.row {
             row.x_offset(min.column)
         } else {
-            row.rect.left()
+            0.0
         };
         let right = if ri == max.row {
             row.x_offset(max.column)
@@ -45,10 +46,10 @@ pub fn paint_text_selection(
             } else {
                 0.0
             };
-            row.rect.right() + newline_size
+            row.size.x + newline_size
         };
 
-        let rect = Rect::from_min_max(pos2(left, row.min_y()), pos2(right, row.max_y()));
+        let rect = Rect::from_min_max(pos2(left, 0.0), pos2(right, row.size.y));
         let mesh = &mut row.visuals.mesh;
 
         // Time to insert the selection rectangle into the row mesh.

crates/egui/src/widget_text.rs
@@ -671,8 +671,8 @@ impl WidgetText {
             Self::RichText(text) => text.font_height(fonts, style),
             Self::LayoutJob(job) => job.font_height(fonts),
             Self::Galley(galley) => {
-                if let Some(row) = galley.rows.first() {
-                    row.height().round_ui()
+                if let Some(placed_row) = galley.rows.first() {
+                    placed_row.height().round_ui()
                 } else {
                     galley.size().y.round_ui()
                 }

crates/egui/src/widgets/label.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
 use crate::{
-    epaint, pos2, text_selection, vec2, Align, Direction, FontSelection, Galley, Pos2, Response,
-    Sense, Stroke, TextWrapMode, Ui, Widget, WidgetInfo, WidgetText, WidgetType,
+    epaint, pos2, text_selection, Align, Direction, FontSelection, Galley, Pos2, Response, Sense,
+    Stroke, TextWrapMode, Ui, Widget, WidgetInfo, WidgetText, WidgetType,
 };
 
 use self::text_selection::LabelSelectionState;
@@ -216,10 +216,10 @@ impl Label {
             let pos = pos2(ui.max_rect().left(), ui.cursor().top());
             assert!(!galley.rows.is_empty(), "Galleys are never empty");
             // collect a response from many rows:
-            let rect = galley.rows[0].rect.translate(vec2(pos.x, pos.y));
+            let rect = galley.rows[0].rect().translate(pos.to_vec2());
             let mut response = ui.allocate_rect(rect, sense);
-            for row in galley.rows.iter().skip(1) {
-                let rect = row.rect.translate(vec2(pos.x, pos.y));
+            for placed_row in galley.rows.iter().skip(1) {
+                let rect = placed_row.rect().translate(pos.to_vec2());
                 response |= ui.allocate_rect(rect, sense);
             }
             (pos, galley, response)

crates/egui_demo_lib/Cargo.toml
@@ -58,6 +58,7 @@ serde = { workspace = true, optional = true }
 criterion.workspace = true
 egui_kittest = { workspace = true, features = ["wgpu", "snapshot"] }
 egui = { workspace = true, features = ["default_fonts"] }
+rand = "0.9"
 
 [[bench]]
 name = "benchmark"

commit e9609ac94e6f3d23314f3577974b4bd6d8ededf6

Fix `leading_space` sometimes being ignored during paragraph splitting (#7031)
Fixes a regression introduced in https://github.com/emilk/egui/pull/5411
(possibly
https://github.com/emilk/egui/pull/5411/commits/d74bee536f7c57d83014b759b18a0ddef8725552)
that breaks `leading_space` handling.
I think this is what the condition should be but I haven't touched this
code in a while.

crates/epaint/src/text/fonts.rs
@@ -913,7 +913,7 @@ impl GalleyCache {
                         "Bad new section range: {new_range:?}"
                     );
                     paragraph_job.sections.push(LayoutSection {
-                        leading_space: if start <= new_range.start {
+                        leading_space: if start <= section_range.start {
                             *leading_space
                         } else {
                             0.0
@@ -1080,20 +1080,20 @@ mod tests {
                     ..Default::default()
                 };
                 job.append(
-                    "1st paragraph has some leading space.\n",
-                    16.0,
-                    TextFormat {
-                        font_id: FontId::new(14.0, FontFamily::Proportional),
-                        ..Default::default()
-                    },
-                );
-                job.append(
-                    "2nd paragraph has underline and strikthrough, and has some non-ASCII characters:\n ÅÄÖ.",
+                    "1st paragraph has underline and strikethrough, and has some non-ASCII characters:\n ÅÄÖ.",
                     0.0,
                     TextFormat {
                         font_id: FontId::new(15.0, FontFamily::Monospace),
                         underline: Stroke::new(1.0, Color32::RED),
                         strikethrough: Stroke::new(1.0, Color32::GREEN),
+                        ..Default::default()
+                    },
+                );
+                job.append(
+                    "2nd paragraph has some leading space.\n",
+                    16.0,
+                    TextFormat {
+                        font_id: FontId::new(14.0, FontFamily::Proportional),
                         ..Default::default()
                     },
                 );