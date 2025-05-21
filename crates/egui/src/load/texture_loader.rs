210a1b77f3bd61abd665f982e522d4bfba99d10a
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-05-21 18:09:51 +0200

    Render the same SVG multiple times, if asked for in different sizes

--- crates/egui/src/load/texture_loader.rs
+++ crates/egui/src/load/texture_loader.rs
@@ -5,9 +5,16 @@ use super::{
     TextureLoadResult, TextureLoader, TextureOptions, TexturePoll,
 };
 
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct Key<'a> {
+    uri: Cow<'a, str>,
+    texture_options: TextureOptions,
+    svg_size_hint: Option<SizeHint>,
+}
+
 #[derive(Default)]
 pub struct DefaultTextureLoader {
-    cache: Mutex<HashMap<(Cow<'static, str>, TextureOptions), TextureHandle>>,
+    cache: Mutex<HashMap<Key<'static>, TextureHandle>>,
 }
 
 impl TextureLoader for DefaultTextureLoader {
@@ -22,8 +29,24 @@ impl TextureLoader for DefaultTextureLoader {
         texture_options: TextureOptions,
         size_hint: SizeHint,
     ) -> TextureLoadResult {
+        let svg_size_hint = if uri.ends_with(".svg") {
+            // For SVGs it's important that we render at the desired size,
+            // or we might get a blurry image when we scale it up.
+            // So we make the size hint a part of the cache key.
+            // This might lead to a lot of extra entries for the same SVG file,
+            // which is potentially wasteful of RAM, but better that than blurry images.
+            Some(size_hint)
+        } else {
+            // For other images we just use one cache value, no matter what the size we render at.
+            None
+        };
+
         let mut cache = self.cache.lock();
-        if let Some(handle) = cache.get(&(Cow::Borrowed(uri), texture_options)) {
+        if let Some(handle) = cache.get(&Key {
+            uri: Cow::Borrowed(uri),
+            texture_options,
+            svg_size_hint,
+        }) {
             let texture = SizedTexture::from_handle(handle);
             Ok(TexturePoll::Ready { texture })
         } else {
@@ -32,7 +55,14 @@ impl TextureLoader for DefaultTextureLoader {
                 ImagePoll::Ready { image } => {
                     let handle = ctx.load_texture(uri, image, texture_options);
                     let texture = SizedTexture::from_handle(&handle);
-                    cache.insert((Cow::Owned(uri.to_owned()), texture_options), handle);
+                    cache.insert(
+                        Key {
+                            uri: Cow::Owned(uri.to_owned()),
+                            texture_options,
+                            svg_size_hint,
+                        },
+                        handle,
+                    );
                     let reduce_texture_memory = ctx.options(|o| o.reduce_texture_memory);
                     if reduce_texture_memory {
                         let loaders = ctx.loaders();
@@ -54,7 +84,7 @@ impl TextureLoader for DefaultTextureLoader {
         #[cfg(feature = "log")]
         log::trace!("forget {uri:?}");
 
-        self.cache.lock().retain(|(u, _), _| u != uri);
+        self.cache.lock().retain(|key, _value| key.uri != uri);
     }
 
     fn forget_all(&self) {
