1abccb3f47d8e252474f9234b1f3e872d865547d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2025-06-03 15:57:43 +0100

    Typo in `run_native` doc comment (replace "a an" with "an") (#7094)
    <!--
    Please read the "Making a PR" section of
    [`CONTRIBUTING.md`](https://github.com/emilk/egui/blob/main/CONTRIBUTING.md)
    before opening a Pull Request!
    
    * Keep your PR:s small and focused.
    * The PR title is what ends up in the changelog, so make it descriptive!
    * If applicable, add a screenshot or gif.
    * If it is a non-trivial addition, consider adding a demo for it to
    `egui_demo_lib`, or a new example.
    * Do NOT open PR:s from your `master` branch, as that makes it hard for
    maintainers to test and add commits to your PR.
    * Remember to run `cargo fmt` and `cargo clippy`.
    * Open the PR as a draft until you have self-reviewed it and run
    `./scripts/check.sh`.
    * When you have addressed a PR comment, mark it as resolved.
    
    Please be patient! I will review your PR, but my time is limited!
    -->

--- crates/eframe/src/lib.rs
+++ crates/eframe/src/lib.rs
@@ -209,7 +209,7 @@ pub mod icon_data;
 
 /// This is how you start a native (desktop) app.
 ///
-/// The first argument is name of your app, which is a an identifier
+/// The first argument is name of your app, which is an identifier
 /// used for the save location of persistence (see [`App::save`]).
 /// It is also used as the application id on wayland.
 /// If you set no title on the viewport, the app id will be used
