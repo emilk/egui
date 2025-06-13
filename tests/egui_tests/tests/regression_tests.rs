b3ea42f0e51e6459cd039100e5798dc149ee6f11
lucasmerlin <hi@lucasmerlin.me>
2025-06-13 13:44:35 +0200

    Report image alt text as text if widget contains no other text

---
+++ tests/egui_tests/tests/regression_tests.rs
@@ -0,0 +1,14 @@
+use egui::{include_image, Image};
+use egui_kittest::kittest::Queryable as _;
+use egui_kittest::Harness;
+
+#[test]
+fn image_button_should_have_alt_text() {
+    let harness = Harness::new_ui(|ui| {
+        _ = ui.button(
+            Image::new(include_image!("../../../crates/eframe/data/icon.png")).alt_text("Egui"),
+        );
+    });
+
+    harness.get_by_label("Egui");
+}
