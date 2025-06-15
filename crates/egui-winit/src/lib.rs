742da95bd7a6f18c1aacc49fb1ce93c537e38576
ardocrat <ardocrat@gri.mw>
2025-06-16 02:28:27 +0300

    Support for Back button Key on Android (#7073)
    When your press a Back button on Android (for example at
    `native-activity`), [Winit translates this
    key](https://github.com/rust-windowing/winit/blob/47b938dbe78702d521c2c7a43b6f741a3bb8cb0b/src/platform_impl/android/keycodes.rs#L237C42-L237C53)
    as `NamedKey::BrowserBack`. Added convertion to `Key::Escape` at
    `egui-winit` module.
    
    ---------
    
    Co-authored-by: Advocat <advocat@ogr.local>

--- crates/egui-winit/src/lib.rs
+++ crates/egui-winit/src/lib.rs
@@ -1144,6 +1144,8 @@ fn key_from_named_key(named_key: winit::keyboard::NamedKey) -> Option<egui::Key>
         NamedKey::F33 => Key::F33,
         NamedKey::F34 => Key::F34,
         NamedKey::F35 => Key::F35,
+
+        NamedKey::BrowserBack => Key::BrowserBack,
         _ => {
             log::trace!("Unknown key: {named_key:?}");
             return None;
