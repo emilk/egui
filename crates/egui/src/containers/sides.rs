fb14578b807300d9300e91969956c04edccc0ef4
lucasmerlin <hi@lucasmerlin.me>
2025-07-03 10:22:29 +0200

    Add `egui::Sides` `shrink_left` / `shrink_right`

--- crates/egui/src/containers/sides.rs
+++ crates/egui/src/containers/sides.rs
@@ -18,7 +18,7 @@ use crate::{Layout, Ui, UiBuilder};
 /// The width of the gap is dynamic, based on the max width of the parent [`Ui`].
 /// When the parent is being auto-sized ([`Ui::is_sizing_pass`]) the gap will be as small as possible.
 ///
-/// If the parent is not wide enough to fit all widgets, the parent will be expanded to the right.
+/// ~If the parent is not wide enough to fit all widgets, the parent will be expanded to the right.~
 ///
 /// The left widgets are first added to the ui, left-to-right.
 /// Then the right widgets are added, right-to-left.
@@ -40,6 +40,16 @@ use crate::{Layout, Ui, UiBuilder};
 pub struct Sides {
     height: Option<f32>,
     spacing: Option<f32>,
+    kind: SidesKind,
+    wrap_mode: Option<crate::TextWrapMode>,
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+enum SidesKind {
+    #[default]
+    Extend,
+    ShrinkLeft,
+    ShrinkRight,
 }
 
 impl Sides {
@@ -68,13 +78,59 @@ impl Sides {
         self
     }
 
+    pub fn shrink_left(mut self) -> Self {
+        self.kind = SidesKind::ShrinkLeft;
+        self
+    }
+
+    pub fn shrink_right(mut self) -> Self {
+        self.kind = SidesKind::ShrinkRight;
+        self
+    }
+
+    pub fn extend(mut self) -> Self {
+        self.kind = SidesKind::Extend;
+        self
+    }
+
+    /// The text wrap mode for the shrinking side.
+    ///
+    /// Does nothing if the kind is [`SidesKind::Extend`].
+    pub fn wrap_mode(mut self, wrap_mode: crate::TextWrapMode) -> Self {
+        self.wrap_mode = Some(wrap_mode);
+        self
+    }
+
+    /// Truncate the text on the shrinking side.
+    ///
+    /// This is a shortcut for [`Self::wrap_mode`].
+    /// Does nothing if the kind is [`SidesKind::Extend`].
+    pub fn truncate(mut self) -> Self {
+        self.wrap_mode = Some(crate::TextWrapMode::Truncate);
+        self
+    }
+
+    /// Wrap the text on the shrinking side.
+    ///
+    /// This is a shortcut for [`Self::wrap_mode`].
+    /// Does nothing if the kind is [`SidesKind::Extend`].
+    pub fn wrap(mut self) -> Self {
+        self.wrap_mode = Some(crate::TextWrapMode::Wrap);
+        self
+    }
+
     pub fn show<RetL, RetR>(
         self,
         ui: &mut Ui,
         add_left: impl FnOnce(&mut Ui) -> RetL,
         add_right: impl FnOnce(&mut Ui) -> RetR,
     ) -> (RetL, RetR) {
-        let Self { height, spacing } = self;
+        let Self {
+            height,
+            spacing,
+            mut kind,
+            mut wrap_mode,
+        } = self;
         let height = height.unwrap_or_else(|| ui.spacing().interact_size.y);
         let spacing = spacing.unwrap_or_else(|| ui.spacing().item_spacing.x);
 
@@ -84,41 +140,118 @@ impl Sides {
         let result_left;
         let result_right;
 
-        let left_rect = {
-            let left_max_rect = top_rect;
-            let mut left_ui = ui.new_child(
-                UiBuilder::new()
-                    .max_rect(left_max_rect)
-                    .layout(Layout::left_to_right(Align::Center)),
-            );
-            result_left = add_left(&mut left_ui);
-            left_ui.min_rect()
-        };
-
-        let right_rect = {
-            let right_max_rect = top_rect.with_min_x(left_rect.max.x);
-            let mut right_ui = ui.new_child(
-                UiBuilder::new()
-                    .max_rect(right_max_rect)
-                    .layout(Layout::right_to_left(Align::Center)),
-            );
-            result_right = add_right(&mut right_ui);
-            right_ui.min_rect()
-        };
-
-        let mut final_rect = left_rect.union(right_rect);
-        let min_width = left_rect.width() + spacing + right_rect.width();
-
         if ui.is_sizing_pass() {
-            // Make as small as possible:
-            final_rect.max.x = left_rect.min.x + min_width;
-        } else {
-            // If the rects overlap, make sure we expand the allocated rect so that the parent
-            // ui knows we overflowed, and resizes:
-            final_rect.max.x = final_rect.max.x.max(left_rect.min.x + min_width);
+            kind = SidesKind::Extend;
+            wrap_mode = None;
         }
 
-        ui.advance_cursor_after_rect(final_rect);
+        match kind {
+            SidesKind::ShrinkLeft => {
+                // Draw right side first, then limit left side width
+                let right_rect = {
+                    let right_max_rect = top_rect;
+                    let mut right_ui = ui.new_child(
+                        UiBuilder::new()
+                            .max_rect(right_max_rect)
+                            .layout(Layout::right_to_left(Align::Center)),
+                    );
+                    result_right = add_right(&mut right_ui);
+                    right_ui.min_rect()
+                };
+
+                let left_rect = {
+                    let available_width = top_rect.width() - right_rect.width() - spacing;
+                    let left_max_rect =
+                        top_rect.with_max_x(top_rect.min.x + available_width.max(0.0));
+                    let mut left_ui = ui.new_child(
+                        UiBuilder::new()
+                            .max_rect(left_max_rect)
+                            .layout(Layout::left_to_right(Align::Center)),
+                    );
+                    if let Some(wrap_mode) = wrap_mode {
+                        left_ui.style_mut().wrap_mode = Some(wrap_mode);
+                    }
+                    result_left = add_left(&mut left_ui);
+                    left_ui.min_rect()
+                };
+
+                let final_rect = left_rect.union(right_rect);
+                ui.advance_cursor_after_rect(final_rect);
+            }
+            SidesKind::ShrinkRight => {
+                // Draw left side first, then limit right side width
+                let left_rect = {
+                    let left_max_rect = top_rect;
+                    let mut left_ui = ui.new_child(
+                        UiBuilder::new()
+                            .max_rect(left_max_rect)
+                            .layout(Layout::left_to_right(Align::Center)),
+                    );
+                    result_left = add_left(&mut left_ui);
+                    left_ui.min_rect()
+                };
+
+                let right_rect = {
+                    let available_width = top_rect.width() - left_rect.width() - spacing;
+                    let right_max_rect = top_rect
+                        .with_min_x(left_rect.max.x + spacing)
+                        .with_max_x(top_rect.max.x);
+                    let right_max_rect =
+                        right_max_rect.with_max_x(right_max_rect.min.x + available_width.max(0.0));
+                    let mut right_ui = ui.new_child(
+                        UiBuilder::new()
+                            .max_rect(right_max_rect)
+                            .layout(Layout::right_to_left(Align::Center)),
+                    );
+                    if let Some(wrap_mode) = wrap_mode {
+                        right_ui.style_mut().wrap_mode = Some(wrap_mode);
+                    }
+                    result_right = add_right(&mut right_ui);
+                    right_ui.min_rect()
+                };
+
+                let final_rect = left_rect.union(right_rect);
+                ui.advance_cursor_after_rect(final_rect);
+            }
+            SidesKind::Extend => {
+                // Original behavior: left first, then right, then final_rect calculations
+                let left_rect = {
+                    let left_max_rect = top_rect;
+                    let mut left_ui = ui.new_child(
+                        UiBuilder::new()
+                            .max_rect(left_max_rect)
+                            .layout(Layout::left_to_right(Align::Center)),
+                    );
+                    result_left = add_left(&mut left_ui);
+                    left_ui.min_rect()
+                };
+
+                let right_rect = {
+                    let right_max_rect = top_rect.with_min_x(left_rect.max.x);
+                    let mut right_ui = ui.new_child(
+                        UiBuilder::new()
+                            .max_rect(right_max_rect)
+                            .layout(Layout::right_to_left(Align::Center)),
+                    );
+                    result_right = add_right(&mut right_ui);
+                    right_ui.min_rect()
+                };
+
+                let mut final_rect = left_rect.union(right_rect);
+                let min_width = left_rect.width() + spacing + right_rect.width();
+
+                if ui.is_sizing_pass() {
+                    // Make as small as possible:
+                    final_rect.max.x = left_rect.min.x + min_width;
+                } else {
+                    // If the rects overlap, make sure we expand the allocated rect so that the parent
+                    // ui knows we overflowed, and resizes:
+                    final_rect.max.x = final_rect.max.x.max(left_rect.min.x + min_width);
+                }
+
+                ui.advance_cursor_after_rect(final_rect);
+            }
+        }
 
         (result_left, result_right)
     }
---
+++ tests/egui_tests/tests/test_sides.rs
@@ -0,0 +1,76 @@
+use egui::{TextWrapMode, Ui, Vec2, containers::Sides};
+use egui_kittest::{Harness, SnapshotResult, SnapshotResults};
+
+#[test]
+fn sides_container_tests() {
+    let mut results = SnapshotResults::new();
+
+    test_variants("default", |sides| sides, &mut results);
+
+    test_variants(
+        "shrink_left",
+        |sides| sides.shrink_left().truncate(),
+        &mut results,
+    );
+
+    test_variants(
+        "shrink_right",
+        |sides| sides.shrink_right().truncate(),
+        &mut results,
+    );
+
+    test_variants(
+        "wrap_left",
+        |sides| sides.shrink_left().wrap_mode(TextWrapMode::Wrap),
+        &mut results,
+    );
+
+    test_variants(
+        "wrap_right",
+        |sides| sides.shrink_right().wrap_mode(TextWrapMode::Wrap),
+        &mut results,
+    );
+}
+
+fn test_variants(
+    name: &str,
+    mut create_sides: impl FnMut(Sides) -> Sides,
+    results: &mut SnapshotResults,
+) {
+    for (variant_name, left_text, right_text, fit_contents) in [
+        ("short", "Left", "Right", false),
+        (
+            "long",
+            "Very long left content that should not fit.",
+            "Very long right text that should also not fit.",
+            false,
+        ),
+        ("short_fit_contents", "Left", "Right", true),
+        (
+            "long_fit_contents",
+            "Very long left content that should not fit.",
+            "Very long right text that should also not fit.",
+            true,
+        ),
+    ] {
+        let mut harness = Harness::builder()
+            .with_size(Vec2::new(400.0, 50.0))
+            .build_ui(|ui| {
+                create_sides(Sides::new()).show(
+                    ui,
+                    |left| {
+                        left.label(left_text);
+                    },
+                    |right| {
+                        right.label(right_text);
+                    },
+                );
+            });
+
+        if fit_contents {
+            harness.fit_contents();
+        }
+
+        results.add(harness.try_snapshot(&format!("sides/{name}_{variant_name}")));
+    }
+}
