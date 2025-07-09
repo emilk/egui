9fd0ad36e0640b2df24312d945862507e38aa8a3
Lucas Meurer <hi@lucasmerlin.me>
2025-07-09 15:29:51 +0200

    Implement `BitOr` and `BitOrAssign` for `Rect` (#7319)

--- crates/epaint/src/text/text_layout.rs
+++ crates/epaint/src/text/text_layout.rs
@@ -691,13 +691,12 @@ fn galley_from_rows(
     let mut num_indices = 0;
 
     for placed_row in &mut rows {
-        rect = rect.union(placed_row.rect());
+        rect |= placed_row.rect();
 
         let row = Arc::make_mut(&mut placed_row.row);
         row.visuals = tessellate_row(point_scale, &job, &format_summary, row);
 
-        mesh_bounds =
-            mesh_bounds.union(row.visuals.mesh_bounds.translate(placed_row.pos.to_vec2()));
+        mesh_bounds |= row.visuals.mesh_bounds.translate(placed_row.pos.to_vec2());
         num_vertices += row.visuals.mesh.vertices.len();
         num_indices += row.visuals.mesh.indices.len();
 
