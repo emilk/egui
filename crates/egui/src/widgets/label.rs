1080d71a0688846d270971ce38bafda59a573268
lucasmerlin <hi@lucasmerlin.me>
2025-07-10 10:35:03 +0200

    Set intrinsic size for Label

--- crates/egui/src/widgets/label.rs
+++ crates/egui/src/widgets/label.rs
@@ -220,6 +220,7 @@ impl Label {
                 .rect_without_leading_space()
                 .translate(pos.to_vec2());
             let mut response = ui.allocate_rect(rect, sense);
+            response.intrinsic_size = Some(galley.intrinsic_size());
             for placed_row in galley.rows.iter().skip(1) {
                 let rect = placed_row.rect().translate(pos.to_vec2());
                 response |= ui.allocate_rect(rect, sense);
@@ -252,7 +253,8 @@ impl Label {
             };
 
             let galley = ui.fonts(|fonts| fonts.layout_job(layout_job));
-            let (rect, response) = ui.allocate_exact_size(galley.size(), sense);
+            let (rect, mut response) = ui.allocate_exact_size(galley.size(), sense);
+            response.intrinsic_size = Some(galley.intrinsic_size());
             let galley_pos = match galley.job.halign {
                 Align::LEFT => rect.left_top(),
                 Align::Center => rect.center_top(),
