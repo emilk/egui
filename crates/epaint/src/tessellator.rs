ac3798f7cc9c776e76586fa53f81eeeb6a9ca66c
Piotr Podusowski <podusowski@gmail.com>
2025-09-29 20:49:09 +0200

    more test fixes

--- crates/epaint/src/tessellator.rs
+++ crates/epaint/src/tessellator.rs
@@ -2543,7 +2543,7 @@ fn path_bounding_box() {
 
         path.stroke(
             1.5,
-            1.0,
+            f32::MAX,
             PathType::Closed,
             &PathStroke::new_uv(width, move |r, p| {
                 assert_eq!(r, expected_rect);
