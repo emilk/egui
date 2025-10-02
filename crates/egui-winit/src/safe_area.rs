7ff814a25a67530360d11254fa2ebba0c2d7a792
Ian Hobson <ian.r.hobson@gmail.com>
2025-10-02 11:24:38 +0200

    Use objc2 instead of objc to get the iOS safe area insets

--- crates/egui-winit/src/safe_area.rs
+++ crates/egui-winit/src/safe_area.rs
@@ -1,41 +1,56 @@
-use egui::{SafeAreaInsets, epaint::MarginF32};
-use objc::runtime::Object;
-use objc::{class, msg_send, sel, sel_impl};
+#[cfg(target_os = "ios")]
+pub use ios::get_safe_area_insets;
 
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct UIEdgeInsets {
-    top: f64,
-    left: f64,
-    bottom: f64,
-    right: f64,
-}
+#[cfg(target_os = "ios")]
+mod ios {
+    use egui::{SafeAreaInsets, epaint::MarginF32};
+    use objc2::{ClassType, rc::Retained};
+    use objc2_foundation::{MainThreadMarker, NSObjectProtocol};
+    use objc2_ui_kit::{UIApplication, UISceneActivationState, UIWindowScene};
 
-/// Gets the ios safe area insets
-/// A safe area defines the area within a view that isn’t covered by a navigation bar, tab bar,
-/// toolbar, or other views a window might provide. Safe areas are essential for avoiding a device’s
-/// interactive and display features, like Dynamic Island on iPhone or the camera housing on some
-/// Mac models. For developer guidance, see Positioning content relative to the safe area.
-///
-/// Once winit v0.31 has been released this can be removed in favour of `winit::Window::safe_area`.
-#[allow(unsafe_code)]
-pub fn get_ios_safe_area_insets() -> UIEdgeInsets {
-    unsafe {
-        let shared_application: *mut Object = msg_send![class!(UIApplication), sharedApplication];
-        let windows: *mut Object = msg_send![shared_application, windows];
-        let first_object: *mut Object = msg_send![windows, firstObject];
-        let safe_area_insets: UIEdgeInsets = msg_send![first_object, safeAreaInsets];
-        safe_area_insets
-    }
-}
+    /// Gets the ios safe area insets.
+    ///
+    /// A safe area defines the area within a view that isn’t covered by a navigation bar, tab bar,
+    /// toolbar, or other views a window might provide. Safe areas are essential for avoiding a
+    /// device’s interactive and display features, like Dynamic Island on iPhone or the camera
+    /// housing on some Mac models.
+    ///
+    /// Once winit v0.31 has been released this can be removed in favour of
+    /// `winit::Window::safe_area`.
+    pub fn get_safe_area_insets() -> SafeAreaInsets {
+        let Some(main_thread_marker) = MainThreadMarker::new() else {
+            log::error!("Getting safe area insets needs to be performed on the main thread");
+            return SafeAreaInsets::default();
+        };
 
-impl From<UIEdgeInsets> for SafeAreaInsets {
-    fn from(value: UIEdgeInsets) -> Self {
-        SafeAreaInsets(MarginF32 {
-            top: value.top as f32,
-            left: value.left as f32,
-            bottom: value.bottom as f32,
-            right: value.right as f32,
-        })
+        let app = UIApplication::sharedApplication(main_thread_marker);
+
+        #[allow(unsafe_code)]
+        unsafe {
+            // Look for the first window scene that's in the foreground
+            for scene in app.connectedScenes() {
+                if scene.isKindOfClass(UIWindowScene::class())
+                    && matches!(
+                        scene.activationState(),
+                        UISceneActivationState::ForegroundActive
+                            | UISceneActivationState::ForegroundInactive
+                    )
+                {
+                    // Safe to cast, the class kind was checked above
+                    let window_scene = Retained::cast::<UIWindowScene>(scene.clone());
+                    if let Some(window) = window_scene.keyWindow() {
+                        let insets = window.safeAreaInsets();
+                        return SafeAreaInsets(MarginF32 {
+                            top: insets.top as f32,
+                            left: insets.left as f32,
+                            right: insets.right as f32,
+                            bottom: insets.bottom as f32,
+                        });
+                    }
+                }
+            }
+        }
+
+        SafeAreaInsets::default()
     }
 }
