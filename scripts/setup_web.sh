4d027feb58fd5822ed4fe138a26dcdffc9f5a0bd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-10-08 16:39:27 +0200

    Do not lock wasm-bindgen version

--- scripts/setup_web.sh
+++ scripts/setup_web.sh
@@ -9,6 +9,7 @@ set -x
 rustup target add wasm32-unknown-unknown
 
 # For generating JS bindings:
+# Keep wasm-bindgen in sync in: setup_web.sh, Cargo.toml, Cargo.lock, rust.yml
 if ! cargo install --list | grep -q 'wasm-bindgen-cli v0.2.104'; then
     cargo install --force --quiet wasm-bindgen-cli --version 0.2.104
 fi
