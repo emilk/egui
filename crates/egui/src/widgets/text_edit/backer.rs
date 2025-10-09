51ecd8959a9c1eb0d4bd0813d1125362890e2738
tye-exe <tye@mailbox.org>
2025-10-09 21:12:48 +0100

    Give TextType read method value of previous display string
    This allows proper parsing of the char datatype, as you can
    identify which char is new and which is old.

--- crates/egui/src/widgets/text_edit/backer.rs
+++ crates/egui/src/widgets/text_edit/backer.rs
@@ -1,13 +1,26 @@
-use std::{convert::Infallible, error::Error, fmt::Display, num::ParseIntError};
+use std::{
+    collections::HashSet, convert::Infallible, error::Error, fmt::Display, num::ParseIntError,
+};
 
 pub trait TextType: Sized {
     type Err: Error;
 
-    fn read_from_string(s: &str) -> Option<Result<Self, Self::Err>>;
+    /// The value of represented data type depending on the previous string text and modified
+    /// (by the user) string.
+    ///
+    /// `None` is output if this type is immutable.
+    /// `Some(result)` is the result of parsing.
+    ///
+    /// This **must** be parse output from [`TextType::string_representation`].
+    fn read_from_strings(previous: &str, modified: &str) -> Option<Result<Self, Self::Err>>;
+    /// Generate the string representation of this type.
+    ///
+    /// This **must** be parseable by [`TextType::read_from_strings`].
     fn string_representation(&self) -> String;
 
+    /// Can the user change the value of this display type?
     fn is_mutable() -> bool {
-        !Self::read_from_string("").is_none()
+        !Self::read_from_strings("", "").is_none()
     }
 }
 
@@ -25,7 +38,7 @@ impl Error for ConversionError {}
 impl TextType for &str {
     type Err = Infallible;
 
-    fn read_from_string(_s: &str) -> Option<Result<Self, Self::Err>> {
+    fn read_from_strings(_previous: &str, _modified: &str) -> Option<Result<Self, Self::Err>> {
         None
     }
 
@@ -37,8 +50,8 @@ impl TextType for &str {
 impl TextType for String {
     type Err = Infallible;
 
-    fn read_from_string(s: &str) -> Option<Result<Self, Self::Err>> {
-        Some(Ok(s.to_string()))
+    fn read_from_strings(_previous: &str, modified: &str) -> Option<Result<Self, Self::Err>> {
+        Some(Ok(modified.to_string()))
     }
 
     fn string_representation(&self) -> String {
@@ -49,8 +62,8 @@ impl TextType for String {
 impl TextType for u8 {
     type Err = ParseIntError;
 
-    fn read_from_string(s: &str) -> Option<Result<Self, Self::Err>> {
-        Some(s.parse())
+    fn read_from_strings(_previous: &str, modified: &str) -> Option<Result<Self, Self::Err>> {
+        Some(modified.parse())
     }
 
     fn string_representation(&self) -> String {
@@ -61,17 +74,23 @@ impl TextType for u8 {
 impl TextType for char {
     type Err = ConversionError;
 
-    fn read_from_string(s: &str) -> Option<Result<Self, Self::Err>> {
-        let mut chars = s.chars();
-        let character = chars.next();
+    fn read_from_strings(previous: &str, modified: &str) -> Option<Result<Self, Self::Err>> {
+        let previous: HashSet<char> = previous.chars().collect();
+        let current_chars: HashSet<char> = modified.chars().collect();
+
+        let mut chars = current_chars.difference(&previous);
+        let diff_character = chars.next();
         let additional = chars.next().is_some();
 
-        Some(match (character, additional) {
-            (None, _) => Err(ConversionError("Zero characters present".to_owned())),
+        Some(match (diff_character, additional) {
+            (None, _) => match modified.chars().next() {
+                Some(c) => Ok(c),
+                None => Err(ConversionError("Zero characters present".to_owned())),
+            },
             (Some(_), true) => Err(ConversionError(
                 "More than one character present".to_owned(),
             )),
-            (Some(character), false) => Ok(character),
+            (Some(character), false) => Ok(*character),
         })
     }
 
@@ -83,8 +102,6 @@ impl TextType for char {
 #[cfg(test)]
 mod tests {
     use crate::TextEdit;
-
-    use super::*;
 
     #[allow(unused_must_use)]
     #[test]
