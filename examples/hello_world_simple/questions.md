785e19f999a514e2b7b0336b141f403efef8fedb
Adrien Zianne <adrien@iq002.ipa.iqrypto.com>
2025-10-16 17:11:51 +0200

    CheckboxStyle

--- examples/hello_world_simple/questions.md
+++ examples/hello_world_simple/questions.md
@@ -1,3 +1,7 @@
+## General
+
+- Interact size is great, but it SHOULD or MUST be at least this size ? Maybe add a parameter for this choice ?
+
 ## Button
 
 - If hovering a button add a stroke, the ui shift. Maybe add the option to avoid this resize by making the frame smaller ?
@@ -7,3 +11,9 @@
 - I understand checking if a sense has been set by the user, but why check if it's different than hover ? Is it for technical purpose or purely to avoid confusion with a link ?
 
 - Selecting the text while being underlined move the underline
+
+## Checkbox
+
+- Checkbox are in fact a label and a check box. To propagate the CheckBoxStyle to the label we need to use a scope or something similar and change the global style. Why bind label to the check box, the user can add it himself (To keep the great prototyping speed we could keep this behavior in a Ui method, that would just merge)
+
+- Propose different type of checkmark ? (eg. small square, dot, filled, custom, etc)
--- examples/hello_world_simple/src/main.rs
+++ examples/hello_world_simple/src/main.rs
@@ -1,9 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
-use std::process::exit;
-
-use eframe::egui::{self, Color32, FontId, Label, RichText, Sense, Stroke, style::WidgetVisuals};
+use eframe::egui::{
+    self, Atom, Checkbox, Color32, FontId, Label, RichText, Sense, Stroke, style::WidgetVisuals,
+    vec2,
+};
 
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -16,12 +17,18 @@ fn main() -> eframe::Result {
     // Our application state:
     let mut name = "Arthur".to_owned();
     let mut age = 42;
+    let mut is_adult = age >= 18;
 
     eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.ctx().style_mut(|s| {
+                s.spacing.icon_width_inner = 8.0;
+                s.spacing.icon_width = 15.0;
+                s.spacing.button_padding = vec2(5.0, 5.0);
+                // s.spacing.interact_size.y = 30.0;
                 s.visuals.widgets.inactive = WidgetVisuals {
                     fg_stroke: Stroke::new(1.0, Color32::LIGHT_GRAY),
+                    bg_stroke: Stroke::new(1.0, Color32::LIGHT_GRAY),
                     ..s.visuals.widgets.inactive
                 };
                 s.visuals.widgets.active = WidgetVisuals {
@@ -29,7 +36,7 @@ fn main() -> eframe::Result {
                     ..s.visuals.widgets.inactive
                 };
                 s.visuals.widgets.hovered = WidgetVisuals {
-                    fg_stroke: Stroke::new(1.0, Color32::YELLOW),
+                    fg_stroke: Stroke::new(3.0, Color32::LIGHT_YELLOW),
                     ..s.visuals.widgets.inactive
                 };
                 s.visuals.widgets.noninteractive = WidgetVisuals {
@@ -55,17 +62,22 @@ fn main() -> eframe::Result {
             // ui.add_enabled(false, Button::new("disabled"));
             // ui.add(Button::new("no frame inactive").frame_when_inactive(false));
 
-            ui.label("Normal text");
-            // Should not be affected by WidgetStyle
-            ui.label(
-                RichText::new("Unaffected by style")
-                    .font(FontId::monospace(15.0))
-                    .color(Color32::KHAKI),
-            );
+            // ui.label("Normal text");
+            // // Should not be affected by WidgetStyle
+            // ui.label(
+            //     RichText::new("Unaffected by style")
+            //         .font(FontId::monospace(15.0))
+            //         .color(Color32::KHAKI),
+            // );
 
-            ui.add(Label::new("interaction click").sense(Sense::click()));
-            ui.add(Label::new("focusable").sense(Sense::focusable_noninteractive()))
-                .request_focus();
+            // ui.add(Label::new("interaction click").sense(Sense::click()));
+            // ui.add(Label::new("focusable").sense(Sense::focusable_noninteractive()))
+            //     .request_focus();
+
+            ui.add(Checkbox::new(&mut is_adult, "test"));
+            ui.add(Checkbox::new(&mut is_adult, "test"));
+            ui.add(Checkbox::new(&mut is_adult, Atom::default()));
+            ui.add(Checkbox::new(&mut is_adult, Atom::default()).indeterminate(true));
         });
     })
 }
