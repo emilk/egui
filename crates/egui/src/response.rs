3fcdab4ebd8a5985c969780880eb003367820a16
Ryan <beaconBrigade@gmail.com>
2025-12-05 02:44:06 -0700

    Typo fix in drag-and-drop documentation (#7750)
    * [x] I have followed the instructions in the PR template
    
    Adding periods to the end of sentences and fixes a grammar mistake on
    documentation for the drag-and-drop code to become consistent with the
    rest of the documentation.
    
    The documentation for `Ui::dnd_drop_zone` could've used the word "its"
    instead of "the" to replace "it", but I think "the" more clearly refers
    to the `Frame` since "its" has been used to refer to the drop-zone
    already.

--- crates/egui/src/response.rs
+++ crates/egui/src/response.rs
@@ -472,7 +472,7 @@ impl Response {
     ///
     /// Only returns something if [`Self::contains_pointer`] is true,
     /// the user is drag-dropping something of this type,
-    /// and they released it this frame
+    /// and they released it this frame.
     #[doc(alias = "drag and drop")]
     pub fn dnd_release_payload<Payload: Any + Send + Sync>(&self) -> Option<Arc<Payload>> {
         // NOTE: we use `response.contains_pointer` here instead of `hovered`, because
