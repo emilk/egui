4169d2c1963b5a4732f68a73b3068b6c4e50408f
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-12-21 21:57:29 +0100

    Enable more clippy lints (#7801)

--- crates/egui_kittest/src/lib.rs
+++ crates/egui_kittest/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../README.md")]
+#![cfg_attr(doc, doc = include_str!("../README.md"))]
 //!
 //! ## Feature flags
 #![cfg_attr(feature = "document-features", doc = document_features::document_features!())]
