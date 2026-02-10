5bc83da2d16670023b1d8ae535762c27bf1570f7
obsoleszenz <obsoleszenz@riseup.net>
2026-02-10 14:53:34 +0100

    fix cargo clippy

--- crates/egui-winit/src/lib.rs
+++ crates/egui-winit/src/lib.rs
@@ -1093,8 +1093,8 @@ impl State {
 
             let is_cmd = self.modifiers.ctrl || self.modifiers.command || self.modifiers.mac_cmd;
 
+            #[expect(deprecated)]
             if pressed && !is_cmd {
-                #[allow(deprecated)]
                 self.egui_input.events.push(egui::Event::Text(text));
             }
         }
