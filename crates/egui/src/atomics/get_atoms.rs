9e60dc5a4526809cb8f449b62897cbd2caecc8d5
Nashvill375 <nash.payden@gmail.com>
2026-04-21 17:05:27 -0700

    Fixed GetAtoms trait and made multiple implementations

--- crates/egui/src/atomics/get_atoms.rs
+++ crates/egui/src/atomics/get_atoms.rs
@@ -1,3 +1,28 @@
 pub trait GetAtoms<'a> {
-  pub fn text(self) -> Atom<'a>;
+  pub fn text(self) -> Atoms<'a>;
+}
+
+impl<'a> GetAtoms<'a> for Button<'a> {
+  pub fn text(self) -> Atoms<'a> {
+    self.layout.atoms
+  }
+}
+
+impl<'a> GetAtoms<'a> for Checkbox<'a> {
+  pub fn text(self) -> Atoms<'a> {
+    self.atoms
+  }
+}
+
+impl<'a> GetAtoms<'a> for DragValue<'a> {
+  pub fn text(self) -> Atoms<'a> {
+    self.atoms
+  }
+}
+
+
+impl<'a> GetAtoms<'a> for RadioButton<'a> {
+  pub fn text(self) -> Atoms<'a> {
+    self.atoms
+  }
 }
