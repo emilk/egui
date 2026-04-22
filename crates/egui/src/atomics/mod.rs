71f198f272e94ab962d3d249b9ab947b6f0bc692
Nashvill375 <nash.payden@gmail.com>
2026-04-21 17:19:51 -0700

    Add get_atoms module to atomics

--- crates/egui/src/atomics/mod.rs
+++ crates/egui/src/atomics/mod.rs
@@ -5,6 +5,7 @@ mod atom_layout;
 mod atoms;
 mod sized_atom;
 mod sized_atom_kind;
+mod get_atoms;
 
 pub use atom::*;
 pub use atom_ext::*;
@@ -13,3 +14,4 @@ pub use atom_layout::*;
 pub use atoms::*;
 pub use sized_atom::*;
 pub use sized_atom_kind::*;
+pub use get_atoms::*;
