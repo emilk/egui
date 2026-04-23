7c024817e4c24565c8acfef2190470ee4828e5ea
Nashvill375 <nash.payden@gmail.com>
2026-04-23 08:33:40 -0700

    Remove unused get_atoms import from mod.rs
    Removed unused import of get_atoms module.

--- crates/egui/src/atomics/mod.rs
+++ crates/egui/src/atomics/mod.rs
@@ -5,7 +5,6 @@ mod atom_layout;
 mod atoms;
 mod sized_atom;
 mod sized_atom_kind;
-mod get_atoms;
 
 pub use atom::*;
 pub use atom_ext::*;
@@ -14,4 +13,3 @@ pub use atom_layout::*;
 pub use atoms::*;
 pub use sized_atom::*;
 pub use sized_atom_kind::*;
-pub use get_atoms::*;
