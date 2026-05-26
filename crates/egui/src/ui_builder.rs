dd72bda5444830992056238b4aa216864bdc73b7
Josie Elliston <68207187+JosieElliston@users.noreply.github.com>
2026-05-26 06:53:55 -0700

    Improve `UiBuilder` docs (#8132)
    Previously, the doc for `Ui::scope_builder` read
    
    > Create a child, add content to it, and then allocate only what was
    used in the parent `Ui`.
    
    which I understood as meaning that "only what was used in the parent
    `UI`" would be allocated (in the child or parent), which makes no sense
    (in either case).
    
    I rewrote it and some related docs.
    
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- crates/egui/src/ui_builder.rs
+++ crates/egui/src/ui_builder.rs
@@ -5,11 +5,13 @@ use crate::ClosableTag;
 use crate::Ui;
 use crate::{Id, LayerId, Layout, Rect, Sense, Style, UiStackInfo};
 
-/// Build a [`Ui`] as the child of another [`Ui`].
+/// The properties specified when creating a top-level or child [`Ui`].
 ///
 /// By default, everything is inherited from the parent,
 /// except for `max_rect` which by default is set to
 /// the parent [`Ui::available_rect_before_wrap`].
+///
+/// See also [`Ui::new`] and [`Ui::new_child`] for uses.
 #[must_use]
 #[derive(Clone, Default)]
 pub struct UiBuilder {
