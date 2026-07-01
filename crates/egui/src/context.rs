e051dbe0fb925f8c0ccc3e354c49fd9a3aba2edd
adrien <221212@umons.ac.be>
2026-07-01 04:34:33 +0200

    replace Context by Ui for correct Style

--- crates/egui/src/context.rs
+++ crates/egui/src/context.rs
@@ -2056,12 +2056,13 @@ impl Context {
     }
 
     /// Compute the [`WidgetStyle`] using the registered theme.
-    pub fn get_widget_style<S: WidgetStyle + Clone + 'static>(
+    pub(crate) fn get_widget_style<S: WidgetStyle + Clone + 'static>(
         &self,
+        ui: &Ui,
         classes: &Classes,
         state: WidgetState,
     ) -> S {
-        self.read(move |ctx| ctx.themes.get::<S>(self, classes, state))
+        self.read(move |ctx| ctx.themes.get::<S>(ui, classes, state))
     }
 }
 
