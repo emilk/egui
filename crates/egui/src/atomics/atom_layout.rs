9164280a606c85414b2b5b912f55673b231cdfe8
Lucas Meurer <hi@lucasmerlin.me>
2026-09-08 13:45:13 +0200

    Add `AtomKind::Paint` (#8524)
    Allows one to do custom painting in places where reading the
    `AtomLayoutResponse` may be hard or impossible.
    Also very useful when combined with `AtomKind::Closure`

--- crates/egui/src/atomics/atom_layout.rs
+++ crates/egui/src/atomics/atom_layout.rs
@@ -1,6 +1,7 @@
 use crate::{
-    AtomKind, Atoms, Direction, FontSelection, Frame, Id, Image, IntoAtoms, Response, Sense,
-    SizedAtom, SizedAtomKind, Stroke, Ui, Widget, text_selection::LabelSelectionState,
+    AtomKind, AtomPaintArgs, Atoms, Direction, FontSelection, Frame, Id, Image, IntoAtoms,
+    Response, Sense, SizedAtom, SizedAtomKind, Stroke, Ui, Widget,
+    text_selection::LabelSelectionState,
 };
 use core::ops::{Deref, DerefMut};
 use emath::{Align2, GuiRounding as _, NumExt as _, Rect, Vec2};
@@ -678,6 +679,15 @@ impl<'atom> SizedAtomLayout<'atom> {
                     image.paint_at(ui, item_rect);
                 }
                 SizedAtomKind::Empty { .. } => {}
+                SizedAtomKind::Paint { paint, size: _ } => {
+                    paint(
+                        ui,
+                        AtomPaintArgs {
+                            rect: item_rect,
+                            fallback_text_color,
+                        },
+                    );
+                }
                 SizedAtomKind::Layout(layout) => {
                     // TODO(lucasmerlin): Add some kind of justify flag, right now nested atoms are always
                     // shown fully stretched.
