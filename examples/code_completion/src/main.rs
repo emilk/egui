f3939c72a43db1c1b2d217e8f06e9e2113fce49b
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2026-09-09 11:58:46 +0200

    Remove unfulfilled `expect(rustdoc::missing_crate_level_docs)`

--- examples/code_completion/src/main.rs
+++ examples/code_completion/src/main.rs
@@ -1,5 +1,4 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-#![expect(rustdoc::missing_crate_level_docs)] // it's an example
 
 //! A code completion popup over a [`egui::TextEdit`], using [`egui::CompletionPopup`].
 //!
