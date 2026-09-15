99cb98e1f9e7eececa3c4f233becf1d95c20df2a
RndUsr123 <150948884+RndUsr123@users.noreply.github.com>
2026-09-15 22:17:43 +0000

    Update hit_test.rs

--- crates/egui/src/hit_test.rs
+++ crates/egui/src/hit_test.rs
@@ -69,9 +69,10 @@ pub fn hit_test(
             // This simplifies the code in `hit_test_on_close` so it doesn't have to check
             // the `enabled` flag everywhere:
             if w.interact_rect.is_negative()
-            || w.rect.any_nan()
-            || w.interact_rect.any_nan()
-            || !w.enabled {
+                || w.rect.any_nan()
+                || w.interact_rect.any_nan()
+                || !w.enabled
+            {
                 return false;
             }
 
