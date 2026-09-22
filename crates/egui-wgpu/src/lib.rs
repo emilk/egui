8a0fa6d454ba4b608811bdf6c0283df775c42e64
878kpmmdxv-ship-it <878kpmmdxv@privaterelay.appleid.com>
2026-09-22 19:21:39 +0200

    Update lib.rs

--- crates/egui-wgpu/src/lib.rs
+++ crates/egui-wgpu/src/lib.rs
@@ -350,6 +350,7 @@ pub struct WgpuConfiguration {
     /// in that case either.
     pub on_surface_status:
         Arc<dyn Fn(&wgpu::CurrentSurfaceTexture) -> SurfaceErrorAction + Send + Sync>,
+    pub reconfigure_lock: Option<std::sync::Arc<epaint::mutex::RwLock<()>>>,
 }
 
 #[test]
